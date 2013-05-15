3e5886d806d0f5ad19bd1ca72a724074a4684548
Joshua Granick <joshua@joshuagranick.com>
2013-05-15 06:55:06 -0700

    Add missing files

---
+++ pazu/events/JoystickEvent.hx
@@ -0,0 +1,24 @@
+package pazu.events;
+#if (display)
+
+
+extern class JoystickEvent extends Event
+{	
+	static var AXIS_MOVE:String;
+	static var BALL_MOVE:String;
+	static var BUTTON_DOWN:String;
+	static var BUTTON_UP:String;
+	static var HAT_MOVE:String;
+	
+	var axis:Array<Float>;
+	var device:Int;
+	var id:Int;
+	var x:Float;
+	var y:Float;
+	var z:Float;
+	
+	function new(type:String, bubbles:Bool = false, cancelable:Bool = false, device:Int = 0, id:Int = 0, x:Float = 0, y:Float = 0, z:Float = 0):Void;
+}
+
+
+#end
---
+++ pazu/events/SystemEvent.hx
@@ -0,0 +1,15 @@
+package pazu.events;
+#if (display)
+
+
+extern class SystemEvent extends Event {
+
+	var data(default, null) : Int;
+	
+	function new(type : String, bubbles : Bool = false, cancelable : Bool = false, data : Int = 0) : Void;
+
+	static var SYSTEM : String;
+}
+
+
+#end
