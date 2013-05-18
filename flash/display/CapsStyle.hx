8ffb873d81190e5ed35ef2b3c0e1e77dee80c71d
Joshua Granick <joshua@joshuagranick.com>
2013-05-18 14:09:47 -0700

    Minor enum change

--- flash/display/CapsStyle.hx
+++ flash/display/CapsStyle.hx
@@ -12,16 +12,16 @@ package flash.display;
 @:fakeEnum(String) extern enum CapsStyle {
 
 	/**
-	 * Used to specify no caps in the <code>caps</code> parameter of the
-	 * <code>flash.display.Graphics.lineStyle()</code> method.
-	 */
-	NONE;
-
-	/**
 	 * Used to specify round caps in the <code>caps</code> parameter of the
 	 * <code>flash.display.Graphics.lineStyle()</code> method.
 	 */
 	ROUND;
+	
+	/**
+	 * Used to specify no caps in the <code>caps</code> parameter of the
+	 * <code>flash.display.Graphics.lineStyle()</code> method.
+	 */
+	NONE;
 
 	/**
 	 * Used to specify square caps in the <code>caps</code> parameter of the
