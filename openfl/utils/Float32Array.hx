edef4df58c5df4c2ed45c7aa6a7c177504d24fe0
Andreas Rønning <andreas@doomsday.no>
2013-07-08 01:16:40 +0200

    Added externs for openfl.utils.Float32Array

--- openfl/utils/Float32Array.hx
+++ openfl/utils/Float32Array.hx
@@ -1,10 +1,12 @@
 package openfl.utils;
 #if display
+import flash.geom.Matrix3D;
 
 
 extern class Float32Array extends ArrayBufferView implements ArrayAccess<Float> {
 	
-	
+	extern public function new (bufferOrArray:Dynamic, start:Int = 0, length:Null<Int> = null);
+	extern public static function fromMatrix (matrix:Matrix3D):Float32Array;
 	
 }
 
