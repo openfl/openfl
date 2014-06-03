9d861e1ea2b3e4b718248b98d090026854d13398
Joshua <joshua@jandyco.com>
2014-06-02 21:20:30 -0400

    Add flash extern for URLRequest to noop userAgent

---
+++ backends/flash/flash/net/URLRequest.hx
@@ -0,0 +1,30 @@
+package flash.net;
+
+@:final extern class URLRequest {
+	function new(?url : String) : Void;
+    public var contentType : String;
+ 	public var data : Dynamic;
+ 	public var digest : String;
+    public var method : String;
+    public var requestHeaders : Array<URLRequestHeader>;
+    public var url : String;
+    public var _userAgent : String;
+    public var userAgent (get,set): String;
+    inline public function get_userAgent (): String { 
+        var userAgent = null;
+        try { 
+            userAgent = untyped this["userAgent"];
+        } catch (e:Dynamic) { 
+			trace ("Warning: URLRequest.userAgent has not been implemented");
+        }
+        return userAgent;
+    }
+    inline public function set_userAgent (userAgent : String): String {
+        try { 
+            untyped this["userAgent"] = userAgent;
+        } catch (e:Dynamic) { 
+			trace ("Warning: URLRequest.userAgent has not been implemented");
+        }
+        return userAgent;
+    }
+}
