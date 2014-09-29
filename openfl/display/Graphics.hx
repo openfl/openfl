6c92875ca33de64efab9b73957618184f7cb869c
Joshua Granick <jgranick@users.noreply.github.com>
2014-09-29 09:35:06 -0700

    Add .drawTiles to openfl.display.Graphics for Flash target, fix openfl.display.Tilesheet for Flash

--- openfl/display/Graphics.hx
+++ openfl/display/Graphics.hx
@@ -958,5 +958,259 @@ class Graphics {
 typedef Graphics = openfl._v2.display.Graphics;
 #end
 #else
-typedef Graphics = flash.display.Graphics;
+
+
+import openfl.geom.Matrix;
+import openfl.geom.Point;
+import openfl.geom.Rectangle;
+
+@:access(openfl.display.Tilesheet)
+
+@:forward(beginBitmapFill, beginFill, beginGradientFill, beginShaderFill, clear, copyFrom, cubicCurveTo, curveTo, drawCircle, drawEllipse, drawGraphicsData, drawPath, drawRect, drawRoundRect, drawRoundRectComplex, drawTriangles, endFill, lineBitmapStyle, lineGradientStyle, lineShaderStyle, lineStyle, lineTo, moveTo, recurse)
+
+
+abstract Graphics(flash.display.Graphics) from flash.display.Graphics to flash.display.Graphics {
+	
+	
+	public function drawTiles (sheet:Tilesheet, tileData:Array<Float>, smooth:Bool = false, flags:Int = 0, count:Int = -1):Void {
+		
+		var useScale = (flags & Tilesheet.TILE_SCALE) > 0;
+		var useRotation = (flags & Tilesheet.TILE_ROTATION) > 0;
+		var useRGB = (flags & Tilesheet.TILE_RGB) > 0;
+		var useAlpha = (flags & Tilesheet.TILE_ALPHA) > 0;
+		var useTransform = (flags & Tilesheet.TILE_TRANS_2x2) > 0;
+		
+		if (useTransform || useScale || useRotation || useRGB || useAlpha) {
+			
+			var scaleIndex = 0;
+			var rotationIndex = 0;
+			var rgbIndex = 0;
+			var alphaIndex = 0;
+			var transformIndex = 0;
+			var numValues = 3;
+			
+			if (useScale) { scaleIndex = numValues; numValues ++; }
+			if (useRotation) { rotationIndex = numValues; numValues ++; }
+			if (useTransform) { transformIndex = numValues; numValues += 4; }
+			if (useRGB) { rgbIndex = numValues; numValues += 3; }
+			if (useAlpha) { alphaIndex = numValues; numValues ++; }
+			
+			var totalCount = count;
+			
+			if (count < 0) {
+				
+				totalCount = tileData.length;
+				
+			}
+			
+			var itemCount = Std.int (totalCount / numValues);
+			
+			var ids = sheet.adjustIDs (sheet.__ids, itemCount);
+			var vertices = sheet.adjustLen (sheet.__vertices, itemCount * 8); 
+			var indices = sheet.adjustIndices (sheet.__indices, itemCount * 6); 
+			var uvtData = sheet.adjustLen (sheet.__uvs, itemCount * 8); 
+			
+			var index = 0;
+			var offset8 = 0;
+			var tileIndex:Int = 0;
+			var tileID:Int = 0;
+			var cacheID:Int = -1;
+			
+			var tile:Rectangle = null;
+			var tileUV:Rectangle = null;
+			var tilePoint:Point = null;
+			var tileHalfHeight:Float = 0;
+			var tileHalfWidth:Float = 0;
+			var tileHeight:Float = 0;
+			var tileWidth:Float = 0;
+
+			while (index < totalCount) {
+				
+				var x = tileData[index];
+				var y = tileData[index + 1];
+				var tileID = Std.int (tileData[index + 2]);
+				var scale = 1.0;
+				var rotation = 0.0;
+				var alpha = 1.0;
+				
+				if (useScale) {
+					
+					scale = tileData[index + scaleIndex];
+					
+				}
+				
+				if (useRotation) {
+					
+					rotation = tileData[index + rotationIndex];
+					
+				}
+				
+				if (useRGB) {
+					
+					//ignore for now
+					
+				}
+				
+				if (useAlpha) {
+					
+					alpha = tileData[index + alphaIndex];
+					
+				}
+				
+				if (cacheID != tileID) {
+					
+					cacheID = tileID;
+					tile = sheet.__tileRects[tileID];
+					tileUV = sheet.__tileUVs[tileID];
+					tilePoint = sheet.__centerPoints[tileID];
+					
+				}
+				
+				if (useTransform) {
+					
+					var tw = tile.width;
+					var th = tile.height;
+					var t0 = tileData[index + transformIndex];
+					var t1 = tileData[index + transformIndex + 1];
+					var t2 = tileData[index + transformIndex + 2];
+					var t3 = tileData[index + transformIndex + 3];
+					var ox = tilePoint.x * tw;
+					var oy = tilePoint.y * th;
+					var ox_ = ox * t0 + oy * t2;
+					oy = ox * t1 + oy * t3;
+					x -= ox_;
+					y -= oy;
+					vertices[offset8] = x;
+					vertices[offset8 + 1] = y;
+					vertices[offset8 + 2] = x + tw * t0;
+					vertices[offset8 + 3] = y + tw * t1;
+					vertices[offset8 + 4] = x + th * t2;
+					vertices[offset8 + 5] = y + th * t3;
+					vertices[offset8 + 6] = x + tw * t0 + th * t2;
+					vertices[offset8 + 7] = y + tw * t1 + th * t3;
+					
+				} else {
+					
+					var tileWidth = tile.width * scale;
+					var tileHeight = tile.height * scale;
+					
+					if (rotation != 0) {
+						
+						var kx = tilePoint.x * tileWidth;
+						var ky = tilePoint.y * tileHeight;
+						var akx = (1 - tilePoint.x) * tileWidth;
+						var aky = (1 - tilePoint.y) * tileHeight;
+						var ca = Math.cos (rotation);
+						var sa = Math.sin (rotation);
+						var xc = kx * sa, xs = kx * ca, yc = ky * sa, ys = ky * ca;
+						var axc = akx * sa, axs = akx * ca, ayc = aky * sa, ays = aky * ca;
+						vertices[offset8] = x - (xc + ys);
+						vertices[offset8 + 1] = y - (-xs + yc);
+						vertices[offset8 + 2] = x + axc - ys;
+						vertices[offset8 + 3] = y - (axs + yc);
+						vertices[offset8 + 4] = x - (xc - ays);
+						vertices[offset8 + 5] = y + xs + ayc;
+						vertices[offset8 + 6] = x + axc + ays;
+						vertices[offset8 + 7] = y + (-axs + ayc);
+						
+					} else {
+						
+						x -= tilePoint.x * tileWidth;
+						y -= tilePoint.y * tileHeight;
+						vertices[offset8] = vertices[offset8 + 4] = x;
+						vertices[offset8 + 1] = vertices[offset8 + 3] = y;
+						vertices[offset8 + 2] = vertices[offset8 + 6] = x + tileWidth;
+						vertices[offset8 + 5] = vertices[offset8 + 7] = y + tileHeight;
+						
+					}
+					
+				}
+				
+				if (ids[tileIndex] != tileID) {
+					
+					ids[tileIndex] = tileID;
+					uvtData[offset8] = uvtData[offset8 + 4] = tileUV.left;
+					uvtData[offset8 + 1] = uvtData[offset8 + 3] = tileUV.top;
+					uvtData[offset8 + 2] = uvtData[offset8 + 6] = tileUV.width;
+					uvtData[offset8 + 5] = uvtData[offset8 + 7] = tileUV.height;
+					
+				}
+				
+				offset8 += 8;
+				index += numValues;
+				tileIndex++;
+				
+			}
+			
+			this.beginBitmapFill (sheet.__bitmap, null, false, smooth);
+			this.drawTriangles (vertices, indices, uvtData);
+			
+		} else {
+			
+			var index = 0;
+			var matrix = new Matrix ();
+			
+			while (index < tileData.length) {
+				
+				var x = tileData[index];
+				var y = tileData[index + 1];
+				var tileID = Std.int (tileData[index + 2]);
+				index += 3;
+				
+				var tile = sheet.__tileRects[tileID];
+				var centerPoint = sheet.__centerPoints[tileID];
+				var ox = centerPoint.x * tile.width, oy = centerPoint.y * tile.height;
+				
+				var scale = 1.0;
+				var rotation = 0.0;
+				var alpha = 1.0;
+				
+				if (useScale) {
+					
+					scale = tileData[index];
+					index ++;
+					
+				}
+				
+				if (useRotation) {
+					
+					rotation = tileData[index];
+					index ++;
+					
+				}
+				
+				if (useRGB) {
+					
+					//ignore for now
+					index += 3;
+					
+				}
+				
+				if (useAlpha) {
+					
+					alpha = tileData[index];
+					index++;
+					
+				}
+				
+				matrix.tx = x - tile.x - ox;
+				matrix.ty = y - tile.y - oy;
+				
+				// need to add support for rotation, alpha, scale and RGB
+				
+				this.beginBitmapFill (sheet.__bitmap, matrix, false, smooth);
+				this.drawRect (x - ox, y - oy, tile.width, tile.height);
+				
+			}
+			
+		}
+		
+		this.endFill ();
+		
+	}
+	
+	
+}
+
+
 #end
--- openfl/display/Tilesheet.hx
+++ openfl/display/Tilesheet.hx
@@ -5,6 +5,9 @@ import openfl.display.BitmapData;
 import openfl.display.Graphics;
 import openfl.geom.Point;
 import openfl.geom.Rectangle;
+import openfl.Vector;
+
+@:allow(openfl.display.Graphics)
 
 
 class Tilesheet {
@@ -20,18 +23,38 @@ class Tilesheet {
 	public static inline var TILE_BLEND_MULTIPLY = 0x00020000;
 	public static inline var TILE_BLEND_SCREEN = 0x00040000;
 	
-	/** @private */ public var __bitmap:BitmapData;
-	/** @private */ public var __centerPoints:Array <Point>;
-	/** @private */ public var __tileRects:Array <Rectangle>;
-	/** @private */ public var __tileUVs:Array <Rectangle>;
+	private static var __defaultPoint = new Point (0, 0);
+	
+	/** @private */ private var __bitmap:BitmapData;
+	/** @private */ private var __centerPoints:Array<Point>;
+	/** @private */ private var __tileRects:Array<Rectangle>;
+	/** @private */ private var __tileUVs:Array<Rectangle>;
+	
+	#if flash
+	private var __bitmapHeight:Int;
+	private var __bitmapWidth:Int;
+	private var __ids:Vector<Int>;
+	private var __indices:Vector<Int>;
+	private var __uvs:Vector<Float>;
+	private var __vertices:Vector<Float>;
+	#end
 	
 	
 	public function new (image:BitmapData) {
 		
 		__bitmap = image;
-		__centerPoints = new Array <Point> ();
-		__tileRects = new Array <Rectangle> ();
-		__tileUVs = new Array <Rectangle> ();
+		__centerPoints = new Array<Point> ();
+		__tileRects = new Array<Rectangle> ();
+		__tileUVs = new Array<Rectangle> ();
+		
+		#if flash
+		__bitmapWidth = __bitmap.width;
+		__bitmapHeight = __bitmap.height;
+		__ids = new Vector <Int>();
+		__vertices = new Vector <Float>();
+		__indices = new Vector <Int>();
+		__uvs = new Vector <Float>();
+		#end
 		
 	}
 	
@@ -42,36 +65,122 @@ class Tilesheet {
 		
 		if (centerPoint == null) {
 			
-			centerPoint = new Point ();
+			centerPoint = __defaultPoint;
 			
 		}
 		
+		#if flash
+		__centerPoints.push (new Point (centerPoint.x / rectangle.width, centerPoint.y / rectangle.height));
+		#else
 		__centerPoints.push (centerPoint);
-		__tileUVs.push (new Rectangle(rectangle.left / __bitmap.width, rectangle.top / __bitmap.height, rectangle.right / __bitmap.width, rectangle.bottom / __bitmap.height));
+		#end
+		
+		__tileUVs.push (new Rectangle (rectangle.left / __bitmap.width, rectangle.top / __bitmap.height, rectangle.right / __bitmap.width, rectangle.bottom / __bitmap.height));
 		
 		return __tileRects.length - 1;
 		
 	}
 	
 	
+	#if flash
+	private inline function adjustIDs (vec:Vector<Int>, len:Int):Vector<Int> {
+		
+		if (vec.length != len) {
+			
+			var prevLen = vec.length;
+			vec.fixed = false;
+			vec.length = len;
+			vec.fixed = true;
+			
+			for (i in prevLen...len)
+				vec[i] = -1;
+				
+		}
+		
+		return vec;
+		
+	}
+	
+	
+	private inline function adjustIndices (vec:Vector<Int>, len:Int):Vector<Int> {
+		
+		if (vec.length != len) {
+			
+			vec.fixed = false;
+			
+			if (vec.length > len) {
+				
+				vec.length = len;
+				vec.fixed = true;
+				
+			} else {
+				
+				var offset6 = vec.length;
+				var offset4 = cast (4 * offset6 / 6, Int);
+				vec.length = len;
+				vec.fixed = true;
+				
+				while (offset6 < len) {
+					
+					vec[offset6] = 0 + offset4;
+					vec[offset6 + 1] = vec[offset6 + 3] = 1 + offset4;
+					vec[offset6 + 2] = vec[offset6 + 5] = 2 + offset4;
+					vec[offset6 + 4] = 3 + offset4;
+					offset4 += 4;
+					offset6 += 6;
+					
+				}
+				
+			}
+			
+		}
+		
+		return vec;
+		
+	}
+	
+	
+	private inline function adjustLen (vec:Vector<Float>, len:Int):Vector<Float> {
+		
+		if (vec.length != len) {
+			
+			vec.fixed = false;
+			vec.length = len;
+			vec.fixed = true;
+			
+		}
+		
+		return vec;
+		
+	}
+	#end
+	
+	
 	public function drawTiles (graphics:Graphics, tileData:Array<Float>, smooth:Bool = false, flags:Int = 0, count:Int = -1):Void {
 		
-		#if !flash
 		graphics.drawTiles (this, tileData, smooth, flags, count);
-		#end
 		
 	}
 	
+	
 	public inline function getTileCenter (index:Int):Point {
+		
 		return __centerPoints[index];
+		
 	}
+	
 	
 	public inline function getTileRect (index:Int):Rectangle {
+		
 		return __tileRects[index];
+		
 	}
 	
+	
 	public inline function getTileUVs (index:Int):Rectangle {
+		
 		return __tileUVs[index];
+		
 	}
 	
 	
