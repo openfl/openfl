a0b686cfa0bb4616c9cd70e2e29b695effa9af61
player-03 <player3.14@gmail.com>
2015-03-16 23:04:18 -0700

    Use high precision only for vertex shaders.
    It's not guaranteed to be available on fragment shaders, but the GLSL ES specification requires it in vertex shaders. Also, vertex shaders are the ones that are likely to need high precision.
    
    I added iOS because Apple (and others) recommend using high precision for vertex shaders. I'd add Blackberry too, but I've never compiled for it.

--- openfl/_internal/aglsl/AGLSLParser.hx
+++ openfl/_internal/aglsl/AGLSLParser.hx
@@ -20,8 +20,14 @@ class AGLSLParser {
 		var body:String = "";
 		var i:Int = 0;
 		
-		#if (html5 || android)
+		#if html5
 		header += "precision highp float;\n";
+		#elseif (android || ios)
+		if (desc.header.type == "vertex") {
+			
+			header += "precision highp float;\n";
+			
+		}
 		#end
 		
 		var tag = desc.header.type.charAt (0); //TODO
