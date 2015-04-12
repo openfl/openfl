ca125d48a5e437d4ad1a0d1793667d5a4fa1c9d1
Joshua Granick <jgranick@users.noreply.github.com>
2015-04-11 22:44:49 -0700

    Try using Dynamic<DisplayObject> on the MovieClip class, should test performance difference later, but useful for SWF library

--- openfl/_legacy/display/MovieClip.hx
+++ openfl/_legacy/display/MovieClip.hx
@@ -4,7 +4,7 @@ package openfl._legacy.display; #if openfl_legacy
 import openfl.display.FrameLabel;
 
 
-class MovieClip extends Sprite {
+class MovieClip extends Sprite implements Dynamic<DisplayObject> {
 	
 	
 	public var currentFrame (get, null):Int;
