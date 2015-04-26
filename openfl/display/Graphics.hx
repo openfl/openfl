7e3556648cb6c05a69a9d346b1bf3a670c706965
Kane Wallmann <k.wallmann@imagingsol.com.au>
2015-04-26 21:52:13 +1000

    Made canvas renderer more consistent with Flash

--- openfl/display/Graphics.hx
+++ openfl/display/Graphics.hx
@@ -911,7 +911,7 @@ class Graphics {
 	 */
 	public function lineStyle (thickness:Null<Float> = null, color:Null<Int> = null, alpha:Null<Float> = null, pixelHinting:Null<Bool> = null, scaleMode:LineScaleMode = null, caps:CapsStyle = null, joints:JointStyle = null, miterLimit:Null<Float> = null):Void {
 		
-		__halfStrokeWidth = (thickness != null) ? thickness / 2 : 0;
+		__halfStrokeWidth = thickness > __halfStrokeWidth ? thickness : __halfStrokeWidth;
 		__commands.push (LineStyle (thickness, color, alpha, pixelHinting, scaleMode, caps, joints, miterLimit));
 		
 		if (thickness != null) __visible = true;
