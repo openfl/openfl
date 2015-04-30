84132dfc02ca662901ec6f2fe04e911c90b95cd0
Lars A. Doucet <lars.doucet@gmail.com>
2015-04-30 09:11:51 -0500

    TextField: make the rectangle have the correct x,y coordinates.

--- openfl/text/TextField.hx
+++ openfl/text/TextField.hx
@@ -1006,7 +1006,7 @@ class TextField extends InteractiveObject {
 	
 	@:noCompletion private override function __getBounds (rect:Rectangle, matrix:Matrix):Void {
 		
-		var bounds = new Rectangle (0, 0, __width, __height);
+		var bounds = new Rectangle (x, y, __width, __height);
 		bounds.transform (__worldTransform);
 		
 		rect.__expand (bounds.x, bounds.y, bounds.width, bounds.height);
