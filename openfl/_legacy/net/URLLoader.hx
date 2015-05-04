b665260244733fecbc0e7b98cde33a8d1f80884e
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-05-04 11:26:01 -0300

    Cleanup, style fix

--- openfl/_legacy/net/URLLoader.hx
+++ openfl/_legacy/net/URLLoader.hx
@@ -30,39 +30,39 @@ private class URLLoadersManager {
 	var loadsQueue : Array<{loader : URLLoader, request : URLRequest}>;
 	var loadsQueueMutex : Mutex;
 
-	public static function getInstance() : URLLoadersManager {
+	public static function getInstance () : URLLoadersManager {
 		if (instance==null)	{
-			instance = new URLLoadersManager();
+			instance = new URLLoadersManager ();
 		}
 		return instance;
 	}
 
-	function new() {
-		activeLoaders = new List<URLLoader>();
+	function new () {
+		activeLoaders = new List<URLLoader> ();
 		loadsQueue = [];
-		loadsQueueMutex = new Mutex();
-		managersThread = Thread.create(mainLoop);
+		loadsQueueMutex = new Mutex ();
+		managersThread = Thread.create (mainLoop);
 	}
 
-	function mainLoop() {
+	function mainLoop () {
 
 		while (true) {
 
-			loadsQueueMutex.acquire();
-			var loadCall = loadsQueue.shift();
-			loadsQueueMutex.release();
+			loadsQueueMutex.acquire ();
+			var loadCall = loadsQueue.shift ();
+			loadsQueueMutex.release ();
 			if (loadCall!=null) {
-				loadCall.loader.loadInCURLThread(loadCall.request);
+				loadCall.loader.loadInCURLThread (loadCall.request);
 			}
 
-			if (!activeLoaders.isEmpty()) {
-				lime_curl_process_loaders();
+			if (!activeLoaders.isEmpty ()) {
+				lime_curl_process_loaders ();
 				var oldLoaders = activeLoaders;
-				activeLoaders = new List<URLLoader>();
+				activeLoaders = new List<URLLoader> ();
 				for (loader in oldLoaders) {
-					loader.update();
+					loader.update ();
 					if (loader.state == URLLoader.urlLoading) {
-						activeLoaders.push(loader);
+						activeLoaders.push (loader);
 					}
 				}
 			}
@@ -72,54 +72,54 @@ private class URLLoadersManager {
 
 	} // mainLoop
 
-	public function enqueueLoad(loader : URLLoader, request : URLRequest) {
-		loadsQueueMutex.acquire();
-		loadsQueue.push({loader : loader, request : request});
-		loadsQueueMutex.release();
+	public function enqueueLoad (loader : URLLoader, request : URLRequest) {
+		loadsQueueMutex.acquire ();
+		loadsQueue.push ({loader : loader, request : request});
+		loadsQueueMutex.release ();
 	}
 
 	public function activeLoadersIsEmpty() {
-		return activeLoaders.isEmpty();
+		return activeLoaders.isEmpty ();
 	}
 
-	public function getActiveLoaders() : List<URLLoader> {
+	public function getActiveLoaders () : List<URLLoader> {
 		if (Thread.current()!=managersThread) throw "Wrong thread : getActiveLoaders";
 		return activeLoaders;
 	}
 
-	public function create(request : URLRequest) : Dynamic {
+	public function create (request : URLRequest) : Dynamic {
 		if (Thread.current()!=managersThread) throw "Wrong thread : create";
-		return lime_curl_create(request);
+		return lime_curl_create (request);
 	}
 
-	public function updateLoader(handle : Dynamic, loader : URLLoader) : Void {
+	public function updateLoader (handle : Dynamic, loader : URLLoader) : Void {
 		if (Thread.current()!=managersThread) throw "Wrong thread : updateLoader";
-		lime_curl_update_loader(handle, loader);
+		lime_curl_update_loader (handle, loader);
 	}
 
-	public function getCode(handle : Dynamic) : Int {
+	public function getCode (handle : Dynamic) : Int {
 		if (Thread.current()!=managersThread) throw "Wrong thread : getCode";
-		return lime_curl_get_code(handle);
+		return lime_curl_get_code (handle);
 	}
 
-	public function getErrorMessage(handle : Dynamic) : String {
+	public function getErrorMessage (handle : Dynamic) : String {
 		if (Thread.current()!=managersThread) throw "Wrong thread : getErrorMessage";
-		return lime_curl_get_error_message(handle);
+		return lime_curl_get_error_message (handle);
 	}
 
-	public function getData(handle : Dynamic) : ByteArray {
+	public function getData (handle : Dynamic) : ByteArray {
 		if (Thread.current()!=managersThread) throw "Wrong thread : getData";
-		return lime_curl_get_data(handle);
+		return lime_curl_get_data (handle);
 	}
 
-	public function getCookies(handle : Dynamic) : Array<String> {
+	public function getCookies (handle : Dynamic) : Array<String> {
 		if (Thread.current()!=managersThread) throw "Wrong thread : getCookies";
-		return lime_curl_get_cookies(handle);
+		return lime_curl_get_cookies (handle);
 	}
 
-	public function getHeaders(handle : Dynamic) : Array<String> {
+	public function getHeaders (handle : Dynamic) : Array<String> {
 		if (Thread.current()!=managersThread) throw "Wrong thread : getHeaders";
-		return lime_curl_get_headers(handle);
+		return lime_curl_get_headers (handle);
 	}
 
 	// Native Methods
@@ -136,79 +136,76 @@ private class URLLoadersManager {
 }
 
 class URLLoader extends EventDispatcher {
-	
-	
+
+
 	public var bytesLoaded (default, null):Int;
 	public var bytesTotal (default, null):Int;
 	public var data:Dynamic;
 	public var dataFormat:URLLoaderDataFormat;
-	
-	//@:noCompletion private static var activeLoaders = new List<URLLoader> ();
+
 	@:noCompletion private static inline var urlInvalid = 0;
 	@:noCompletion private static inline var urlInit = 1;
-	
+
 	@:allow(openfl._legacy.net.URLLoadersManager)
 	@:noCompletion private static inline var urlLoading = 2;
-	
+
 	@:noCompletion private static inline var urlComplete = 3;
 	@:noCompletion private static inline var urlError = 4;
-	
+
 	@:allow(openfl._legacy.net.URLLoadersManager)
 	@:noCompletion private var state:Int;
-	
+
 	@:noCompletion private var __handle:Dynamic;
 	@:noCompletion public var __onComplete:Dynamic -> Bool;
-	
-	
+
+
 	public function new (request:URLRequest = null) {
-		
+
 		super ();
-		
+
 		__handle = 0;
 		bytesLoaded = 0;
 		bytesTotal = -1;
 		state = urlInvalid;
 		dataFormat = URLLoaderDataFormat.TEXT;
-		
+
 		if (request != null) {
-			
+
 			load (request);
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public function close ():Void {
-		
-		
-		
+
+
+
 	}
-	
-	
+
+
 	private function dispatchHTTPStatus (code:Int):Void {
-		
+
 		var event = new HTTPStatusEvent (HTTPStatusEvent.HTTP_STATUS, false, false, code);
-		//var headers:Array<String> = lime_curl_get_headers (__handle);
 		var headers = URLLoadersManager.getInstance().getHeaders(__handle);
-		
+
 		for (header in headers) {
-			
+
 			var index = header.indexOf(": ");
 			if (index > 0) {
-				
+
 				event.responseHeaders.push (new URLRequestHeader (header.substr (0, index), header.substr (index + 2, header.length - index - 4)));
-				
+
 			}
-			
+
 		}
-		
-		//dispatchEvent (event);
+
 		enqueueEvent(this, event);
-		
+
 	}
-	
-	
+
+
 	public function getCookies ():Array<String> {
 		trace("GET COOKIES");
 		return [];
@@ -216,95 +213,93 @@ class URLLoader extends EventDispatcher {
 		return lime_curl_get_cookies (__handle);
 		*/
 	}
-	
-	
+
+
 	public static function hasActive ():Bool {
-		
+
 		return !URLLoadersManager.getInstance().activeLoadersIsEmpty();
-		
+
 	}
-	
-	
+
+
 	public static function initialize (caCertFilePath:String):Void {
 		trace("INITIALIZE");
 		/*
 		lime_curl_initialize (caCertFilePath);
 		*/
-		
+
 	}
-	
-	
+
+
 	public function load (request:URLRequest):Void {
-		
+
 		URLLoadersManager.getInstance().enqueueLoad(this, request);
 
 	}
 
 	@:allow(openfl._legacy.net.URLLoadersManager)
-	function loadInCURLThread(request:URLRequest):Void {
+	function loadInCURLThread (request:URLRequest):Void {
 
 		state = urlInit;
-		
+
 		var pref = request.url.substr (0, 7);
 		if (pref != "http://" && pref != "https:/") {
-			
+
 			try {
-				
+
 				var bytes = ByteArray.readFile (request.url);
-				
+
 				if (bytes == null) {
-					
+
 					throw ("Could not open file \"" + request.url + "\"");
-					
+
 				}
-				
+
 				switch (dataFormat) {
-					
+
 					case TEXT: data = bytes.asString ();
 					case VARIABLES: data = new URLVariables(bytes.asString());
 					default: data = bytes;
-					
+
 				}
-				
+
 			} catch (e:Dynamic) {
-				
+
 				onError (e);
 				return;
-				
+
 			}
-			
+
 			__dataComplete ();
-			
+
 		} else {
-			
+
 			request.__prepare ();
 
-			//__handle = lime_curl_create (request);
 			__handle = URLLoadersManager.getInstance().create(request);
-			
+
 			if (__handle == null) {
-				
+
 				onError ("Could not open URL");
-				
+
 			} else {
-				
+
 				URLLoadersManager.getInstance().getActiveLoaders().push (this);
-				
+
 			}
-			
+
 		}
 
 	}
-	
-	
+
+
 	private function onError (msg:String):Void {
-		
+
 		URLLoadersManager.getInstance().getActiveLoaders().remove (this);
-		//dispatchEvent (new IOErrorEvent (IOErrorEvent.IO_ERROR, true, false, msg));
 		enqueueEvent(this, new IOErrorEvent (IOErrorEvent.IO_ERROR, true, false, msg));
-		
+
 	}
-	
+
 	@:allow(openfl._legacy.net.URLLoadersManager)
 	private function update():Void {
 
@@ -312,27 +307,22 @@ class URLLoader extends EventDispatcher {
 
 			var old_loaded = bytesLoaded;
 			var old_total = bytesTotal;
-			//lime_curl_update_loader (__handle, this);
 			URLLoadersManager.getInstance().updateLoader(__handle, this);
 
 			if (old_total < 0 && bytesTotal > 0) {
-				//dispatchEvent (new Event (Event.OPEN));
 				enqueueEvent(this, new Event (Event.OPEN));
 			}
-			
+
 			if (bytesTotal > 0 && bytesLoaded != old_loaded) {
-				//dispatchEvent (new ProgressEvent (ProgressEvent.PROGRESS, false, false, bytesLoaded, bytesTotal));
 				var evt = new ProgressEvent (ProgressEvent.PROGRESS, false, false, bytesLoaded, bytesTotal);
 				enqueueEvent(this, evt);
 			}
-			
-			//var code:Int = lime_curl_get_code (__handle);
+
 			var code = URLLoadersManager.getInstance().getCode(__handle);
-			
+
 			if (state == urlComplete) {
 
 				dispatchHTTPStatus (code);
-				//var bytes:ByteArray = lime_curl_get_data (__handle);
 				var bytes = URLLoadersManager.getInstance().getData(__handle);
 				switch (dataFormat) {
 					case TEXT, VARIABLES:
@@ -342,61 +332,57 @@ class URLLoader extends EventDispatcher {
 				}
 
 				if (code < 400) {
-					__dataComplete ();					
+					__dataComplete ();
 				} else {
 					var event = new IOErrorEvent (IOErrorEvent.IO_ERROR, true, false, data, code);
 					__handle = null;
-					//dispatchEvent (event);
 					enqueueEvent(this, event);
 				}
-				
+
 			} else if (state == urlError) {
 				dispatchHTTPStatus (code);
 				var errorMessage = URLLoadersManager.getInstance().getErrorMessage(__handle);
 				var event = new IOErrorEvent (IOErrorEvent.IO_ERROR, true, false, errorMessage, code);
 				__handle = null;
-				//dispatchEvent (event);
 				enqueueEvent(this, event);
 			}
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	@:noCompletion private function __dataComplete ():Void {
-		
+
 		URLLoadersManager.getInstance().getActiveLoaders().remove (this);
-		
+
 		if (__onComplete != null) {
-			
+
 			if (__onComplete (data)) {
-				
-				//dispatchEvent (new Event (Event.COMPLETE));
+
 				enqueueEvent(this, new Event (Event.COMPLETE));
-				
+
 			} else {
-				
+
 				__dispatchIOErrorEvent ();
-				
+
 			}
-			
+
 		} else {
-			
-			//dispatchEvent (new Event (Event.COMPLETE));
+
 			enqueueEvent(this, new Event (Event.COMPLETE));
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	@:noCompletion public static function __loadPending ():Bool {
-		
+
 		return !URLLoadersManager.getInstance().activeLoadersIsEmpty();
-		
+
 	}
-	
+
 	static var eventsQueue : Array<{loader : URLLoader, event : Event}> = [];
 
 	static function enqueueEvent(loader : URLLoader, event : Event) {
