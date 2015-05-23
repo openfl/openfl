1f1e24ba8a4cfd463eaff7e29fa5d5897d0232dc
Joshua Granick <jgranick@users.noreply.github.com>
2015-05-22 19:55:31 -0700

    Set event.target even if there are no listeners

--- openfl/events/EventDispatcher.hx
+++ openfl/events/EventDispatcher.hx
@@ -1,6 +1,7 @@
 package openfl.events; #if !flash #if !openfl_legacy
 
 
+import gfx.reels.Reel;
 import openfl.events.EventPhase;
 import openfl.events.IEventDispatcher;
 
@@ -253,14 +254,7 @@ class EventDispatcher implements IEventDispatcher {
 	 */
 	public function dispatchEvent (event:Event):Bool {
 		
-		if (__eventMap == null || event == null) return false;
-		
-		var type = event.type;
-		
-		var list = __eventMap.get (type);
-		if (list == null) return false;
-		
-		__dispatching.set (type, true);
+		if (event == null) return false;
 		
 		if (event.target == null) {
 			
@@ -276,6 +270,14 @@ class EventDispatcher implements IEventDispatcher {
 			
 		}
 		
+		if (__eventMap == null) return false;
+		
+		var type = event.type;
+		
+		var list = __eventMap.get (type);
+		if (list == null) return false;
+		
+		__dispatching.set (type, true);
 		event.currentTarget = this;
 		
 		var capture = (event.eventPhase == EventPhase.CAPTURING_PHASE);
