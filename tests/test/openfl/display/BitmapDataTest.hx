7a6ed36b552c382b2a19b9ff7139ef8c5cdd7397
Joshua Granick <jgranick@users.noreply.github.com>
2015-07-20 22:37:17 -0700

    Test fix

--- tests/test/openfl/display/BitmapDataTest.hx
+++ tests/test/openfl/display/BitmapDataTest.hx
@@ -268,11 +268,14 @@ class BitmapDataTest {
 		
 		bitmapData.copyChannel (bitmapData2, bitmapData2.rect, new Point (), BitmapDataChannel.ALPHA, BitmapDataChannel.ALPHA);
 		
+		#if (!flash && !lime_legacy)
 		if (@:privateAccess bitmapData.__image.premultiplied) {
 			
 			Assert.areEqual (hex (0x22F7F7F7), hex (bitmapData.getPixel32 (0, 0)));
 			
-		} else {
+		} else
+		#end
+		{
 			
 			Assert.areEqual (hex (0x22FFFFFF), hex (bitmapData.getPixel32 (0, 0)));
 			
