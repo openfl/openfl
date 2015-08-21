ca506cc743d8543ca5d0e65d3d263bd051eeb9c3
Joshua Granick <jgranick@users.noreply.github.com>
2015-08-21 10:06:03 -0700

    Update docs

--- docs/ImportAll.hx
+++ docs/ImportAll.hx
@@ -1,6 +1,10 @@
 package;
 
 
+import openfl.desktop.Clipboard;
+import openfl.desktop.ClipboardFormats;
+import openfl.desktop.ClipboardTransferMode;
+import openfl.display.Application;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
 import openfl.display.BitmapDataChannel;
@@ -11,6 +15,7 @@ import openfl.display.DisplayObject;
 import openfl.display.DisplayObjectContainer;
 import openfl.display.DOMSprite;
 import openfl.display.FPS;
+import openfl.display.FrameLabel;
 import openfl.display.GradientType;
 import openfl.display.Graphics;
 import openfl.display.GraphicsBitmapFill;
@@ -28,15 +33,15 @@ import openfl.display.IGraphicsPath;
 import openfl.display.IGraphicsStroke;
 import openfl.display.InteractiveObject;
 import openfl.display.InterpolationMethod;
-import openfl.display.JPEGEncoderOptions;
 import openfl.display.JointStyle;
+import openfl.display.JPEGEncoderOptions;
 import openfl.display.LineScaleMode;
 import openfl.display.Loader;
 import openfl.display.LoaderInfo;
 import openfl.display.MovieClip;
 import openfl.display.OpenGLView;
-import openfl.display.PNGEncoderOptions;
 import openfl.display.PixelSnapping;
+import openfl.display.PNGEncoderOptions;
 import openfl.display.Preloader;
 import openfl.display.Shape;
 import openfl.display.SimpleButton;
@@ -50,6 +55,7 @@ import openfl.display.StageQuality;
 import openfl.display.StageScaleMode;
 import openfl.display.Tilesheet;
 import openfl.display.TriangleCulling;
+import openfl.display.Window;
 import openfl.display3D.textures.CubeTexture;
 import openfl.display3D.textures.RectangleTexture;
 import openfl.display3D.textures.Texture;
@@ -78,16 +84,19 @@ import openfl.errors.ArgumentError;
 import openfl.errors.EOFError;
 import openfl.errors.Error;
 import openfl.errors.IllegalOperationError;
+import openfl.errors.IOError;
 import openfl.errors.RangeError;
 import openfl.errors.SecurityError;
 import openfl.errors.TypeError;
 import openfl.events.AccelerometerEvent;
 import openfl.events.AsyncErrorEvent;
+import openfl.events.DataEvent;
 import openfl.events.ErrorEvent;
 import openfl.events.Event;
 import openfl.events.EventDispatcher;
 import openfl.events.EventPhase;
 import openfl.events.FocusEvent;
+import openfl.events.GameInputEvent;
 import openfl.events.HTTPStatusEvent;
 import openfl.events.IEventDispatcher;
 import openfl.events.IOErrorEvent;
@@ -151,12 +160,15 @@ import openfl.net.URLRequestHeader;
 import openfl.net.URLRequestMethod;
 import openfl.net.URLVariables;
 import openfl.net.XMLSocket;
+import openfl.profiler.Telemetry;
 import openfl.sensors.Accelerometer;
 import openfl.system.ApplicationDomain;
 import openfl.system.Capabilities;
 import openfl.system.LoaderContext;
+import openfl.system.Security;
 import openfl.system.SecurityDomain;
 import openfl.system.System;
+import openfl.system.TouchscreenType;
 import openfl.text.AntiAliasType;
 import openfl.text.Font;
 import openfl.text.FontStyle;
@@ -168,6 +180,9 @@ import openfl.text.TextFieldType;
 import openfl.text.TextFormat;
 import openfl.text.TextFormatAlign;
 import openfl.text.TextLineMetrics;
+import openfl.ui.GameInput;
+import openfl.ui.GameInputControl;
+import openfl.ui.GameInputDevice;
 import openfl.ui.KeyLocation;
 import openfl.ui.Keyboard;
 import openfl.ui.Mouse;
@@ -196,6 +211,7 @@ import openfl.Vector;
 import lime.app.Application;
 import lime.app.Config;
 import lime.app.Event;
+import lime.app.IModule;
 import lime.app.Module;
 import lime.app.Preloader;
 import lime.audio.openal.AL;
@@ -211,6 +227,24 @@ import lime.audio.AudioSource;
 import lime.audio.FlashAudioContext;
 import lime.audio.HTML5AudioContext;
 import lime.audio.WebAudioContext;
+import lime.graphics.cairo.Cairo;
+import lime.graphics.cairo.CairoAntialias;
+import lime.graphics.cairo.CairoContent;
+import lime.graphics.cairo.CairoExtend;
+import lime.graphics.cairo.CairoFillRule;
+import lime.graphics.cairo.CairoFilter;
+import lime.graphics.cairo.CairoFont;
+import lime.graphics.cairo.CairoFontOptions;
+import lime.graphics.cairo.CairoFormat;
+import lime.graphics.cairo.CairoHintMetrics;
+import lime.graphics.cairo.CairoImageSurface;
+import lime.graphics.cairo.CairoLineCap;
+import lime.graphics.cairo.CairoLineJoin;
+import lime.graphics.cairo.CairoOperator;
+import lime.graphics.cairo.CairoPattern;
+import lime.graphics.cairo.CairoStatus;
+import lime.graphics.cairo.CairoSubpixelOrder;
+import lime.graphics.cairo.CairoSurface;
 import lime.graphics.format.BMP;
 import lime.graphics.format.JPEG;
 import lime.graphics.format.PNG;
@@ -228,15 +262,22 @@ import lime.graphics.opengl.GLTexture;
 import lime.graphics.opengl.GLUniformLocation;
 import lime.graphics.utils.ImageCanvasUtil;
 import lime.graphics.utils.ImageDataUtil;
+import lime.graphics.CairoRenderContext;
 import lime.graphics.CanvasRenderContext;
 import lime.graphics.DOMRenderContext;
 import lime.graphics.FlashRenderContext;
 import lime.graphics.GLRenderContext;
 import lime.graphics.Image;
 import lime.graphics.ImageBuffer;
+import lime.graphics.ImageChannel;
 import lime.graphics.ImageType;
+import lime.graphics.PixelFormat;
 import lime.graphics.RenderContext;
 import lime.graphics.Renderer;
+import lime.graphics.RendererType;
+import lime.math.color.ARGB;
+import lime.math.color.BGRA;
+import lime.math.color.RGBA;
 import lime.math.ColorMatrix;
 import lime.math.Matrix3;
 import lime.math.Matrix4;
@@ -255,6 +296,8 @@ import lime.net.oauth.OAuthRequest;
 import lime.net.oauth.OAuthSignatureMethod;
 import lime.net.oauth.OAuthToken;
 import lime.net.oauth.OAuthVersion;
+//import lime.net.NetConnection;
+//import lime.net.NetConnectionManager;
 import lime.net.URIParser;
 import lime.net.URLLoader;
 import lime.net.URLLoaderDataFormat;
@@ -262,11 +305,7 @@ import lime.net.URLRequest;
 import lime.net.URLRequestHeader;
 import lime.net.URLRequestMethod;
 import lime.net.URLVariables;
-
-
 #if (windows || mac || linux || neko)
-
-
 import lime.project.ApplicationData;
 import lime.project.Architecture;
 import lime.project.Asset;
@@ -289,12 +328,15 @@ import lime.project.PlatformType;
 import lime.project.ProjectXMLParser;
 import lime.project.SplashScreen;
 import lime.project.WindowData;
-
-
 #end
-
-
+import lime.system.BackgroundWorker;
+import lime.system.Clipboard;
+import lime.system.Display;
+import lime.system.DisplayMode;
+import lime.system.Endian;
+import lime.system.JNI;
 import lime.system.System;
+import lime.system.ThreadPool;
 import lime.text.Font;
 import lime.text.Glyph;
 import lime.text.GlyphMetrics;
@@ -302,11 +344,7 @@ import lime.text.GlyphPosition;
 import lime.text.TextDirection;
 import lime.text.TextLayout;
 import lime.text.TextScript;
-
-
 #if (windows || mac || linux || neko)
-
-
 import lime.tools.helpers.AIRHelper;
 import lime.tools.helpers.AndroidHelper;
 import lime.tools.helpers.AntHelper;
@@ -348,11 +386,7 @@ import lime.tools.platforms.MacPlatform;
 import lime.tools.platforms.TizenPlatform;
 import lime.tools.platforms.WebOSPlatform;
 import lime.tools.platforms.WindowsPlatform;
-
-
 #end
-
-
 import lime.ui.Gamepad;
 import lime.ui.GamepadAxis;
 import lime.ui.GamepadButton;
@@ -360,21 +394,27 @@ import lime.ui.KeyCode;
 import lime.ui.KeyModifier;
 import lime.ui.Mouse;
 import lime.ui.MouseCursor;
+import lime.ui.Touch;
 import lime.ui.Window;
 import lime.utils.ArrayBuffer;
 import lime.utils.ArrayBufferView;
 import lime.utils.ByteArray;
+import lime.utils.DataView;
 import lime.utils.Float32Array;
+import lime.utils.Float64Array;
 import lime.utils.GLUtils;
 import lime.utils.IDataInput;
 import lime.utils.IMemoryRange;
 import lime.utils.Int16Array;
 import lime.utils.Int32Array;
 import lime.utils.Int8Array;
-import lime.system.JNI;
+import lime.utils.Log;
+import lime.utils.LZMA;
 import lime.utils.UInt16Array;
 import lime.utils.UInt32Array;
 import lime.utils.UInt8Array;
+import lime.utils.UInt8ClampedArray;
+//import lime.vm.NekoVM;
 import lime.Assets;
 
 
