877bc21963556eeb2c4da56e155fe455d388c685
MrCdK <codecdakiller@gmail.com>
2015-09-10 01:32:32 +0200

    fix set_width and set_height

--- openfl/display/DisplayObject.hx
+++ openfl/display/DisplayObject.hx
@@ -1050,10 +1050,17 @@ class DisplayObject extends EventDispatcher implements IBitmapDrawable {
 	}
 	
 	
-	@:noCompletion private inline function __getLocalBounds (rect:Rectangle):Void {
+	@:noCompletion private inline function __getLocalBounds (rect:Rectangle, ?withLocalTransform:Bool = true):Void {
 		
-		var m = __localTransform.clone();
-		m.__translateTransformed ( -m.tx, -m.ty);
+		var m:Matrix;
+		if (withLocalTransform) {
+			m = __localTransform.clone();
+			m.__translateTransformed ( -m.tx, -m.ty);
+		} else { 
+			m = Matrix.__temp;
+			m.identity();
+		}
+		
 		__getBounds (rect, m);
 		
 	}
@@ -1579,7 +1586,7 @@ class DisplayObject extends EventDispatcher implements IBitmapDrawable {
 	@:noCompletion private function set_height (value:Float):Float {
 		
 		var bounds = new Rectangle ();
-		__getLocalBounds (bounds);
+		__getLocalBounds (bounds, false);
 		
 		if (value != bounds.height) {
 			
@@ -1882,7 +1889,7 @@ class DisplayObject extends EventDispatcher implements IBitmapDrawable {
 	@:noCompletion private function set_width (value:Float):Float {
 		
 		var bounds = new Rectangle ();
-		__getLocalBounds (bounds);
+		__getLocalBounds (bounds, false);
 		
 		if (value != bounds.width) {
 			
