d21371bfbba10180cfab49186e796a8b11665bad
Joshua Granick <jgranick@users.noreply.github.com>
2015-09-16 17:44:37 -0700

    Add extra management functions

--- openfl/display/Tilemap.hx
+++ openfl/display/Tilemap.hx
@@ -19,6 +19,7 @@ class Tilemap extends #if !flash DisplayObject #else Bitmap implements IDisplayO
 	public var allowRotation:Bool;
 	public var allowScale:Bool;
 	public var allowTransform:Bool;
+	public var numLayers (default, null):Int;
 	
 	#if !flash
 	public var smoothing:Bool;
@@ -39,31 +40,86 @@ class Tilemap extends #if !flash DisplayObject #else Bitmap implements IDisplayO
 		FlashRenderer.register (this);
 		#end
 		
+		__layers = new Array ();
+		numLayers = 0;
 		smoothing = true;
 		
 	}
 	
 	
-	public function addLayer (layer:TilemapLayer):Void {
-		
-		if (__layers == null) {
-			
-			__layers = new Array ();
-			
-		}
+	public function addLayer (layer:TilemapLayer):TilemapLayer {
 		
 		__layers.push (layer);
+		numLayers++;
+		
+		return layer;
 		
 	}
 	
 	
-	public function removeLayer (layer:TilemapLayer):Void {
+	public function addLayerAt (layer:TilemapLayer, index:Int):TilemapLayer {
 		
-		if (__layers != null) {
+		__layers.remove (layer);
+		__layers.insert (index, layer);
+		numLayers = __layers.length;
+		
+		return layer;
+		
+	}
+	
+	
+	public function contains (layer:TilemapLayer):Bool {
+		
+		return (__layers.indexOf (layer) > -1);
+		
+	}
+	
+	
+	public function getLayerAt (index:Int):TilemapLayer {
+		
+		if (index >= 0 && index < numLayers) {
 			
-			__layers.remove (layer);
+			return __layers[index];
 			
 		}
+		
+		return null;
+		
+	}
+	
+	
+	public function getLayerIndex (layer:TilemapLayer):Int {
+		
+		for (i in 0...__layers.length) {
+			
+			if (__layers[i] == layer) return i;
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	
+	public function removeLayer (layer:TilemapLayer):TilemapLayer {
+		
+		__layers.remove (layer);
+		numLayers = __layers.length;
+		
+		return layer;
+		
+	}
+	
+	
+	public function removeLayerAt (index:Int):TilemapLayer {
+		
+		if (index >= 0 && index < numLayers) {
+			
+			return removeLayer (__layers[index]);
+			
+		}
+		
+		return null;
 		
 	}
 	
--- openfl/display/TilemapLayer.hx
+++ openfl/display/TilemapLayer.hx
@@ -10,6 +10,7 @@ class TilemapLayer {
 	
 	// TODO: Handle dirty flag
 	
+	public var numTiles (default, null):Int;
 	public var tileset:Tileset;
 	
 	private var __buffer:GLBuffer;
@@ -23,22 +24,98 @@ class TilemapLayer {
 		this.tileset = tileset;
 		
 		__tiles = new Array ();
+		numTiles = 0;
 		
 	}
 	
 	
-	public function addTile (tile:Tile):Void {
+	public function addTile (tile:Tile):Tile {
 		
 		__tiles.push (tile);
 		__dirty = true;
+		numTiles++;
+		
+		return tile;
 		
 	}
 	
 	
-	public function removeTile (tile:Tile):Void {
+	public function addTiles (tiles:Array<Tile>):Array<Tile> {
+		
+		__tiles = __tiles.concat (tiles);
+		__dirty = true;
+		numTiles = __tiles.length;
+		
+		return tiles;
+		
+	}
+	
+	
+	public function addTileAt (tile:Tile, index:Int):Tile {
+		
+		__tiles.remove (tile);
+		__tiles.insert (index, tile);
+		__dirty = true;
+		numTiles = __tiles.length;
+		
+		return tile;
+		
+	}
+	
+	
+	public function contains (tile:Tile):Bool {
+		
+		return (__tiles.indexOf (tile) > -1);
+		
+	}
+	
+	
+	public function getTileAt (index:Int):Tile {
+		
+		if (index >= 0 && index < numTiles) {
+			
+			return __tiles[index];
+			
+		}
+		
+		return null;
+		
+	}
+	
+	
+	public function getTileIndex (tile:Tile):Int {
+		
+		for (i in 0...__tiles.length) {
+			
+			if (__tiles[i] == tile) return i;
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	
+	public function removeTile (tile:Tile):Tile {
 		
 		__tiles.remove (tile);
 		__dirty = true;
+		numTiles = __tiles.length;
+		
+		return tile;
+		
+	}
+	
+	
+	public function removeTileAt (index:Int):Tile {
+		
+		if (index >= 0 && index < numTiles) {
+			
+			return removeTile (__tiles[index]);
+			
+		}
+		
+		return null;
 		
 	}
 	
