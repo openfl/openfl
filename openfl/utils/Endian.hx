8e5e3fd8d22737440cf4b8cd56e0de9c9e115c94
Joshua Granick <jgranick@users.noreply.github.com>
2015-12-12 18:05:18 -0800

    Remove type on Endian values

--- openfl/utils/Endian.hx
+++ openfl/utils/Endian.hx
@@ -3,7 +3,7 @@ package openfl.utils;
 
 @:enum abstract Endian(String) from String to String {
 	
-	public var BIG_ENDIAN:String = "bigEndian";
-	public var LITTLE_ENDIAN:String = "littleEndian";
+	public var BIG_ENDIAN = "bigEndian";
+	public var LITTLE_ENDIAN = "littleEndian";
 	
 }
