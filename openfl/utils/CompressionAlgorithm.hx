5f66964c2f726c18c6573fe8ac743ab954584b50
Lars A. Doucet <lars.doucet@gmail.com>
2015-12-30 13:14:23 -0600

    compile fix

--- openfl/utils/CompressionAlgorithm.hx
+++ openfl/utils/CompressionAlgorithm.hx
@@ -1,38 +1,12 @@
 package openfl.utils; #if !openfl_legacy
 
 
-@:enum abstract CompressionAlgorithm(Null<Int>) {
+@:enum abstract CompressionAlgorithm(String) from String to String {
 	
-	public var DEFLATE = 0;
-	//GZIP;
-	public var LZMA = 1;
-	public var ZLIB = 2;
-	
-	@:from private static function fromString (value:String):CompressionAlgorithm {
-		
-		return switch (value) {
-			
-			case "deflate": DEFLATE;
-			case "lzma": LZMA;
-			case "zlib": ZLIB;
-			default: null;
-			
-		}
-		
-	}
-	
-	@:to private static function toString (value:Int):String {
-		
-		return switch (value) {
-			
-			case CompressionAlgorithm.DEFLATE: "deflate";
-			case CompressionAlgorithm.LZMA: "lzma";
-			case CompressionAlgorithm.ZLIB: "zlib";
-			default: null;
-			
-		}
-		
-	}
+	public var DEFLATE = "deflate";
+	public var GZIP = "gzip";
+	public var LZMA = "lzma";
+	public var ZLIB = "zlib";
 	
 }
 
