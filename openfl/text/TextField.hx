f4dcd9a2c856a905c2e12fa20a91585c696840c8
Lars Doucet <lars.doucet@gmail.com>
2016-01-06 09:41:56 -0600

    remove errant function that somehow got in this branch

--- openfl/text/TextField.hx
+++ openfl/text/TextField.hx
@@ -126,7 +126,7 @@ class TextField extends InteractiveObject {
 		
 	}
 	
-	@:access(openfl._internal.text.TextEngine)
+	
 	public function getCharBoundaries (charIndex:Int):Rectangle {
 		
 		if (charIndex < 0 || charIndex > __textEngine.text.length - 1) return null;
@@ -137,10 +137,15 @@ class TextField extends InteractiveObject {
 			
 			if (charIndex >= group.startIndex && charIndex <= group.endIndex) {
 				
-				var x = group.offsetX + __textEngine.getFormattedTextWidth(text.substr(group.startIndex, (charIndex-group.startIndex)), group.format);
-				var width = __textEngine.getFormattedTextWidth(text.charAt(charIndex), group.format);
+				var x = group.offsetX;
 				
-				return new Rectangle (x, group.offsetY, width, group.ascent + group.descent);
+				for (i in 0...(charIndex - group.startIndex)) {
+					
+					x += group.advances[i];
+					
+				}
+				
+				return new Rectangle (x, group.offsetY, group.advances[charIndex - group.startIndex], group.ascent + group.descent);
 				
 			}
 			
