b5e6b4e6bc79f37dbb361852dc32b43977078bf4
Kaspars <kasparsj@gmail.com>
2016-03-14 17:01:10 +0200

    fix for color attribute

--- openfl/text/TextField.hx
+++ openfl/text/TextField.hx
@@ -1271,7 +1271,7 @@ class TextField extends InteractiveObject {
 										format.font = faceEreg.matched(1);
 									}
 
-									var colorEreg = ~/color="([^"]+)/i;
+									var colorEreg = ~/color="#([^"]+)/i;
 									if (colorEreg.match(segment)) {
 										format.color = Std.parseInt ("0x" + colorEreg.matched(1));
 									}
