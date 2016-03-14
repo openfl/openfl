f3d92e1e130597fac0caac63d40537cb5f5f5687
Kaspars <kasparsj@gmail.com>
2016-03-14 17:16:54 +0200

    bug fix

--- openfl/text/TextField.hx
+++ openfl/text/TextField.hx
@@ -1280,7 +1280,7 @@ class TextField extends InteractiveObject {
 
 							formatStack.push(format);
 
-							if (start < (segment.length - 1)) {
+							if (start < segment.length) {
 
 								sub = segment.substring (start);
 								__textEngine.textFormatRanges.push (new TextFormatRange (format, value.length, value.length + sub.length));
