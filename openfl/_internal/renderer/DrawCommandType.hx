6e0b8a58ff4ab1a678883274186dfc3f898fdc0b
James Gray <james@james4k.com>
2016-07-22 15:58:35 -0500

    DrawCommandType enum abstract

--- openfl/_internal/renderer/DrawCommandType.hx
+++ openfl/_internal/renderer/DrawCommandType.hx
@@ -1,26 +1,26 @@
 package openfl._internal.renderer;
 
 
-enum DrawCommandType {
+@:enum abstract DrawCommandType (Int) {
 	
-	BEGIN_BITMAP_FILL;
-	BEGIN_FILL;
-	BEGIN_GRADIENT_FILL;
-	CUBIC_CURVE_TO;
-	CURVE_TO;
-	DRAW_CIRCLE;
-	DRAW_ELLIPSE;
-	DRAW_RECT;
-	DRAW_ROUND_RECT;
-	DRAW_TILES;
-	DRAW_TRIANGLES;
-	END_FILL;
-	LINE_BITMAP_STYLE;
-	LINE_GRADIENT_STYLE;
-	LINE_STYLE;
-	LINE_TO;
-	MOVE_TO;
-	OVERRIDE_MATRIX;
-	UNKNOWN;
+	var BEGIN_BITMAP_FILL   =  0;
+	var BEGIN_FILL          =  1;
+	var BEGIN_GRADIENT_FILL =  2;
+	var CUBIC_CURVE_TO      =  3;
+	var CURVE_TO            =  4;
+	var DRAW_CIRCLE         =  5;
+	var DRAW_ELLIPSE        =  6;
+	var DRAW_RECT           =  7;
+	var DRAW_ROUND_RECT     =  8;
+	var DRAW_TILES          =  9;
+	var DRAW_TRIANGLES      = 10;
+	var END_FILL            = 11;
+	var LINE_BITMAP_STYLE   = 12;
+	var LINE_GRADIENT_STYLE = 13;
+	var LINE_STYLE          = 14;
+	var LINE_TO             = 15;
+	var MOVE_TO             = 16;
+	var OVERRIDE_MATRIX     = 17;
+	var UNKNOWN             = 18;
 	
 }
