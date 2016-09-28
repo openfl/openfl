018ff5e748997c7178a182797b4c4829a8e2f913
Joshua Granick <jgranick@users.noreply.github.com>
2016-09-28 11:33:02 -0700

    Improve use from HXML builds

--- openfl/Lib.hx
+++ openfl/Lib.hx
@@ -188,14 +188,40 @@ import haxe.macro.Context;
 class Lib {
 	
 	
+	public static function extraParams ():Void {
+		
+		if (!Context.defined ("tools")) {
+			
+			if (Context.defined ("display")) {
+				
+				includeExterns ();
+				
+			}
+			
+			if (!Context.defined ("flash")) {
+				
+				Compiler.allowPackage ("flash");
+				Compiler.define ("swf-version", "22.0");
+				
+			}
+			
+		}
+		
+	}
+	
+	
 	public static function includeExterns ():Void {
 		
-		var childPath = Context.resolvePath ("extern/openfl");
+		var childPath = Context.resolvePath ("externs/core");
 		
 		var parts = StringTools.replace (childPath, "\\", "/").split ("/");
 		parts.pop ();
 		
-		Compiler.addClassPath (parts.join ("/"));
+		var externsPath = parts.join ("/");
+		
+		Compiler.addClassPath (externsPath + "/core/openfl");
+		Compiler.addClassPath (externsPath + "/core/flash");
+		Compiler.addClassPath (externsPath + "/extras");
 		
 	}
 	
