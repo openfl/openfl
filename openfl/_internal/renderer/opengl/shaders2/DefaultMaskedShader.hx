6e3e5b1548ef87c68412b0e22533a06dbd81effe
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2016-09-29 13:26:37 +0200

    Reuse DefAttrib/Uniform/Varying in masked shader

--- openfl/_internal/renderer/opengl/shaders2/DefaultMaskedShader.hx
+++ openfl/_internal/renderer/opengl/shaders2/DefaultMaskedShader.hx
@@ -1,34 +1,34 @@
 package openfl._internal.renderer.opengl.shaders2;
 
 import lime.graphics.GLRenderContext;
-
+import openfl._internal.renderer.opengl.shaders2.DefaultShader;
 
 class DefaultMaskedShader extends Shader {
 
 	public static var VERTEX_SRC(default, null) = [
-			'attribute vec2 ${MaskedAttrib.Position};',
-			'attribute vec2 ${MaskedAttrib.TexCoord};',
-			'attribute vec4 ${MaskedAttrib.Color};',
+			'attribute vec2 ${DefAttrib.Position};',
+			'attribute vec2 ${DefAttrib.TexCoord};',
+			'attribute vec4 ${DefAttrib.Color};',
 
-			'uniform mat3 ${MaskedUniform.ProjectionMatrix};',
+			'uniform mat3 ${DefUniform.ProjectionMatrix};',
 			'uniform mat3 ${MaskedUniform.MaskMatrix};',
-			'uniform bool ${MaskedUniform.UseColorTransform};',
+			'uniform bool ${DefUniform.UseColorTransform};',
 
-			'varying vec2 ${MaskedVarying.TexCoord};',
+			'varying vec2 ${DefVarying.TexCoord};',
 			'varying vec2 ${MaskedVarying.MaskTexCoord};',
-			'varying vec4 ${MaskedVarying.Color};',
+			'varying vec4 ${DefVarying.Color};',
 
 			'void main(void) {',
-			'   gl_Position = vec4((${MaskedUniform.ProjectionMatrix} * vec3(${MaskedAttrib.Position}, 1.0)).xy, 0.0, 1.0);',
-			'   ${MaskedVarying.TexCoord} = ${MaskedAttrib.TexCoord};',
-			'   ${MaskedVarying.MaskTexCoord} = (${MaskedUniform.MaskMatrix} * vec3(${MaskedAttrib.Position}, 1.0)).xy;',
-			'   if(${MaskedUniform.UseColorTransform})',
-			'   	${MaskedVarying.Color} = ${MaskedAttrib.Color};',
+			'   gl_Position = vec4((${DefUniform.ProjectionMatrix} * vec3(${DefAttrib.Position}, 1.0)).xy, 0.0, 1.0);',
+			'   ${DefVarying.TexCoord} = ${DefAttrib.TexCoord};',
+			'   ${MaskedVarying.MaskTexCoord} = (${MaskedUniform.MaskMatrix} * vec3(${DefAttrib.Position}, 1.0)).xy;',
+			'   if(${DefUniform.UseColorTransform})',
+			'   	${DefVarying.Color} = ${DefAttrib.Color};',
 			'   else',
 			#if (js && html5)
-			'   	${MaskedVarying.Color} = vec4(${MaskedAttrib.Color}.rgb * ${MaskedAttrib.Color}.a, ${MaskedAttrib.Color}.a);',
+			'   	${DefVarying.Color} = vec4(${DefAttrib.Color}.rgb * ${DefAttrib.Color}.a, ${DefAttrib.Color}.a);',
 			#else
-			'   	${MaskedVarying.Color} = vec4(${MaskedAttrib.Color}.bgr * ${MaskedAttrib.Color}.a, ${MaskedAttrib.Color}.a);',
+			'   	${DefVarying.Color} = vec4(${DefAttrib.Color}.bgr * ${DefAttrib.Color}.a, ${DefAttrib.Color}.a);',
 			#end
 			'}'
 		];
@@ -43,19 +43,19 @@ class DefaultMaskedShader extends Shader {
 			'precision lowp float;',
 			'#endif',
 
-			'uniform sampler2D ${MaskedUniform.Sampler};',
+			'uniform sampler2D ${DefUniform.Sampler};',
 			'uniform sampler2D ${MaskedUniform.MaskSampler};',
-			'uniform vec4 ${MaskedUniform.ColorMultiplier};',
-			'uniform vec4 ${MaskedUniform.ColorOffset};',
-			'uniform bool ${MaskedUniform.UseColorTransform};',
+			'uniform vec4 ${DefUniform.ColorMultiplier};',
+			'uniform vec4 ${DefUniform.ColorOffset};',
+			'uniform bool ${DefUniform.UseColorTransform};',
 			'uniform vec2 ${MaskedUniform.MaskUVScale};',
 
-			'varying vec2 ${MaskedVarying.TexCoord};',
+			'varying vec2 ${DefVarying.TexCoord};',
 			'varying vec2 ${MaskedVarying.MaskTexCoord};',
-			'varying vec4 ${MaskedVarying.Color};',
+			'varying vec4 ${DefVarying.Color};',
 
 			'vec4 colorTransform(const vec4 color, const vec4 tint, const vec4 multiplier, const vec4 offset) {',
-			'	if(!${MaskedUniform.UseColorTransform}) {',
+			'	if(!${DefUniform.UseColorTransform}) {',
 			'		return color * tint;',
 			'	}',
 
@@ -73,13 +73,13 @@ class DefaultMaskedShader extends Shader {
 			'}',
 
 			'void main(void) {',
-			'   vec4 tc = texture2D(${MaskedUniform.Sampler}, ${MaskedVarying.TexCoord});',
+			'   vec4 tc = texture2D(${DefUniform.Sampler}, ${DefVarying.TexCoord});',
 			'   vec4 mask = texture2D(${MaskedUniform.MaskSampler}, ${MaskedVarying.MaskTexCoord} * ${MaskedUniform.MaskUVScale});',
 			'   float inside = step( 0.0, ${MaskedVarying.MaskTexCoord}.x ) * step( -1.0, -${MaskedVarying.MaskTexCoord}.x );',
 			'   inside *= step( 0.0, ${MaskedVarying.MaskTexCoord}.y ) * step( -1.0, -${MaskedVarying.MaskTexCoord}.y );',
 			'	float maskAlpha = inside * step( 0.9, mask.a);',
 			//'   gl_FragColor = vec4(maskAlpha, maskAlpha, maskAlpha, 1.0 );',
-			'   gl_FragColor = colorTransform(tc, ${MaskedVarying.Color}, ${MaskedUniform.ColorMultiplier}, ${MaskedUniform.ColorOffset})* maskAlpha;',
+			'   gl_FragColor = colorTransform(tc, ${DefVarying.Color}, ${DefUniform.ColorMultiplier}, ${DefUniform.ColorOffset})* maskAlpha;',
 			'}'
 
 		];
@@ -91,44 +91,27 @@ class DefaultMaskedShader extends Shader {
 	override private function init(force:Bool = false) {
 		super.init(force);
 
-		getAttribLocation(MaskedAttrib.Position);
-		getAttribLocation(MaskedAttrib.TexCoord);
-		getAttribLocation(MaskedAttrib.Color);
-		getUniformLocation(MaskedUniform.ProjectionMatrix);
+		getAttribLocation(DefAttrib.Position);
+		getAttribLocation(DefAttrib.TexCoord);
+		getAttribLocation(DefAttrib.Color);
+		getUniformLocation(DefUniform.ProjectionMatrix);
 		getUniformLocation(MaskedUniform.MaskMatrix);
 		getUniformLocation(MaskedUniform.MaskUVScale);
-		getUniformLocation(MaskedUniform.Sampler);
+		getUniformLocation(DefUniform.Sampler);
 		getUniformLocation(MaskedUniform.MaskSampler);
-		getUniformLocation(MaskedUniform.ColorMultiplier);
-		getUniformLocation(MaskedUniform.ColorOffset);
-		getUniformLocation(MaskedUniform.UseColorTransform);
+		getUniformLocation(DefUniform.ColorMultiplier);
+		getUniformLocation(DefUniform.ColorOffset);
+		getUniformLocation(DefUniform.UseColorTransform);
 	}
 
 }
 
-// TODO Find a way to apply these default attributes and uniforms to other shaders
-
-@:enum abstract MaskedAttrib(String) from String to String {
-	var Position = "openfl_aPosition";
-	var TexCoord = "openfl_aTexCoord0";
-	var Color = "openfl_aColor";
-}
-
 @:enum abstract MaskedUniform(String) from String to String {
-	var Sampler = "openfl_uSampler0";
 	var MaskSampler = "openfl_uSampler1";
 	var MaskUVScale = 'openfl_uUVScale';
-	var ProjectionMatrix = "openfl_uProjectionMatrix";
 	var MaskMatrix = "openfl_uMaskMatrix";
-	var Color = "openfl_uColor";
-	var Alpha = "openfl_uAlpha";
-	var ColorMultiplier = "openfl_uColorMultiplier";
-	var ColorOffset = "openfl_uColorOffset";
-	var UseColorTransform = "openfl_uUseColorTransform";
 }
 
 @:enum abstract MaskedVarying(String) from String to String {
-	var TexCoord = "openfl_vTexCoord";
-	var MaskTexCoord = "openfl_vMaskTexCoord";
-	var Color = "openfl_vColor";
+   var MaskTexCoord = "openfl_vMaskTexCoord";
 }
