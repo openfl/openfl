d2c98111bd6c400b8757b288834eac869c8144cc
Joshua Granick <jgranick@users.noreply.github.com>
2016-10-11 09:57:52 -0700

    Quick patch for 'eating' of text with different format

--- openfl/_internal/text/TextEngine.hx
+++ openfl/_internal/text/TextEngine.hx
@@ -863,6 +863,7 @@ class TextEngine {
 				
 				while (true) {
 					
+					if (textIndex == formatRange.end) break;
 					if (spaceIndex == -1) spaceIndex = formatRange.end;
 					
 					advances = getAdvances (text, textIndex, spaceIndex);
