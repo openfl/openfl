a13e07be01fb7409e946c3074737a834462b954e
Joshua Granick <jgranick@users.noreply.github.com>
2016-11-30 09:32:17 -0800

    Fix DOM shape worldVisible (resolve #1362)

--- openfl/_internal/renderer/dom/DOMDisplayObject.hx
+++ openfl/_internal/renderer/dom/DOMDisplayObject.hx
@@ -14,8 +14,8 @@ class DOMDisplayObject {
 	public static inline function render (displayObject:DisplayObject, renderSession:RenderSession):Void {
 		
 		#if (js && html5)
-		if (displayObject.opaqueBackground == null && displayObject.__graphics == null) return;
-		if (!displayObject.__renderable || displayObject.__worldAlpha <= 0) return;
+		//if (displayObject.opaqueBackground == null && displayObject.__graphics == null) return;
+		//if (!displayObject.__renderable || displayObject.__worldAlpha <= 0) return;
 		
 		if (displayObject.opaqueBackground != null && displayObject.width > 0 && displayObject.height > 0) {
 			
