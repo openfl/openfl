2e7b8dc07da69bb35ea582b0dede99c013a81dd2
Joshua Granick <jgranick@users.noreply.github.com>
2016-12-22 14:34:07 -0800

    Use AppName.embed instead lime.embed for HTML5 by default

--- templates/haxe/ApplicationMain.hx
+++ templates/haxe/ApplicationMain.hx
@@ -108,14 +108,7 @@ class ApplicationMain {
 	
 	
 	#if (js && html5)
-	@:keep @:expose("lime.::APP_FILE::.embed")
-	public static function __embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
-		
-		embed (element, width, height, background, assetsPrefix);
-		
-	}
-	
-	@:keep @:expose("lime.embed")
+	@:keep @:expose("::APP_FILE::.embed")
 	public static function embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
 		
 		var htmlElement:js.html.Element = null;
@@ -175,8 +168,16 @@ class ApplicationMain {
 	}
 	
 	
-	@:keep @:expose("openfl.embed")
+	@:keep @:expose("lime.embed")
 	public static function _embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
+		
+		embed (element, width, height, background, assetsPrefix);
+		
+	}
+	
+	
+	@:keep @:expose("openfl.embed")
+	public static function _embed2 (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
 		
 		embed (element, width, height, background, assetsPrefix);
 		
