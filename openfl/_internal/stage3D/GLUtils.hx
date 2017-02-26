0b565abb757271b3f1e33101fe21a8b3808516eb
Joseph Cloutier <jkcloutier@gmail.com>
2017-02-26 01:34:46 -0800

    Print useful GL error messages.

--- openfl/_internal/stage3D/GLUtils.hx
+++ openfl/_internal/stage3D/GLUtils.hx
@@ -5,20 +5,40 @@ import openfl.gl.GL;
 
 
 class GLUtils {
-
+	
     private static var debug = false;
-
+	
     // Comment the following line if you want the GL Error check in release mode on device
     //[Conditional("DEBUG")]
-
-    public static function CheckGLError():Void
-    {
+	
+    public static function CheckGLError ():Void {
         if (!debug) return;
 
-        var error = GL.getError();
+        var error = GL.getError ();
+		
         if (error != GL.NO_ERROR) {
-            throw new IllegalOperationError("Error calling openGL api. Error: " + error + "\n");
+			
+			var errorText = switch (error) {
+				case GL.NO_ERROR:
+					"GL_NO_ERROR";
+				case GL.INVALID_ENUM:
+					"GL_INVALID_ENUM";
+				case GL.INVALID_OPERATION:
+					"GL_INVALID_OPERATION";
+				case GL.INVALID_FRAMEBUFFER_OPERATION:
+					"GL_INVALID_FRAMEBUFFER_OPERATION";
+				case GL.INVALID_VALUE:
+					"GL_INVALID_VALUE";
+				case GL.OUT_OF_MEMORY:
+					"GL_OUT_OF_MEMORY";
+				default:
+					Std.string (error);
+			};
+			
+            throw new IllegalOperationError("Error calling openGL api. Error: " + errorText + "\n");
+			
         }
+		
     }
-
+	
 }
