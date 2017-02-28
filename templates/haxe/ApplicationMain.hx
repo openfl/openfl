7949c90fec2170f831743c342f7234b1cfd9ba35
Joshua Granick <jgranick@users.noreply.github.com>
2017-02-28 15:34:41 -0800

    Fix support for multiple embeds and loaderInfo.parameters

--- templates/haxe/ApplicationMain.hx
+++ templates/haxe/ApplicationMain.hx
@@ -8,6 +8,7 @@ import haxe.macro.Expr;
 #end
 
 @:access(lime.app.Application)
+@:access(lime.system.System)
 @:access(openfl.display.Stage)
 
 
@@ -17,13 +18,11 @@ import haxe.macro.Expr;
 	#if !macro
 	
 	
-	public static var config:lime.app.Config;
-	public static var preloader:openfl.display.Preloader;
-	
-	
 	public static function main () {
 		
-		config = {
+		var projectName = "::APP_FILE::";
+		
+		var config = {
 			
 			build: "::meta.buildNumber::",
 			company: "::meta.company::",
@@ -48,7 +47,7 @@ import haxe.macro.Expr;
 					hidden: #if munit true #else ::hidden:: #end,
 					maximized: ::maximized::,
 					minimized: ::minimized::,
-					parameters: "::parameters::",
+					parameters: ::parameters::,
 					resizable: ::resizable::,
 					stencilBuffer: ::stencilBuffer::,
 					title: "::title::",
@@ -61,6 +60,8 @@ import haxe.macro.Expr;
 			
 		};
 		
+		lime.system.System.__registerEntryPoint (projectName, create, config);
+		
 		#if (hxtelemetry && !macro)
 		var telemetry = new hxtelemetry.HxTelemetry.Config ();
 		telemetry.allocations = ::if (config.hxtelemetry != null)::("::config.hxtelemetry.allocations::" == "true")::else::true::end::;
@@ -71,26 +72,26 @@ import haxe.macro.Expr;
 		
 		#if (js && html5)
 		#if (munit || utest)
-		embed (null, ::WIN_WIDTH::, ::WIN_HEIGHT::, "::WIN_FLASHBACKGROUND::");
+		lime.system.System.embed (projectName, null, ::WIN_WIDTH::, ::WIN_HEIGHT::, config);
 		#end
 		#else
-		create ();
+		create (config);
 		#end
 		
 	}
 	
 	
-	public static function create ():Void {
+	public static function create (config:lime.app.Config):Void {
 		
 		var app = new openfl.display.Application ();
 		app.create (config);
 		
-		ManifestResources.init ();
+		ManifestResources.init (config);
 		
-		preloader = getPreloader ();
+		var preloader = getPreloader ();
 		app.setPreloader (preloader);
 		preloader.create (config);
-		preloader.onComplete.add (start);
+		preloader.onComplete.add (start.bind (app.window.stage));
 		
 		for (library in ManifestResources.preloadLibraries) {
 			
@@ -115,85 +116,26 @@ import haxe.macro.Expr;
 	}
 	
 	
+	#if (openfl < "5.0.0")
 	#if (js && html5)
 	@:keep @:expose("::APP_FILE::.embed")
-	public static function embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
-		
-		var htmlElement:js.html.Element = null;
-		
-		if (Std.is (element, String)) {
-			
-			htmlElement = cast js.Browser.document.getElementById (cast (element, String));
-			
-		} else if (element == null) {
-			
-			htmlElement = cast js.Browser.document.createElement ("div");
-			
-		} else {
-			
-			htmlElement = cast element;
-			
-		}
-		
-		var color = null;
-		
-		if (background != null && background != "") {
-			
-			background = StringTools.replace (background, "#", "");
-			
-			if (background.indexOf ("0x") > -1) {
-				
-				color = Std.parseInt (background);
-				
-			} else {
-				
-				color = Std.parseInt ("0x" + background);
-				
-			}
-			
-		}
-		
-		if (width == null) {
-			
-			width = 0;
-			
-		}
-		
-		if (height == null) {
-			
-			height = 0;
-			
-		}
-		
-		config.windows[0].background = color;
-		config.windows[0].element = htmlElement;
-		config.windows[0].width = width;
-		config.windows[0].height = height;
-		config.assetsPrefix = assetsPrefix;
-		
-		create ();
-		
-	}
-	
-	
-	@:keep @:expose("lime.embed")
 	public static function _embed (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
 		
-		embed (element, width, height, background, assetsPrefix);
+		lime.system.System.embed ("::APP_FILE::", element, width, height, { background: background, assetsPrefix: assetsPrefix });
 		
 	}
-	
 	
 	@:keep @:expose("openfl.embed")
 	public static function _embed2 (element:Dynamic, width:Null<Int> = null, height:Null<Int> = null, background:String = null, assetsPrefix:String = null) {
 		
-		embed (element, width, height, background, assetsPrefix);
+		lime.system.System.embed ("::APP_FILE::", element, width, height, { background: background, assetsPrefix: assetsPrefix });
 		
 	}
 	#end
+	#end
 	
 	
-	public static function start ():Void {
+	public static function start (stage:openfl.display.Stage):Void {
 		
 		#if flash
 		
@@ -207,15 +149,15 @@ import haxe.macro.Expr;
 			
 		} catch (e:Dynamic) {
 			
-			openfl.Lib.current.stage.__handleError (e);
+			stage.__handleError (e);
 			
 		}
 		
-		openfl.Lib.current.stage.dispatchEvent (new openfl.events.Event (openfl.events.Event.RESIZE, false, false));
+		stage.dispatchEvent (new openfl.events.Event (openfl.events.Event.RESIZE, false, false));
 		
-		if (openfl.Lib.current.stage.window.fullscreen) {
+		if (stage.window.fullscreen) {
 			
-			openfl.Lib.current.stage.dispatchEvent (new openfl.events.FullScreenEvent (openfl.events.FullScreenEvent.FULL_SCREEN, false, false, true, true));
+			stage.dispatchEvent (new openfl.events.FullScreenEvent (openfl.events.FullScreenEvent.FULL_SCREEN, false, false, true, true));
 			
 		}
 		#end
@@ -253,7 +195,20 @@ import haxe.macro.Expr;
 					
 				} else if (type.constructor != null) {
 					
-					return macro { new DocumentClass (); };
+					return macro {
+						
+						var current:openfl.display.DisplayObjectContainer = cast stage.getChildAt (0);
+						
+						if (current == null) {
+							
+							current = new openfl.display.MovieClip ();
+							stage.addChild (current);
+							
+						}
+						
+						new DocumentClass (current);
+						
+					};
 					
 				} else {
 					
@@ -388,13 +343,13 @@ class DocumentClass {
 				
 				var method = macro {
 					
-					openfl.Lib.current.addChild (this);
+					current.addChild (this);
 					super ();
 					dispatchEvent (new openfl.events.Event (openfl.events.Event.ADDED_TO_STAGE, false, false));
 					
 				}
 				
-				fields.push ({ name: "new", access: [ APublic ], kind: FFun({ args: [], expr: method, params: [], ret: macro :Void }), pos: Context.currentPos () });
+				fields.push ({ name: "new", access: [ APublic ], kind: FFun({ args: [ { name: "current", opt: false, type: macro :openfl.display.DisplayObjectContainer, value: null } ], expr: method, params: [], ret: macro :Void }), pos: Context.currentPos () });
 				
 				return fields;
 				
