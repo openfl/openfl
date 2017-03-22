0a39d0300038a939581e6789842858b9c3082c3d
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2017-03-22 16:05:34 +0100

    Avoid double cursor

--- openfl/_internal/renderer/canvas/CanvasTextField.hx
+++ openfl/_internal/renderer/canvas/CanvasTextField.hx
@@ -232,7 +232,7 @@ class CanvasTextField {
 
 							if (textField.__selectionIndex == textField.__caretIndex) {
 
-								if (textField.__showCursor && group.startIndex <= textField.__caretIndex && group.endIndex >= textField.__caretIndex) {
+								if (textField.__showCursor && group.startIndex <= textField.__caretIndex && ( group.endIndex > textField.__caretIndex || textEngine.text.length == textField.__caretIndex)) {
 
 									advance = 0.0;
 
