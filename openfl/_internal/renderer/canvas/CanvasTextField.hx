d309918e733a9ebf9cdc4863e48656ad70215fb4
Pieter Vantorre <pieter.vantorre@fishingcactus.com>
2017-03-22 10:59:36 +0100

    Better condition to highlight selection. exclude groups that are completely outside

--- openfl/_internal/renderer/canvas/CanvasTextField.hx
+++ openfl/_internal/renderer/canvas/CanvasTextField.hx
@@ -247,7 +247,8 @@ class CanvasTextField {
 
 								}
 
-							} else if ((group.startIndex <= textField.__caretIndex && group.endIndex >= textField.__caretIndex) || (group.startIndex <= textField.__selectionIndex && group.endIndex >= textField.__selectionIndex)) {
+							} else if (!((group.startIndex < textField.__caretIndex && group.startIndex < textField.__selectionIndex && group.endIndex < textField.__caretIndex && group.endIndex < textField.__selectionIndex)
+									|| (group.startIndex > textField.__caretIndex && group.startIndex > textField.__selectionIndex && group.endIndex > textField.__caretIndex && group.endIndex > textField.__selectionIndex))) {
 
 								var selectionStart = Std.int (Math.min (textField.__selectionIndex, textField.__caretIndex));
 								var selectionEnd = Std.int (Math.max (textField.__selectionIndex, textField.__caretIndex));
