cf23d0ca1e2236bf36cca6f2971c790a5eb2d3c0
Joshua Granick <jgranick@users.noreply.github.com>
2017-04-25 23:30:49 -0700

    GraphicsTrianglePath added, some graphics optimizations

---
+++ externs/core/openfl/openfl/display/GraphicsTrianglePath.hx
@@ -0,0 +1,22 @@
+package openfl.display; #if (display || !flash)
+
+
+import openfl.Vector;
+
+
+@:final extern class GraphicsTrianglePath implements IGraphicsData implements IGraphicsPath {
+	
+	
+	public var culling:TriangleCulling;
+	public var indices:Vector<Int>;
+	public var uvtData:Vector<Float>;
+	public var vertices:Vector<Float>;
+	
+	public function new (vertices:Vector<Float> = null, indices:Vector<Int> = null, uvtData:Vector<Float> = null, culling:TriangleCulling = NONE);
+	
+}
+
+
+#else
+typedef GraphicsTrianglePath = flash.display.GraphicsTrianglePath;
+#end
