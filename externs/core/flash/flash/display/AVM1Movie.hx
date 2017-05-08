f012b3df8624ed3b18c3e50acd306f65e809392a
Joshua Granick <jgranick@users.noreply.github.com>
2017-05-08 15:39:16 -0700

    Add AVM1Movie (for better compatibility with Flash code)

---
+++ externs/core/flash/flash/display/AVM1Movie.hx
@@ -0,0 +1,21 @@
+package flash.display; #if (!display && flash)
+
+
+import haxe.Constraints.Function;
+
+
+extern class AVM1Movie extends DisplayObject {
+	
+	
+	private function new ():Void;
+	
+	public function addCallback (functionName:String, closure:Function):Void;
+	public function call (functionName:String, ?p1:Dynamic, ?p2:Dynamic, ?p3:Dynamic, ?p4:Dynamic, ?p5:Dynamic):Dynamic;
+	
+	
+}
+
+
+#else
+typedef AVM1Movie = openfl.display.AVM1Movie;
+#end
