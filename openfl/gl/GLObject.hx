3e69b6c4f47c70ced9973314bb6080035dce9d73
Joshua Granick <jgranick@users.noreply.github.com>
2017-05-08 10:54:41 -0700

    Compile fix

--- openfl/gl/GLObject.hx
+++ openfl/gl/GLObject.hx
@@ -1,4 +1,8 @@
 package openfl.gl;
 
 
-typedef GLObject = lime.graphics.opengl.GLObject;
+#if (!js || !html5 || display)
+typedef GLObject = lime.graphics.opengl.GL.GLObject;
+#else
+typedef GLObject = Dynamic;
+#end
