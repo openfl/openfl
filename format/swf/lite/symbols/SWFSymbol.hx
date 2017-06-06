8334c16f46337c621b85be9576b010b2703f099c
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-06-06 17:11:11 +0200

    Added useUniqueSharedBitmapCache in symbol

--- format/swf/lite/symbols/SWFSymbol.hx
+++ format/swf/lite/symbols/SWFSymbol.hx
@@ -2,22 +2,21 @@ package format.swf.lite.symbols;
 
 import openfl.ObjectPool;
 import openfl.display.DisplayObject;
-
+import openfl.display.BitmapData;
 
 @:keepSub class SWFSymbol {
 	
 	
 	public var className:String;
 	public var id:Int;
-
 	public var poolable(default, set):Bool = false;
-
 	public var pool:ObjectPool<DisplayObject>;
+	public var useUniqueSharedBitmapCache = false;
+	public var uniqueSharedCachedBitmap:BitmapData = null;
 
 	public function new () {
 
 	}
-	
 
 	public function set_poolable(value:Bool):Bool {
 
