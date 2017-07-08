acbd96e48b852583cd711b2974595bd3458cb037
Mike Smullin <mike.smullin@wildworks.com>
2017-07-07 18:16:48 -0600

    support shared fonts

--- tools/format/swf/exporters/SWFLiteExporter.hx
+++ tools/format/swf/exporters/SWFLiteExporter.hx
@@ -696,6 +696,7 @@ class SWFLiteExporter {
 		symbol.wordWrap = tag.wordWrap;
 		symbol.input = !tag.readOnly;
 		
+		// embedded fonts
 		if (tag.hasFont) {
 			
 			var font:IDefinitionTag = cast data.getCharacter (tag.fontId);
@@ -708,6 +709,13 @@ class SWFLiteExporter {
 			
 			symbol.fontID = tag.fontId;
 			symbol.fontName = cast (font, TagDefineFont2).fontName;
+			
+		}
+		
+		// shared fonts
+		if (tag.hasFontClass) {
+			
+			symbol.fontName = tag.fontClass;
 			
 		}
 		
