dc520ed967372103b03cb4910515c9e8ef58aa2d
Jarrio <Jarrio@users.noreply.github.com>
2017-07-16 21:37:45 +0100

    Added enum's to MouseCursor
    WAIT_ARROW may be platform dependant

--- openfl/ui/MouseCursor.hx
+++ openfl/ui/MouseCursor.hx
@@ -8,5 +8,13 @@ package openfl.ui;
 	public var BUTTON = "button";
 	public var HAND = "hand";
 	public var IBEAM = "ibeam";
+	public var CROSSHAIR = "crosshair";
+	public var MOVE = "move";
+	public var WAIT = "wait";
+	public var WAIT_ARROW = "waitarrow";
+	public var RESIZE_NESW = "resize_nesw";
+	public var RESIZE_NS = "resize_ns";
+	public var RESIZE_NWSE = "resize_nwse";
+	public var RESIZE_WE = "resize_we";
 	
 }
