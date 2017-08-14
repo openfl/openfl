167e54e8ab7944913c8ec01584a828269cdb06ae
Jeff Cook <jeff@jeffcook.io>
2017-08-14 19:03:13 -0400

    Use Future to resolve a loading soundsprite. ...
    This allows us to bypass some hackiness and restore the pure
    `AudioBuffer.loadFromFiles` function from Lime.

--- openfl/media/SoundSprite.hx
+++ openfl/media/SoundSprite.hx
@@ -5,6 +5,7 @@ class SoundSprite { }
 import lime.media.howlerjs.Howl;
 import js.html.Audio;
 import lime.media.AudioBuffer;
+import lime.app.Future;
 
 @:access(lime.Assets)
 @:access(openfl.media.Sound)
@@ -22,13 +23,20 @@ class SoundSprite {
   public var children = new Map<String, Array<Dynamic>>();
   public var assetKey:String;
 
+  private var __bufferFuture:Future<AudioBuffer>;
   private var __spriteIndex = new Map<String, Array<Dynamic>>();
 
 	public function new (assetKey:String, spritePaths:Array<String>, spriteIndex:Map<String, Dynamic>, ?preload:Bool=false) {
     this.spritePaths = spritePaths;
-    this.buffer = AudioBuffer.makeSprite(spritePaths, spriteIndex, preload);
     trace('Requesting files associated with this soundsprite...');
-    AudioBuffer.loadFromFiles(spritePaths, this.buffer);
+    if (this.__bufferFuture == null) {
+      this.buffer = AudioBuffer.makeSprite(spritePaths, spriteIndex, preload);
+      this.__bufferFuture = AudioBuffer.loadFromFiles(spritePaths);
+      this.__bufferFuture.onComplete(function(a:AudioBuffer) {
+        trace('bufferFuture loaded; setting this.buffer.');
+        this.buffer = a;
+      });
+    }
 	}
 
   public static function prepSpriteData(spritePart:SoundSpriteInfo):Dynamic {
