fb19472341fc8c47c48cc39257a4d28c8c58d392
Joshua Granick <jgranick@users.noreply.github.com>
2017-09-22 12:40:12 -0700

    drawTriangles fixes (resolve #1719)

--- openfl/_internal/renderer/cairo/CairoGraphics.hx
+++ openfl/_internal/renderer/cairo/CairoGraphics.hx
@@ -840,6 +840,7 @@ class CairoGraphics {
 					var matrix = new Matrix3 ();
 					
 					cairo.antialias = NONE;
+					cairo.source = fillPattern;
 					
 					while (i < l) {
 						
@@ -941,7 +942,6 @@ class CairoGraphics {
 						
 						matrix.setTo (t1, t2, t3, t4, dx, dy);
 						cairo.matrix = matrix;
-						cairo.source = fillPattern;
 						if (!hitTesting) cairo.fill ();
 						
 						i += 3;
