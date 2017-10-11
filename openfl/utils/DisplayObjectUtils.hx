68e4840a2fad26dce91a9e9a7b043f1e8be6fdc3
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2017-10-11 18:23:51 +0200

    Added DisplayObjectUtils.takeScreenshot() and removed StopAllChildren()

--- openfl/utils/DisplayObjectUtils.hx
+++ openfl/utils/DisplayObjectUtils.hx
@@ -1,6 +1,11 @@
 package openfl.utils;
 
+import openfl._internal.renderer.RenderSession;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
 import openfl.display.DisplayObject;
+import openfl.display.PixelSnapping;
+import openfl.geom.Rectangle;
 import openfl.utils.UnshrinkableArray;
 
 class DisplayObjectUtils {
@@ -23,11 +28,17 @@ class DisplayObjectUtils {
 
     }
 
-    public static function stopAllChildren (displayObject:DisplayObject) {
+    public static function takeScreenshot(displayObject:DisplayObject, renderSession:RenderSession):Bitmap {
+        var bitmapData = @:privateAccess BitmapData.__asRenderTexture ();
+        var renderBounds = Rectangle.pool.get ();
+        displayObject.__cacheBitmapFn (bitmapData, renderBounds, renderSession);
+        Rectangle.pool.put (renderBounds);
 
-        applyToAllChildren( displayObject, function (child) {
-            if (Reflect.field (child, "__playing") == true) {
-                Reflect.callMethod (child, Reflect.field (child, "stop"), []);
-            }});
+        var screenshotBitmap:Bitmap = new Bitmap(bitmapData);
+        screenshotBitmap.smoothing = true;
+        screenshotBitmap.pixelSnapping = PixelSnapping.NEVER;
+        @:privateAccess screenshotBitmap.__transform.copyFrom (displayObject.__transform);
+
+        return screenshotBitmap;
     }
 }
