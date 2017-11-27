4bf10256cad7cb412bba5d8166ba2bc3ce26580b
Sid Pitt <sid.pitt@wildworks.com>
2017-11-27 13:51:09 -0700

    Skip rendering of offscreen CanvasDisplayObjects

--- openfl/_internal/renderer/canvas/CanvasDisplayObject.hx
+++ openfl/_internal/renderer/canvas/CanvasDisplayObject.hx
@@ -3,19 +3,30 @@ package openfl._internal.renderer.canvas;
 
 import lime.math.color.ARGB;
 import openfl.display.DisplayObject;
+import openfl.geom.Rectangle;
+
 
 @:access(openfl.display.DisplayObject)
+@:access(openfl.display.Graphics)
 @:access(openfl.geom.Matrix)
 
 
 class CanvasDisplayObject {
-	
-	
+
 	public static inline function render (displayObject:DisplayObject, renderSession:RenderSession):Void {
 		
 		#if (js && html5)
 		if (displayObject.opaqueBackground == null && displayObject.__graphics == null) return;
 		if (!displayObject.__renderable || displayObject.__worldAlpha <= 0) return;
+
+		// Bail on rendering if bounds are completely offscreen
+		var gBounds:Rectangle = displayObject.__graphics.__bounds;
+		var rX : Float = gBounds == null ? Math.NaN : displayObject.__worldTransform.tx + gBounds.x;
+		var rY : Float = gBounds == null ? Math.NaN : displayObject.__worldTransform.ty + gBounds.y;
+		if ((!Math.isNaN(rX) && !Math.isNaN(rY)) && (rX < 0 && (rX * -1) > gBounds.width) || (rY < 0 && (rY * -1) > gBounds.height) || (renderSession.renderer != null && (rX > renderSession.renderer.width || rY > renderSession.renderer.height)) )
+		{
+			return;
+		}
 		
 		if (displayObject.opaqueBackground != null && !displayObject.__cacheBitmapRender && displayObject.width > 0 && displayObject.height > 0) {
 			
