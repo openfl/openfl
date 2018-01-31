7da9dc397c8c1bb49c7bbd384e620d6862c35efe
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-31 12:21:09 -0800

    Compile fix

--- src/openfl/display/Tilemap.hx
+++ src/openfl/display/Tilemap.hx
@@ -442,7 +442,7 @@ class Tilemap extends #if !flash DisplayObject #else Bitmap implements IDisplayO
 	
 	
 	#if !flash
-	private override function __updateCacheBitmap (renderSession:RenderSession, force:Bool):Void {
+	private override function __updateCacheBitmap (renderSession:RenderSession, force:Bool):Bool {
 		
 		if (filters == null) return false;
 		return super.__updateCacheBitmap (renderSession, force);
