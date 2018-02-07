c0d5a58d9ebdaa7d09c664246a24313428c00698
Joshua Granick <jgranick@users.noreply.github.com>
2018-02-07 12:30:56 -0800

    Minor type fixes

--- lib/openfl/Vector.d.ts
+++ lib/openfl/Vector.d.ts
@@ -1,13 +1,13 @@
 declare namespace openfl {
 	
 	
-	export class Vector<T> implements ArrayAccess<T> {
+	export class Vector<T> {
 		
 		
 		public fixed:boolean;
 		public length:number;
 		
-		public constructor (length?:number, fixed?:Bool):Void;
+		public constructor (length?:number, fixed?:boolean);
 		
 		public concat (a?:Vector<T>):Vector<T>;
 		public copy ():Vector<T>;
@@ -15,20 +15,22 @@ declare namespace openfl {
 		public indexOf (x:T, from?:number):number;
 		public insertAt (index:number, element:T):void;
 		public join (sep:string):string;
-		public lastIndexOf (x:T, from?:number = 0):number;
-		public pop ():Null<T>;
+		public lastIndexOf (x:T, from?:number):number;
+		public pop ():null | T;
 		public push (x:T):number;
 		public removeAt (index:number):T;
 		public reverse ():Vector<T>;
 		public set (index:number, value:T):T;
-		public shift ():Null<T>;
+		public shift ():null | T;
 		public slice (pos?:number, end?:number):Vector<T>;
 		public sort (f:(a:T, b:T)=>number):void;
 		public splice (pos:number, len:number):Vector<T>;
 		public unshift (x:T):void;
 		public static ofArray<T> (a:Array<T>):Vector<T>;
 		
-		// @:noCompletion public iterator ():Iterator<T>;
+		//public iterator ():Iterator<T>;
+		public next:T;
+		[index:number]:T;
 		
 		
 	}
