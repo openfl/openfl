2e95efc0a6ed4e5262c693f736b70c2352ec34a3
Joshua Granick <jgranick@users.noreply.github.com>
2018-02-20 15:41:15 -0800

    Add openfl.display.DOMElement (like DOMSprite, but extends DisplayObject instead)

---
+++ src/openfl/display/DOMElement.hx
@@ -0,0 +1,67 @@
+package openfl.display;
+
+
+#if !flash
+import openfl._internal.renderer.dom.DOMRenderer;
+import openfl._internal.renderer.RenderSession;
+#end
+
+import openfl.display.Sprite;
+import openfl.display.Stage;
+
+#if (js && html5)
+import js.html.Element;
+#end
+
+
+class DOMElement extends DisplayObject {
+	
+	
+	private var __active:Bool;
+	private var __element:#if (js && html5) Element #else Dynamic #end;
+	
+	
+	public function new (element:#if (js && html5) Element #else Dynamic #end) {
+		
+		super ();
+		
+		__element = element;
+		
+	}
+	
+	
+	#if !flash
+	private override function __renderDOM (renderSession:RenderSession):Void {
+		
+		#if (js && html5)
+		if (stage != null && __worldVisible && __renderable) {
+			
+			if (!__active) {
+				
+				DOMRenderer.initializeElement (this, __element, renderSession);
+				__active = true;
+				
+			}
+			
+			DOMRenderer.updateClip (this, renderSession);
+			DOMRenderer.applyStyle (this, renderSession, true, true, true);
+			
+		} else {
+			
+			if (__active) {
+				
+				renderSession.element.removeChild (__element);
+				__active = false;
+				
+			}
+			
+		}
+		
+		super.__renderDOM (renderSession);
+		#end
+		
+	}
+	#end
+	
+	
+}
