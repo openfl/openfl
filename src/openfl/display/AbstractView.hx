c82afdef89de0d8eff35a65b307b810a230d5747
Joshua Granick <jgranick@users.noreply.github.com>
2018-02-21 22:16:38 -0800

    Add DOM support to AbstractView, adding a WebGL or Canvas element

--- src/openfl/display/AbstractView.hx
+++ src/openfl/display/AbstractView.hx
@@ -2,12 +2,33 @@ package openfl.display;
 
 
 import lime.graphics.cairo.Cairo;
+import lime.graphics.opengl.GL;
+import lime.graphics.GLRenderContext;
+import openfl._internal.renderer.canvas.CanvasRenderer;
+import openfl._internal.renderer.dom.DOMRenderer;
 import openfl._internal.renderer.opengl.GLRenderer;
+import openfl._internal.renderer.AbstractRenderer;
 import openfl._internal.renderer.RenderSession;
 import openfl.events.RenderEvent;
 import openfl.geom.ColorTransform;
 import openfl.geom.Matrix;
 
+#if (js && html5)
+import js.html.webgl.RenderingContext;
+import js.html.CanvasElement;
+import js.Browser;
+#end
+
+#if !openfl_debug
+@:fileXml('tags="haxe,release"')
+@:noDebug
+#end
+
+@:access(lime._backend.html5.HTML5GLRenderContext)
+@:access(lime._backend.native.NativeGLRenderContext)
+@:access(lime.graphics.opengl.GL)
+@:access(openfl._internal.renderer.AbstractRenderer)
+@:access(openfl._internal.renderer.RenderSession)
 @:access(openfl.events.Event)
 @:access(openfl.geom.ColorTransform)
 
@@ -15,6 +36,8 @@ import openfl.geom.Matrix;
 class AbstractView extends DisplayObject {
 	
 	
+	private var __addedToDOM:Bool;
+	private var __domRenderer:AbstractRenderer;
 	private var __renderEvent:RenderEvent;
 	
 	
@@ -80,6 +103,109 @@ class AbstractView extends DisplayObject {
 		
 		renderSession.maskManager.popObject (this);
 		__renderEvent.context = null;
+		
+	}
+	
+	
+	private override function __renderDOM (renderSession:RenderSession):Void {
+		
+		#if (js && html5)
+		if (stage != null && __worldVisible && __renderable && __renderDirty) {
+			
+			if (__canvas == null) {
+				
+				__canvas = cast Browser.document.createElement ("canvas");
+				__canvas.width = stage.stageWidth;
+				__canvas.height = stage.stageHeight;
+				
+				var window = stage.window;
+				
+				var options = {
+					
+					alpha: true,
+					antialias: false,
+					depth: Reflect.hasField (window.config, "depthBuffer") ? window.config.depthBuffer : true,
+					premultipliedAlpha: true,
+					stencil: Reflect.hasField (window.config, "stencilBuffer") ? window.config.stencilBuffer : false,
+					preserveDrawingBuffer: false
+					
+				};
+				
+				var glContextType = [ "webgl2", "webgl", "experimental-webgl" ];
+				
+				var webgl:RenderingContext = null;
+				
+				for (name in glContextType) {
+					
+					webgl = cast __canvas.getContext (name, options);
+					if (webgl != null) break;
+					
+				}
+				
+				if (webgl != null) {
+					
+					var gl = new GLRenderContext (cast webgl);
+					
+					if (GL.context == null) {
+						
+						GL.context = gl;
+						
+					}
+					
+					__domRenderer = new GLRenderer (stage, gl);
+					
+				} else {
+					
+					__context = __canvas.getContext ("2d");
+					__domRenderer = new CanvasRenderer (stage, __context);
+					
+				}
+				
+			} else if (__canvas.width != stage.stageWidth || __canvas.height != stage.stageHeight) {
+				
+				__canvas.width = stage.stageWidth;
+				__canvas.height = stage.stageHeight;
+				
+			}
+			
+			if (!__addedToDOM) {
+				
+				renderSession.element.appendChild (__canvas);
+				__addedToDOM = true;
+				
+				DOMRenderer.initializeElement (this, __canvas, renderSession);
+				
+			}
+			
+			DOMRenderer.applyStyle (this, renderSession, false, true, true);
+			
+			if (__context != null) {
+				
+				__domRenderer.renderSession.blendModeManager.setBlendMode (NORMAL);
+				
+				__context.setTransform (1, 0, 0, 1, 0, 0);
+				__context.globalAlpha = 1;
+				__context.clearRect (0, 0, stage.stageWidth * stage.window.scale, stage.stageHeight * stage.window.scale);
+				
+				__renderCanvas (__domRenderer.renderSession);
+				
+			} else {
+				
+				var gl = __domRenderer.renderSession.gl;
+				gl.clearColor (0, 0, 0, 0);
+				gl.clear (gl.COLOR_BUFFER_BIT);
+				
+				__renderGL (__domRenderer.renderSession);
+				
+			}
+			
+		} else if (__addedToDOM) {
+			
+			renderSession.element.removeChild (__canvas);
+			__addedToDOM = false;
+			
+		}
+		#end
 		
 	}
 	
