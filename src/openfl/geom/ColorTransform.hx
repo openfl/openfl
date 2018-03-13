50abd227f75d8212443486bc14fce6671dc1d7ae
Joshua Granick <jgranick@users.noreply.github.com>
2018-03-12 21:54:32 -0700

    Update colorTransform.concat (thanks @pluginio)

--- src/openfl/geom/ColorTransform.hx
+++ src/openfl/geom/ColorTransform.hx
@@ -51,15 +51,15 @@ class ColorTransform {
 	
 	public function concat (second:ColorTransform):Void {
 		
-		redMultiplier *= second.redMultiplier;   
+		redOffset = second.redOffset * redMultiplier + redOffset;
+		greenOffset = second.greenOffset * greenMultiplier + greenOffset;
+		blueOffset = second.blueOffset * blueMultiplier + blueOffset;
+		alphaOffset = second.alphaOffset * alphaMultiplier + alphaOffset;
+		
+		redMultiplier *= second.redMultiplier;
 		greenMultiplier *= second.greenMultiplier;
 		blueMultiplier *= second.blueMultiplier;
 		alphaMultiplier *= second.alphaMultiplier;
-		
-		redOffset = second.redMultiplier * redOffset + second.redOffset;
-		greenOffset = second.greenMultiplier * greenOffset + second.greenOffset;
-		blueOffset = second.blueMultiplier * blueOffset + second.blueOffset;
-		alphaOffset = second.alphaMultiplier * alphaOffset + second.alphaOffset;
 		
 	}
 	
