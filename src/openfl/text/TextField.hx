5afdfe27b3698b0141685b3484fe9a61ddb933e2
nenad.bojkovski <nenadbojkovski@gmail.com>
2018-04-03 19:00:45 +0200

    Fixes the htmltext getter

--- src/openfl/text/TextField.hx
+++ src/openfl/text/TextField.hx
@@ -1986,7 +1986,11 @@ class TextField extends InteractiveObject implements IShaderDrawable {
 	
 	private function get_htmlText ():String {
 		
+		#if (js && html5)
+		return __isHTML ? __rawHtmlText : __text;
+		#else
 		return __text;
+		#end
 		
 	}
 	
@@ -2004,11 +2008,7 @@ class TextField extends InteractiveObject implements IShaderDrawable {
 		__isHTML = true;
 		
 		#if (js && html5)
-		if (DisplayObject.__supportDOM) {
-			
-			__rawHtmlText = value;
-			
-		}
+		__rawHtmlText = value;
 		#end
 		
 		value = HTMLParser.parse(value, __textFormat, __textEngine.textFormatRanges);
