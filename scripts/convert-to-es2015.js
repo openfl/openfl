46906cba20b297a704fc3d60d2bd78266ad4169d
Andres Alva <andresa88@gmail.com>
2018-04-30 23:48:38 -0700

    Update to script

--- scripts/convert-to-es2015.js
+++ scripts/convert-to-es2015.js
@@ -5,28 +5,14 @@ let globby = require('globby');
 let path = require('path');
 
 /*
-This script replaces all occurrences of:
-
-exports.default =
-
-with
-
-export default
-
-and remove the $global lines at the top of all the js files
-
-under lib/_gen/ to make all the modules es2015 modules as opposed to commonjs modules. This then 
-allows webpack to properly tree shake our code. Any unused modules can be excluded 
-from the webpack generated js bundle.
-
+This script goes through each of the commonjs modules under lib/_gen/ and 
+generates es2015 versions of them. It will also go through each of the 
+modules that re-export a single module and generate es2015 version of that.
 */
 
-//console.log('Replacing exports.default = with export default...');
 
-// Search for all the js files under lib/_gen/
-
-var c = 0;
 startCreateEsmModules();
+
 startCreateDefaultReExportEsms();
 
 
@@ -74,21 +60,26 @@ function createEsmModule(filePath) {
   // We must remove these lines at the top of the generated js files or else when the
   // webpack generated bundle is run in the browser we will encounter an "exports is not defined" 
   // error. The variable "exports" is only availabe in commonjs modules.
-  result = result.replace('var $global = typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this', '');
+  
   result = result.replace('$global.Object.defineProperty(exports, "__esModule", {value: true});', '');
   result = result.replace('Object.defineProperty(exports, "__esModule", {value: true});', '');
   
+  // TODO: Remove this line ONLY if $global is not used anywhere else in the module
+  //result = result.replace('var $global = typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this', '');
+  
+  
+  // Replace
+  // require("./../../_gen/openfl/display/Sprite");
+  // with
+  // require("./../../_gen/openfl/display/Sprite.esm");
+  // BUT ONY IF Sprite.esm.js exists, if not, leave as is
   result = result.replace(/require\(['"](.+?)['"]\)/gm, (match, p1) => {
     
     try {
       
-      
+      // Check if the esm.js file event exists
 			let fullPath = path.resolve(path.dirname(filePath), p1 + '.esm.js');
-			//fullPath = path.dirname(filePath) + '/' + p2 + '.js';
 			
-      if (c++ < 100) {
-        console.log(fullPath);
-      }
 			if (fs.statSync(fullPath).isFile()) {
         return 'require("' + p1 + '.esm")';
 			}
@@ -97,15 +88,13 @@ function createEsmModule(filePath) {
     }
     
     return 'require("' + p1 + '")';
-    
-    
   });
   
   
   try {
     
     let esmFilePath = filePath.replace(/\.js$/, '.esm.js');
-    //console.log('writing', esmFilePath);
+    
     fs.writeFileSync(esmFilePath, result, 'utf8');
     
   } catch (error) {
@@ -118,20 +107,25 @@ function createEsmModule(filePath) {
 
 
 
+// Here we look create the es2015 version of the commonjs modules that contain:
+// module.exports = require("./../../_gen/openfl/display/Sprite").default;
+
+// The generated es2015 module will contain:
+// export { default } from "./../../_gen/openfl/display/Sprite.esm");
 function startCreateDefaultReExportEsms() {
   
   globby(['../lib/openfl/**/*.js']).then((paths) => {
 
     let updated = 0;
-  
     
     for (let path of paths) {
     
-      // Skip the barrel index.js and index.esm.js files
+      // Ignore the esm.js files
       if (path.match(/\.esm\.js$/) != null) {
         continue;
       }
       
+      // Skip the barrel index.js and index.esm.js files
       if (path.match(/index(\.js|\.esm\.js|\.d\.ts)$/) != null) {
         continue;
       }
@@ -163,9 +157,7 @@ function createDefaultReExportEsm(filePath) {
   if (content.indexOf('module.exports.default') > -1) {
     //console.log(filePath);
   }
-  //module.exports = require("./../../_gen/openfl/display/Graphics");
-  // By replacing exports.default with export default we are converting the module 
-  // to an es2015 module from commonjs
+  
   var result = content;
   
   
@@ -207,9 +199,7 @@ function createDefaultReExportEsm(filePath) {
     
     let esmFilePath = filePath.replace(/\.js$/, '.esm.js');
     
-    //console.log(esmFilePath);
     fs.writeFileSync(esmFilePath, result, 'utf8');
-    
     
   } catch (error) {
     console.error('Could not write to file:', filePath);
