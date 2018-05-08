a2c4410ff34251649d6600bad5f7ac1ad98130ff
Andres Alva <andresa88@gmail.com>
2018-05-07 22:25:58 -0700

    Added path-mapping.js files for use in the webpack alias field

---
+++ lib_esm/path-mapping.js
@@ -0,0 +1,43 @@
+
+"use strict"
+
+var path = require('path');
+var dir = path.resolve(__dirname);
+
+module.exports = function() {
+  return {
+		"openfl/desktop": "openfl/lib_esm/openfl/desktop",
+		"openfl/display": "openfl/lib_esm/openfl/display",
+		"openfl/display3D": "openfl/lib_esm/openfl/display3D",
+		"openfl/errors": "openfl/lib_esm/openfl/errors",
+		"openfl/events": "openfl/lib_esm/openfl/events",
+		"openfl/external": "openfl/lib_esm/openfl/external",
+		"openfl/filters": "openfl/lib_esm/openfl/filters",
+		"openfl/geom": "openfl/lib_esm/openfl/geom",
+		"openfl/media": "openfl/lib_esm/openfl/media",
+		"openfl/net": "openfl/lib_esm/openfl/net",
+		"openfl/profiler": "openfl/lib_esm/openfl/profiler",
+		"openfl/sensors": "openfl/lib_esm/openfl/sensors",
+		"openfl/system": "openfl/lib_esm/openfl/system",
+		"openfl/text": "openfl/lib_esm/openfl/text",
+		"openfl/ui": "openfl/lib_esm/openfl/ui",
+		"openfl/utils": "openfl/lib_esm/openfl/utils",
+		"openfl/utils/Assets": "openfl/lib_esm/openfl/utils/Assets",
+		"openfl/Lib": "openfl/lib_esm/openfl/Lib",
+		"openfl/Memory": "openfl/lib_esm/openfl/Memory",
+		"openfl/Vector": "openfl/lib_esm/openfl/Vector",
+		"openfl/VectorData": "openfl/lib_esm/openfl/VectorData",
+		"openfl/_internal": "openfl/lib_esm/openfl/_internal",
+		"openfl/_Vector": "openfl/lib_esm/openfl/_Vector",
+		"lime/_backend": "openfl/lib_esm/_gen/lime/_backend",
+		"lime/app": "openfl/lib_esm/_gen/lime/app",
+		"lime/graphics": "openfl/lib_esm/_gen/lime/graphics",
+		"lime/math": "openfl/lib_esm/_gen/lime/math",
+		"lime/media": "openfl/lib_esm/_gen/lime/media",
+		"lime/net": "openfl/lib_esm/_gen/lime/net",
+		"lime/system": "openfl/lib_esm/_gen/lime/system",
+		"lime/text": "openfl/lib_esm/_gen/lime/text",
+		"lime/ui": "openfl/lib_esm/_gen/lime/ui",
+		"lime/utils": "openfl/lib_esm/_gen/lime/utils",
+};
+}
