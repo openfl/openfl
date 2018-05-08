4a2104bcbd4b1837e06282711a0cf688edef09d1
Andres Alva <andresa88@gmail.com>
2018-05-07 19:04:17 -0700

    Refactored generate-es2015.js so that it replaces most require() calls with import calls. Had to implement solution to circular dependencies

--- scripts/generate-es2015.js
+++ scripts/generate-es2015.js
@@ -8,25 +8,24 @@ let path = require('path');
 Run this after the "npm run build-lib" script. This script does the following:
 
 - Goes through each of the commonjs modules under lib/_gen/ and generates es2015 versions 
-of them with .esm.js file extension. See startCreateEsmModules()
+of them and plaaces them under a different directory. See startCreateEsmModules()
 
 - Goes through each of the commonjs modules under lib/openfl/ that re-export a single module and 
 generates the equivalent es2015 module. See startCreateDefaultReExportEsms()
 
-- Takes all the commonjs barrel index.js files under lib/openfl and generates es2015 versions of them
-and saves them as index.esm.js. Also generates the index.d.ts declaration files for 
-typescript. See startCreateBarrelModules()
+- Takes all the commonjs barrel index.js files under lib/openfl and generates es2015 versions of 
+them. Also generates the index.d.ts declaration files for typescript. See startCreateBarrelModules()
 
 
 We then need to make sure we add the following:
 
-"module": "lib/openfl/index.esm.js"
+"module": "lib_esm/openfl/index.js"
 
 to the openfl package.json. This way whenever the following import is encountered
 
 import { Sprite, Stage } from "openfl";
 
-webpack will rely on index.esm.js to import the listed classes. We also need to add the 
+webpack will rely on index.js to import the listed classes. We also need to add the 
 following:
 
 "sideEffects": false
@@ -49,6 +48,9 @@ var c = 0;
 
 
 console.log('generate-es2015 running...');
+
+// First create the directory we will export our es2015 modules to
+mkDirByPathSync('../lib_esm/_gen');
 
 startCreateEsmModules().then(() => {
   
@@ -165,6 +167,53 @@ function startCreateEsmModules() {
   
 }
 
+function moveImportsToTop(content, importLines) {
+      
+  for (let importLine of importLines) {
+    content = content.replace(importLine, '');
+  }
+  
+  let importLineStr = importLines.reduce((prev, value) => {
+    return prev + "\n" + value;
+  }, '');
+  content = content.replace('// Imports', '// Imports\n' + importLineStr);
+  
+  return content; 
+}
+
+
+function importAndCallInit(content, lookFor, className) {
+  let rep = `
+init${lookFor}();
+var ${className} = function(`;
+  
+  content = content.replace(new RegExp(`var ${className} = function\\(`), rep);
+    
+  r = `import { default as ${lookFor}, init as init${lookFor} } from `;
+  content = content.replace(new RegExp(`import { default as ${lookFor} } from `), r);
+  
+  return content;
+}
+
+function exportInit(content, className) {
+  let rep = `
+var ${className};
+export function init() {
+
+if (${className})
+  return;
+  
+${className} = function(`;
+
+  content = content.replace(new RegExp(`var ${className} = function\\(`), rep);
+  
+  // We need to use "export { Sprite as default }"" as opposed to "export default Sprite" in order to 
+  // take advantage of live bindings. This way our module init functions can initialize our classes 
+  // when other modules call the init function.
+  content = content.replace(`export default ${className};`, `}\ninit();\nexport { ${className} as default };`);
+  
+  return content;
+}
 
 
 function createEsmModule(filePath) {
@@ -173,29 +222,27 @@ function createEsmModule(filePath) {
   if (content === false)
     return;
   
-  
-  var result = content.replace(/exports\.default =/g, 'export default');
-  // Replace 
-  // exports.default =
-  // with
-  // export default 
+    
+  var result = content;
   
   
-  // We must remove these lines at the top of the generated js files or else when the
-  // webpack generated bundle is run in the browser we will encounter an "exports is not defined" 
-  // error. The variable "exports" is only availabe in commonjs modules.
   
+  // We must remove these lines at the top as they are only needed in commonjs modules
   result = result.replace('$global.Object.defineProperty(exports, "__esModule", {value: true});', '');
   result = result.replace('Object.defineProperty(exports, "__esModule", {value: true});', '');
   
   // TODO: Remove this line ONLY if $global is not used anywhere else in the module
   //result = result.replace('var $global = typeof window != "undefined" ? window : typeof global != "undefined" ? global : typeof self != "undefined" ? self : this', '');
   
-  //filePath.replace('lib/', 'lib-es/')
-  let esmFilePath = filePath.replace(/\.js$/, '.esm.js');
+  let esmFilePath = filePath.replace('lib/', 'lib_esm/');
+  // ../lib/_gen/openfl/display/Sprite.js
+  // to
+  // ../lib_esm/_gen/openfl/display/Sprite.js
   
   
-    
+  mkDirByPathSync(path.dirname(esmFilePath));
+  
+  
   return writeFileSync(esmFilePath, result);
 }
 
@@ -205,7 +252,7 @@ function startModifyEsmModules() {
   
   // Now we need to go through the esm.js modules that were generated 
   // and replace the required file paths with the .esm.js versions
-  return globby(['../lib/_gen/**/*.esm.js']).then((paths) => {
+  return globby(['../lib_esm/_gen/**/*.js']).then((paths) => {
     
     for (let path of paths) {
       
@@ -228,13 +275,55 @@ function modifyEsmModule(filePath) {
   // var $hxClasses = require("./../../hxClasses_stub").default;
   // with
   // import { default as $hxClasses } from "./../../hxClasses_stub";
-  result = result.replace(/^var (.+?) = require\(['"](.+?)['"]\)\.default;/gm, 'import $1 from "$2";');
+  result = result.replace(/^var (.+?) = require\(['"](.+?)['"]\)\.default;/gm, 'import { default as $1 } from "$2";');
   
   // Replace
   // function openfl_display_DisplayObject() {return require("./../../openfl/display/DisplayObject");}
   // with
   // import { default as openfl_display_DisplayObject } from "./../../openfl/display/DisplayObject";
-  result = result.replace(/^function (.+?)\(\) {return require\(['"]\.\/(.+?)['"]\);}/gm, 'import $1 from "./$2";');
+  result = result.replace(/^function (.+?)\(\) {return require\(['"]\.\/(.+?)['"]\);}/gm, 'import { default as $1 } from "./$2";');
+  
+  
+  
+  
+  result = result.replace(/exports\.default =/g, 'export default');
+  // Replace 
+  // exports.default = Sprite
+  // with
+  // export { Sprite as default };
+  
+  
+  
+  if (filePath.indexOf('openfl/display/DisplayObject.') > -1) {
+    result = exportInit(result, 'DisplayObject');
+    
+    result = moveImportsToTop(result, [
+      'import { default as haxe_ds_StringMap } from "./../../haxe/ds/StringMap";', 
+      'import { default as lime_utils_ObjectPool } from "./../../lime/utils/ObjectPool";'
+    ]);
+    
+  }
+  
+  
+  if (filePath.indexOf('openfl/display/Bitmap.js') > -1) {
+    result = importAndCallInit(result, 'openfl_display_DisplayObject', 'Bitmap');
+  }
+
+  if (filePath.indexOf('openfl/display/BitmapData.js') > -1) {
+    
+    result = exportInit(result, 'BitmapData');
+      
+    result = moveImportsToTop(result, [
+'import { default as openfl_geom_Rectangle } from "./../../openfl/geom/Rectangle";',
+'import { default as lime_graphics_Image } from "./../../lime/graphics/Image";',
+'import { default as lime_math_Vector2 } from "./../../lime/math/Vector2";'
+    ]);
+  }
+  
+  if (filePath.indexOf('openfl/_internal/renderer/opengl/GLMaskShader.js') > -1) {
+    
+    result = importAndCallInit(result, 'openfl_display_BitmapData', 'GLMaskShader');
+  }
   
   
   
@@ -243,7 +332,7 @@ function modifyEsmModule(filePath) {
   // with
   // openfl_display_DisplayObject.call(this);
   
-  // I had to add the [^(] character negation to deal with this case:
+  // I had to add the [^(] character negation (instead of .) to deal with this case:
   // (symbol1,(openfl__$internal_symbols_BitmapSymbol().default))
   // Had I instead used the dot metacharacter, it would have transformed it into this:
   // symbol1,(openfl__$internal_symbols_BitmapSymbol)
@@ -251,85 +340,27 @@ function modifyEsmModule(filePath) {
   // (symbol1,openfl__$internal_symbols_BitmapSymbol)
   result = result.replace(/\(([^(]+?)\(\)\.default\)/gm, '$1');
   
-  
-  
-  // Replace
-  // require("./../../_gen/openfl/display/Sprite");
-  // with
-  // require("./../../_gen/openfl/display/Sprite.esm");
-  // BUT ONY IF Sprite.esm.js exists, if not, leave as is as is the case 
-  // with the howler and pako requires.
-  /*
-  result = result.replace(/require\s*\(['"](.+?)['"]\)/gm, (match, p1) => {
-    
-    let fullPath;
-    try {
-      
-      // Check if the esm.js file exists, if not, leave it
-			fullPath = path.resolve(path.dirname(filePath), p1 + '.esm.js');
-			
-			if (isFile(fullPath)) {
-        return 'require("' + p1 + '.esm")';
-			}
-		} catch (error) {
-      
-      //console.log('f', filePath);
-      //console.log(fullPath);
-    }
-    
-    return 'require("' + p1 + '")';
-  });
-  */
  
-  
- result = result.replace(/(import .+? from )['"](.+?)['"];/gm, (match, p1, p2) => {
-    
-  let fullPath;
-  try {
-    
-    // Check if the esm.js file exists, if not, leave it
-    fullPath = path.resolve(path.dirname(filePath), p2 + '.esm.js');
-    
-    if (isFile(fullPath)) {
-      //return 'require("' + p1 + '.esm")';
-      return p1 + '"' + p2 + '.esm' + '";'
-    }
-  } catch (error) {
-    
-    //console.log('f', filePath);
-    //console.log(fullPath);
-  }
-  
-  //return 'require("' + p1 + '")';
-  return match;
-});
-    
-  //let esmFilePath = filePath.replace(/\.js$/, '.esm.js');
   let esmFilePath = filePath;
-  
   
   return writeFileSync(esmFilePath, result);
 }
 
 
-// Here we look create the es2015 version of the commonjs modules that contain:
+// Here we create the es2015 version of the commonjs modules that contain:
 // module.exports = require("./../../_gen/openfl/display/Sprite").default;
 
 // The generated es2015 module will contain:
-// export { default } from "./../../_gen/openfl/display/Sprite.esm");
+// export { default } from "./../../_gen/openfl/display/Sprite");
 function startCreateDefaultReExportEsms() {
   
   return globby(['../lib/openfl/**/*.js']).then((paths) => {
     
     for (let path of paths) {
     
-      // Ignore the esm.js files
-      if (path.match(/\.esm\.js$/) != null) {
-        continue;
-      }
       
       // Skip the barrel index.js and index.esm.js files
-      if (path.match(/index(\.js|\.esm\.js|\.d\.ts)$/) != null) {
+      if (path.match(/index(\.js|\.d\.ts)$/) != null) {
         continue;
       }
       
@@ -352,7 +383,7 @@ function createDefaultReExportEsm(filePath) {
   
   var result = content;
   
-  result = result.replace(/^\s*module\.exports\s*=\s*require\(['"](.+?)['"]\);/gm, 'export { default } from "$1.esm";');
+  result = result.replace(/^\s*module\.exports\s*=\s*require\(['"](.+?)['"]\);/gm, 'export { default } from "$1";');
   // Replace:
   // "module.exports = require("./../../_gen/openfl/display/Graphics");"
   // with
@@ -378,35 +409,29 @@ function createDefaultReExportEsm(filePath) {
   // export default 
   
   
-  result = result.replace(/^\s*var (.*?) = require \(['"](.*?)['"]\)\.default;/gm, 'import { default as $1 } from "$2.esm";');
+  result = result.replace(/^\s*var (.*?) = require \(['"](.*?)['"]\)\.default;/gm, 'import { default as $1 } from "$2";');
   // Replace
   // var Lib = require ("./../../_gen/openfl/Lib").default;
   // with
   // import { default as Lib } from "./../../_gen/openfl/Lib.esm";
   
-    
-  let esmFilePath = filePath.replace(/\.js$/, '.esm.js');
+  
+  result = result.replace('module.exports._internal = internal;', 'export { internal as _internal };');
+  // Replace (in lib_esm/openfl/utils/AssetLibrary.js)
+  // module.exports._internal = internal;
+  // with
+  // export { internal as _internal };
+
+  
+  
+  let esmFilePath = filePath.replace('lib/', 'lib_esm/');
+  mkDirByPathSync(path.dirname(esmFilePath));
+  // ../lib/openfl/display/Sprite.js
+  // to
+  // ../lib_esm/openfl/display/Sprite.js
   
   return writeFileSync(esmFilePath, result);
 }
-
-
-
-
-/*
-Take all the barrel index.js files under lib/openfl and generate es2015 versions of them
-and save them as index.esm.js. We then need to make sure we add the key-value pair: 
-
-"module": "lib/openfl/index.esm.js"
-
-to the openfl package.json. This way when webpack is run, it will be able to tree shake our bundle, excluding 
-modules that are not used in an openfl app. Also add
-
-"sideEffects": false
-
-to package.json to indicate to webpack 4 that it is safe to exclude entire unused modules from the bundle
-
-*/
 
 function startCreateBarrelModules() {
     
@@ -432,14 +457,14 @@ function createEsmIndex(filePath) {
   
   
 	// Handle comment lines
-	result = result.replace(/^\s*\/\/(.+?): require\(["'](.+?)["']\)\.default.*/gm, '// export { default as $1 } from "$2.esm";');
+	result = result.replace(/^\s*\/\/(.+?): require\(["'](.+?)["']\)\.default.*/gm, '// export { default as $1 } from "$2";');
 	// Replaces: 
 	// "// Application: require("./Application").default,"
 	// with
 	// "export { default as Application } from "./Application.esm";"
 	
 	
-	result = result.replace(/^\s*(.+?): require\(["'](.+?)["']\)\.default.*/gm, 'export { default as $1 } from "$2.esm";');
+	result = result.replace(/^\s*(.+?): require\(["'](.+?)["']\)\.default.*/gm, 'export { default as $1 } from "$2";');
 	//result = result.replace(/^\s*(.+?): require\(["'](.+?)["']\)\.default.*/gm, 'export { default as $1 } from "$2";');
 	// Replaces: 
 	// "Bitmap: require("./Bitmap").default,"
@@ -451,24 +476,22 @@ function createEsmIndex(filePath) {
   
 	
 	// Deal with the barrel index modules that are re-rexported
-	// Note: Must call this AFTER the previous replace() call
-	//result = result.replace(/^\s*(.+?): require\(["'](.+?)["']\).*/gm, 'export * from "$2/index.esm";');
-	result = result.replace(/^\s*(.+?): require\(["'](.+?)["']\).*/gm, (match, p1, p2) => {
+  // Note: Must call this AFTER the previous replace() call
+  result = result.replace(/^\s*(.+?): require\(["'](.+?)["']\).*/gm, (match, p1, p2) => {
 		
 		try {
 			
 			let fullPath = path.resolve(path.dirname(filePath), p2 + '.js');
-			//fullPath = path.dirname(filePath) + '/' + p2 + '.js';
 			
 			if (fs.statSync(fullPath).isFile()) {
-				//return 'export * from "' + p2 + '";';
-				return 'export * from "' + p2 + '.esm";';
+				
+        return 'export * from "' + p2 + '";';
 			}
 		} catch (error) {
 			
 		}
 		
-		return 'export * from "' + p2 + '/index.esm";';
+    return 'export * from "' + p2 + '";';
 	});
 	// Replaces: 
 	// textures: require("./textures"),
@@ -486,12 +509,15 @@ function createEsmIndex(filePath) {
   
   // We save the file as index.esm.js and place it in the same directory as the 
   // index.js that we read
-  var esmFilePath = filePath.replace(/\.js$/, '.esm.js');
+  //var esmFilePath = filePath.replace(/\.js$/, '.esm.js');
+  let esmFilePath = filePath.replace('lib/', 'lib_esm/');
+  mkDirByPathSync(path.dirname(esmFilePath));
+  // ../lib/openfl/display/index.js
+  // to
+  // ../lib_esm/openfl/display/index.js
+  
   writeFileSync(esmFilePath, result);
   
-  
-  result = result.replace(/\/index.esm/g, '');
-  result = result.replace(/\.esm/g, '');
   
   var dTSFilePath = filePath.replace(/\.js$/, '.d.ts');
   writeFileSync(dTSFilePath, result);
@@ -519,8 +545,6 @@ function isFile(filename) {
   }
   
   return true;
-  
-  
 }
 
 function readFileSync(filename) {
@@ -564,3 +588,27 @@ function writeFileSync(filename, content) {
   
   writtenContentsMap.set(filename, content);
 }
+
+
+function mkDirByPathSync(targetDir, {isRelativeToScript = false} = {}) {
+  const sep = path.posix.sep;
+  const initDir = path.isAbsolute(targetDir) ? sep : '';
+  const baseDir = isRelativeToScript ? __dirname : '.';
+
+  targetDir.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(baseDir, parentDir, childDir);
+    try {
+      fs.mkdirSync(curDir);
+      //console.log(`Directory ${curDir} created!`);
+    } catch (err) {
+      if (err.code !== 'EEXIST') {
+        throw err;
+      }
+
+      //console.log(`Directory ${curDir} already exists!`);
+    }
+
+    return curDir;
+  }, initDir);
+}
+
