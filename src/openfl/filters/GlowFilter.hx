42cbc535cfb049f6042008a5ea15323872fca3b9
Dan Korostelev <nadako@gmail.com>
2018-07-05 17:01:11 +0200

    handle alpha properly for GlowFilter and DropShadowFilter

--- src/openfl/filters/GlowFilter.hx
+++ src/openfl/filters/GlowFilter.hx
@@ -91,7 +91,7 @@ import openfl.geom.Rectangle;
 		var r = (__color >> 16) & 0xFF;
 		var g = (__color >> 8) & 0xFF;
 		var b = __color & 0xFF;
-		sourceBitmapData.colorTransform (sourceBitmapData.rect, new ColorTransform (0, 0, 0, 1, r, g, b, __alpha * 0xFF));
+		sourceBitmapData.colorTransform (sourceBitmapData.rect, new ColorTransform (0, 0, 0, __alpha, r, g, b, 0));
 		
 		var finalImage = ImageDataUtil.gaussianBlur (bitmapData.image, sourceBitmapData.image, sourceRect.__toLimeRectangle (), destPoint.__toLimeVector2 (), __blurX, __blurY, __quality, __strength);
 		
