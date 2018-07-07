5b563ae75b857e12b46f83534ad2df655699c62c
Nick Johnson <nj2cool4u@yahoo.com>
2018-07-06 22:14:09 -0400

    Off by 1

--- src/openfl/text/TextField.hx
+++ src/openfl/text/TextField.hx
@@ -293,7 +293,7 @@ class TextField extends InteractiveObject {
 	
 	public function getFirstCharInParagraph (charIndex:Int):Int {
 		
-		if (charIndex < 0 || charIndex > text.length + 1) return -1;
+		if (charIndex < 0 || charIndex > text.length) return -1;
 		
 		var index = __textEngine.getLineBreakIndex ();
 		var startIndex = 0;
@@ -476,7 +476,7 @@ class TextField extends InteractiveObject {
 	
 	public function getParagraphLength (charIndex:Int):Int {
 		
-		if (charIndex < 0 || charIndex > text.length + 1) return -1;
+		if (charIndex < 0 || charIndex > text.length) return -1;
 		
 		var startIndex = getFirstCharInParagraph (charIndex);
 		
