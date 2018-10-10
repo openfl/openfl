9b15c2ef9c21040a14a762e28655faf39cef73d5
Joshua Granick <jgranick@users.noreply.github.com>
2018-10-10 12:47:51 -0700

    Add DisplacementMapFilter

---
+++ externs/flash/flash/filters/DisplacementMapFilterMode.hx
@@ -0,0 +1,16 @@
+package flash.filters; #if flash
+
+
+@:enum abstract DisplacementMapFilterMode(String) from String to String {
+	
+	public var CLAMP = "clamp";
+	public var COLOR = "color";
+	public var IGNORE = "ignore";
+	public var WRAP = "wrap";
+	
+}
+
+
+#else
+typedef DisplacementMapFilterMode = openfl.filters.DisplacementMapFilterMode;
+#end
--- src/openfl/display/OpenGLRenderer.hx
+++ src/openfl/display/OpenGLRenderer.hx
@@ -358,9 +358,9 @@ class OpenGLRenderer extends DisplayObjectRenderer {
 			__currentShader = shader;
 			__initShader (shader);
 			__context3D.setProgram (shader.program);
-			 __context3D.__flushGLProgram ();
-			 __context3D.__flushGLTextures ();
-			 __currentShader.__enable ();
+			__context3D.__flushGLProgram ();
+			__context3D.__flushGLTextures ();
+			__currentShader.__enable ();
 			__context3D.__state.shader = shader;
 			
 		}
