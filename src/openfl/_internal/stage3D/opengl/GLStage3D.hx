8fb56f7bb264269c4b7ab104707ab646fbd2b6f0
Daniil Korostelev <daniil.korostelev@innogames.com>
2018-10-10 13:36:58 +0200

    Always enable Context3D program when rendering Stage3D

--- src/openfl/_internal/stage3D/opengl/GLStage3D.hx
+++ src/openfl/_internal/stage3D/opengl/GLStage3D.hx
@@ -24,7 +24,19 @@ class GLStage3D {
 			
 			renderSession.blendModeManager.setBlendMode (null);
 			
-			if (renderSession.shaderManager.currentShader != null) {
+			// TODO: the commented check below is the result of the assumption that
+			// shaderManager.currentShader being null means the we only used Context3D shaders
+			// and we don't need to re-enable the Context3D program as it's already active.
+			//
+			// However this is now not the case, because our new BatchRenderer doesn't use
+			// the shader manager as well: it sets shaderManager.currentShader to null and then
+			// manually enables its own shader. So the check below becomes invalid.
+			//
+			// There are a number of more correct approaches to fix this, one of them being
+			// to adapt BatchRenderer to use ShaderManager, but it's too intrusive at the moment,
+			// so instead we just always enable the Stage3D program.
+			
+			// if (renderSession.shaderManager.currentShader != null) {
 				
 				renderSession.shaderManager.setShader (null);
 				
@@ -34,7 +46,7 @@ class GLStage3D {
 					
 				}
 				
-			}
+			// }
 			
 		}
 		
