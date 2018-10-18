ddf36e53b2231bbbcc3007c33acdb5111dce7426
Łukasz Modliński <lukasz.modlinski@gmail.com>
2018-10-18 10:01:50 +0200

    Software Displacement Map Filtering
    Software support for Displacement Map Filter - sourceBitmapData again, has to be cloned in order for all this to work.
    
    Uses: https://github.com/openfl/lime/pull/1249

--- src/openfl/filters/DisplacementMapFilter.hx
+++ src/openfl/filters/DisplacementMapFilter.hx
@@ -1,7 +1,14 @@
 package openfl.filters; #if !flash
 
 
-// import lime._internal.graphics.ImageDataUtil;
+#if (js && html5)
+import lime._internal.graphics.ImageCanvasUtil;
+#end
+
+import lime._internal.graphics.ImageDataUtil;
+import lime.math.Vector2;
+import lime.math.Vector4;
+
 import openfl.display.BitmapDataChannel;
 import openfl.geom.Rectangle;
 import openfl.geom.Point;
@@ -78,11 +85,27 @@ import openfl.display.Shader;
 	
 	
 	@:noCompletion private override function __applyFilter(bitmapData:BitmapData, sourceBitmapData:BitmapData, sourceRect:Rectangle, destPoint:Point):BitmapData {
-		
-		// TODO
-		// ImageDataUtil.displace(bitmapData.image, sourceBitmapData.image, __mapBitmap.image, __componentX, __componentY, __scaleX, __scaleY);
+
+		__updateMapMatrix();
+
+		#if (js && html5)
+		ImageCanvasUtil.convertToData (sourceBitmapData.image);
+		ImageCanvasUtil.convertToData (__mapBitmap.image);
+		#end
+
+		ImageDataUtil.displaceMap(
+			bitmapData.image,
+			sourceBitmapData.clone().image,
+
+			__mapBitmap.image,
+			new Vector2(__mapPoint.x / __mapBitmap.width, __mapPoint.y / __mapBitmap.height),
+
+			new Vector4(__matrixData[0], __matrixData[4], __matrixData[8], __matrixData[12]),
+			new Vector4(__matrixData[1], __matrixData[5], __matrixData[9], __matrixData[13]),
+			__smooth
+		);
 		return bitmapData;
-		
+
 	}
 	
 	
