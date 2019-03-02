c61ee142754b4cb03ad6b5a496d480035a6d609d
pixelsyntax <thomas.purnell@gmail.com>
2019-03-02 20:49:32 +1000

    Fix hasRect() always false for non square tiles (#2143)
    * Fix hasRect() always false for non square tiles
    
    fix: Tileset.hasRect() always returns false for non square tiles due to comparing rect.width == tileData.height
    
    * Fix getRectID() returning null incorrectly
    
    Fixed: getRectID() will always return null for non square tiles due to typo

--- src/openfl/display/Tileset.hx
+++ src/openfl/display/Tileset.hx
@@ -86,7 +86,7 @@ class Tileset
 	{
 		for (tileData in __data)
 		{
-			if (rect.x == tileData.x && rect.y == tileData.y && rect.width == tileData.height && rect.height == tileData.height)
+			if (rect.x == tileData.x && rect.y == tileData.y && rect.width == tileData.width && rect.height == tileData.height)
 			{
 				return true;
 			}
@@ -113,7 +113,7 @@ class Tileset
 		{
 			tileData = __data[i];
 
-			if (rect.x == tileData.x && rect.y == tileData.y && rect.width == tileData.height && rect.height == tileData.height)
+			if (rect.x == tileData.x && rect.y == tileData.y && rect.width == tileData.width && rect.height == tileData.height)
 			{
 				return i;
 			}
