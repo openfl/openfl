cb426b7beed46cdb681053ff8eba708a9ab1a49f
Joshua Granick <jgranick@users.noreply.github.com>
2019-03-07 16:48:50 -0800

    Format fix

--- src-draft/openfl/_internal/formats/xfl/display/XFLElement.hx
+++ src-draft/openfl/_internal/formats/xfl/display/XFLElement.hx
@@ -20,38 +20,34 @@ import xfl.XFLSymbolArguments;
 /**
  * XFL element
  */
-interface XFLElement {
-
-    // xfl symbol arguments
-    public var xflSymbolArguments(get, never): XFLSymbolArguments;
-
-    // display object container
-    public function addChild(child: DisplayObject): DisplayObject;
-
-    // XFL elements
-    public function getXFLElementUntyped(name: String) : Dynamic;
-    public function getXFLElement(name: String) : XFLElement;
-    public function getXFLMovieClip(name: String) : XFLMovieClip;
-    public function getXFLSprite(name: String) : XFLSprite;
-    public function getXFLDisplayObject(name: String) : DisplayObject;
-    public function getXFLDisplayObjectContainer(name: String) : DisplayObjectContainer;
-    public function getXFLTextField(name: String) : TextField;
-    public function getXFLTextArea(name: String) : TextArea;
-    public function getXFLTextInput(name: String) : TextInput;
-    public function getXFLSlider(name: String) : Slider;
-    public function getXFLComboBox(name: String) : ComboBox;
-    public function getXFLCheckBox(name: String) : CheckBox;
-    public function getXFLRadioButton(name: String) : RadioButton;
-    public function getXFLList(name: String) : List;
-    public function getXFLScrollBar(name: String): ScrollBar;
-    public function getXFLUIScrollBar(name: String): UIScrollBar;
-    public function getXFLUIComponent(name: String): UIComponent;
-    public function getXFLScrollPane(name: String) : ScrollPane;
-    public function getXFLProgressBar(name: String) : ProgressBar;
-
-    // values
-    public function removeValue(key: String): Void;
-    public function addValue(key: String, value: Dynamic): Void;
-    public function getValue(key: String) : Dynamic;
-
+interface XFLElement
+{
+	// xfl symbol arguments
+	public var xflSymbolArguments(get, never):XFLSymbolArguments;
+	// display object container
+	public function addChild(child:DisplayObject):DisplayObject;
+	// XFL elements
+	public function getXFLElementUntyped(name:String):Dynamic;
+	public function getXFLElement(name:String):XFLElement;
+	public function getXFLMovieClip(name:String):XFLMovieClip;
+	public function getXFLSprite(name:String):XFLSprite;
+	public function getXFLDisplayObject(name:String):DisplayObject;
+	public function getXFLDisplayObjectContainer(name:String):DisplayObjectContainer;
+	public function getXFLTextField(name:String):TextField;
+	public function getXFLTextArea(name:String):TextArea;
+	public function getXFLTextInput(name:String):TextInput;
+	public function getXFLSlider(name:String):Slider;
+	public function getXFLComboBox(name:String):ComboBox;
+	public function getXFLCheckBox(name:String):CheckBox;
+	public function getXFLRadioButton(name:String):RadioButton;
+	public function getXFLList(name:String):List;
+	public function getXFLScrollBar(name:String):ScrollBar;
+	public function getXFLUIScrollBar(name:String):UIScrollBar;
+	public function getXFLUIComponent(name:String):UIComponent;
+	public function getXFLScrollPane(name:String):ScrollPane;
+	public function getXFLProgressBar(name:String):ProgressBar;
+	// values
+	public function removeValue(key:String):Void;
+	public function addValue(key:String, value:Dynamic):Void;
+	public function getValue(key:String):Dynamic;
 }
--- src-draft/openfl/_internal/formats/xfl/display/XFLMovieClip.hx
+++ src-draft/openfl/_internal/formats/xfl/display/XFLMovieClip.hx
@@ -23,101 +23,123 @@ import openfl.text.TextField;
 /**
  * XFL movie clip
  */
-class XFLMovieClip extends MovieClip implements XFLElement {
+class XFLMovieClip extends MovieClip implements XFLElement
+{
+	private var xflImplementation:XFLImplementation;
 
-	private var xflImplementation: XFLImplementation;
-
-	public function new(xflSymbolArguments: XFLSymbolArguments = null) {
+	public function new(xflSymbolArguments:XFLSymbolArguments = null)
+	{
 		super(xflSymbolArguments);
 		xflImplementation = new XFLImplementation(this);
 	}
 
-	public function getXFLElementUntyped(name: String) : Dynamic {
+	public function getXFLElementUntyped(name:String):Dynamic
+	{
 		return xflImplementation.getXFLElementUntyped(name);
 	}
 
-	public function getXFLElement(name: String) : XFLElement {
+	public function getXFLElement(name:String):XFLElement
+	{
 		return xflImplementation.getXFLElement(name);
 	}
 
-	public function getXFLMovieClip(name: String) : XFLMovieClip {
+	public function getXFLMovieClip(name:String):XFLMovieClip
+	{
 		return xflImplementation.getXFLMovieClip(name);
 	}
 
-	public function getXFLSprite(name: String) : XFLSprite {
+	public function getXFLSprite(name:String):XFLSprite
+	{
 		return xflImplementation.getXFLSprite(name);
 	}
 
-	public function getXFLDisplayObject(name: String) : DisplayObject {
+	public function getXFLDisplayObject(name:String):DisplayObject
+	{
 		return xflImplementation.getXFLDisplayObject(name);
 	}
 
-	public function getXFLDisplayObjectContainer(name: String) : DisplayObjectContainer {
+	public function getXFLDisplayObjectContainer(name:String):DisplayObjectContainer
+	{
 		return xflImplementation.getXFLDisplayObjectContainer(name);
 	}
 
-	public function getXFLTextField(name: String) : TextField {
+	public function getXFLTextField(name:String):TextField
+	{
 		return xflImplementation.getXFLTextField(name);
 	}
 
-	public function getXFLTextArea(name: String) : TextArea {
+	public function getXFLTextArea(name:String):TextArea
+	{
 		return xflImplementation.getXFLTextArea(name);
 	}
 
-	public function getXFLTextInput(name: String) : TextInput {
+	public function getXFLTextInput(name:String):TextInput
+	{
 		return xflImplementation.getXFLTextInput(name);
 	}
 
-	public function getXFLSlider(name: String) : Slider {
+	public function getXFLSlider(name:String):Slider
+	{
 		return xflImplementation.getXFLSlider(name);
 	}
 
-	public function getXFLComboBox(name: String) : ComboBox {
+	public function getXFLComboBox(name:String):ComboBox
+	{
 		return xflImplementation.getXFLComboBox(name);
 	}
 
-	public function getXFLCheckBox(name: String) : CheckBox {
+	public function getXFLCheckBox(name:String):CheckBox
+	{
 		return xflImplementation.getXFLCheckBox(name);
 	}
 
-	public function getXFLRadioButton(name: String) : RadioButton {
+	public function getXFLRadioButton(name:String):RadioButton
+	{
 		return xflImplementation.getXFLRadioButton(name);
 	}
 
-	public function getXFLList(name: String) : List {
+	public function getXFLList(name:String):List
+	{
 		return xflImplementation.getXFLList(name);
 	}
 
-	public function getXFLScrollBar(name: String) : ScrollBar {
+	public function getXFLScrollBar(name:String):ScrollBar
+	{
 		return xflImplementation.getXFLScrollBar(name);
 	}
 
-	public function getXFLUIScrollBar(name: String) : UIScrollBar {
+	public function getXFLUIScrollBar(name:String):UIScrollBar
+	{
 		return xflImplementation.getXFLUIScrollBar(name);
 	}
 
-	public function getXFLUIComponent(name: String): UIComponent {
+	public function getXFLUIComponent(name:String):UIComponent
+	{
 		return xflImplementation.getXFLUIComponent(name);
 	}
 
-	public function getXFLScrollPane(name: String) : ScrollPane {
+	public function getXFLScrollPane(name:String):ScrollPane
+	{
 		return xflImplementation.getXFLScrollPane(name);
 	}
 
-	public function getXFLProgressBar(name: String) : ProgressBar {
+	public function getXFLProgressBar(name:String):ProgressBar
+	{
 		return xflImplementation.getXFLProgressBar(name);
 	}
 
-	public function removeValue(key: String): Void {
+	public function removeValue(key:String):Void
+	{
 		xflImplementation.removeValue(key);
 	}
 
-	public function addValue(key: String, value: Dynamic) : Void {
+	public function addValue(key:String, value:Dynamic):Void
+	{
 		xflImplementation.addValue(key, value);
 	}
 
-	public function getValue(key: String) : Dynamic {
+	public function getValue(key:String):Dynamic
+	{
 		return xflImplementation.getValue(key);
 	}
-
 }
