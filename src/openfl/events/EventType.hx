798364778dd77a237ae96a92efb0e664ec71ad94
Joshua Granick <jgranick@users.noreply.github.com>
2019-03-11 16:17:09 -0700

    Make EventType operators inline

--- src/openfl/events/EventType.hx
+++ src/openfl/events/EventType.hx
@@ -16,12 +16,12 @@ package openfl.events;
 **/
 abstract EventType<T>(String) from String to String
 {
-	@:op(A == B) private static function equals<T>(a:EventType<T>, b:String):Bool
+	@:op(A == B) private static inline function equals<T>(a:EventType<T>, b:String):Bool
 	{
 		return (a : String) == b;
 	}
 
-	@:op(A != B) private static function notEquals<T>(a:EventType<T>, b:String):Bool
+	@:op(A != B) private static inline function notEquals<T>(a:EventType<T>, b:String):Bool
 	{
 		return (a : String) != b;
 	}
