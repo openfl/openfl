1c1a842147447bd6c2428b5ec574dd6a24268568
Joshua Granick <jgranick@users.noreply.github.com>
2019-03-11 08:24:18 -0700

    Add stub for FileSystem test

---
+++ test/haxelib/integration/src/filesystem/FileSystemTest.hx
@@ -0,0 +1,17 @@
+package filesystem;
+
+#if ((flash && air) || draft)
+import openfl.filesystem.File;
+
+class FileSystemTest
+{
+	@Test public function absolutePath()
+	{
+		// TODO: Confirm functionality
+
+		var file = new File();
+		var absolutePath = file.absolutePath;
+		Assert.isNotNull(absolutePath);
+	}
+}
+#end
