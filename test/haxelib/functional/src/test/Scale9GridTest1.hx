ec100c7690bee1b8ad282d74486cc560fe3498b9
Tuna Hunerli <tuna@kixeye.com>
2019-03-27 14:00:02 -0700

    More scale9Grid assets for functional testing

--- test/haxelib/functional/src/test/Scale9GridTest1.hx
+++ test/haxelib/functional/src/test/Scale9GridTest1.hx
@@ -16,13 +16,25 @@ class Scale9GridTest1 extends FunctionalTest
 		content = new Sprite();
 
 		var movieClip = Assets.getMovieClip("scale9Grid:Frame");
-
 		movieClip.x = 100;
 		movieClip.y = 100;
 		movieClip.width = 400;
 		movieClip.height = 300;
-
 		content.addChild(movieClip);
+
+		var button1 = Assets.getMovieClip("scale9Grid:ButtonPrimary");
+		button1.x = 600;
+		button1.y = 100;
+		button1.width = 128;
+		button1.height = 32;
+		content.addChild(button1);
+
+		var button2 = Assets.getMovieClip("scale9Grid:ButtonSecondary");
+		button2.x = 600;
+		button2.y = 200;
+		button2.width = 128;
+		button2.height = 32;
+		content.addChild(button2);
 	}
 
 	public override function stop():Void
