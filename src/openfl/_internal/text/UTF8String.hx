4e39f2fc8e8375be9efbc5022ce9eb7e6986bdcc
yutian.liu <939029835@qq.com>
2019-08-22 13:05:27 +0800

    Fix the problem that Chinese cannot be rendered properly.
    I think that Haxe 4.0 has improved UTF-8 characters. This fixes the
    Chinese rendering problem of C++ (Android & IOS).

--- src/openfl/_internal/text/UTF8String.hx
+++ src/openfl/_internal/text/UTF8String.hx
@@ -1,7 +1,9 @@
 package openfl._internal.text;
 
-#if lime
+#if (haxe_ver >= "4.0.0")
+typedef UTF8String = String;
+#elseif lime
 typedef UTF8String = lime.text.UTF8String;
 #else
-typedef UTF8String = String;
+typedef UTF8String = UnicodeString;
 #end
