44d897fd807b26ee696c1f1dc18efe2061c629ca
Joshua Granick <jgranick@users.noreply.github.com>
2019-08-23 08:54:59 -0700

    Fix UTF8String

--- src/openfl/_internal/text/UTF8String.hx
+++ src/openfl/_internal/text/UTF8String.hx
@@ -1,7 +1,7 @@
 package openfl._internal.text;
 
-#if (haxe_ver >= "4.0.0")
-typedef UTF8String = String;
-#elseif lime
+#if (!haxe4 && lime)
 typedef UTF8String = lime.text.UTF8String;
+#else
+typedef UTF8String = String;
 #end
