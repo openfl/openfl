b01261bb3a4647acfdae53fea913ffecea1e113f
mrchnk <mrchnk@gmail.com>
2019-09-04 19:00:40 +0300

    fix string value of ClipboardFormats.TEXT_FORMAT (typo)

--- src/openfl/desktop/ClipboardFormats.hx
+++ src/openfl/desktop/ClipboardFormats.hx
@@ -53,7 +53,7 @@ package openfl.desktop;
 {
 	public var HTML_FORMAT = "air:html";
 	public var RICH_TEXT_FORMAT = "air:rtf";
-	public var TEXT_FORMAT = "air:txt";
+	public var TEXT_FORMAT = "air:text";
 }
 #end
 #else
