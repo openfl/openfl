4dfb2b12bfeb6aec45933e43ce257e7fc2e1b021
Zaphod <zaphy@yandex.ru>
2019-10-23 14:05:04 +0300

    Bitmap should use width/height getters of DisplayObject
    This fixes the case when Bitmap is rotated with 90 degrees, which leads to "swap" between Bitmap's width and height values

--- src/openfl/display/Bitmap.hx
+++ src/openfl/display/Bitmap.hx
@@ -339,15 +339,6 @@ class Bitmap extends DisplayObject
 		return __bitmapData;
 	}
 
-	@:noCompletion private override function get_height():Float
-	{
-		if (__bitmapData != null)
-		{
-			return __bitmapData.height * Math.abs(scaleY);
-		}
-		return 0;
-	}
-
 	@:noCompletion private override function set_height(value:Float):Float
 	{
 		if (__bitmapData != null)
@@ -359,15 +350,6 @@ class Bitmap extends DisplayObject
 			scaleY = 0;
 		}
 		return value;
-	}
-
-	@:noCompletion private override function get_width():Float
-	{
-		if (__bitmapData != null)
-		{
-			return __bitmapData.width * Math.abs(__scaleX);
-		}
-		return 0;
 	}
 
 	@:noCompletion private override function set_width(value:Float):Float
