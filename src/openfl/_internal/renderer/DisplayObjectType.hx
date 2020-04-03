232d41c540b18b3f7b4fbda73d35b44cbf3502c6
Joshua Granick <jgranick@users.noreply.github.com>
2020-04-03 12:12:39 -0700

    Add missing file

---
+++ src/openfl/_internal/renderer/DisplayObjectType.hx
@@ -0,0 +1,19 @@
+package openfl._internal.renderer;
+
+enum DisplayObjectType
+{
+	BITMAP;
+	DISPLAY_OBJECT;
+	DISPLAY_OBJECT_CONTAINER;
+	DOM_ELEMENT;
+	SHAPE;
+	SIMPLE_BUTTON;
+	TEXTFIELD;
+	TILEMAP;
+	VIDEO;
+	CUSTOM;
+	#if draft
+	GL_GRAPHICS;
+	GEOMETRY;
+	#end
+}
