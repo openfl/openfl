6f5323efc20848f4b6f90c43e461e6f9fe7b7f84
Josh Tynjala <joshtynjala@bowlerhat.dev>
2020-05-07 10:05:02 -0700

    Stage: If FocusEvent.KEY_FOCUS_CHANGE is cancelled, cancel window.onKeyDown
    This improves compatibility of the html5 target, which was allowing the browser to incorrectly steal focus

--- src/openfl/display/Stage.hx
+++ src/openfl/display/Stage.hx
@@ -1594,6 +1594,11 @@ class Stage extends DisplayObjectContainer #if lime implements IModule #end
 						stack.reverse();
 
 						__dispatchStack(focusEvent, stack);
+
+						if (focusEvent.isDefaultPrevented())
+						{
+							window.onKeyDown.cancel();
+						}
 					}
 
 					if (focusEvent == null || !focusEvent.isDefaultPrevented())
