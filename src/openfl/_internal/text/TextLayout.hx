928dd46b3aa49df73ba79e4f807e244edf18b8e0
Nick <nj2cool4u@yahoo.com>
2020-05-20 13:51:49 -0400

    Fix UTF16 on Linux (#2328)
    Merging this since it's helped people and doesn't affect performance

--- src/openfl/_internal/text/TextLayout.hx
+++ src/openfl/_internal/text/TextLayout.hx
@@ -140,7 +140,7 @@ class TextLayout
 			__hbBuffer.script = script.toHBScript();
 			__hbBuffer.language = new HBLanguage(language);
 			__hbBuffer.clusterLevel = HBBufferClusterLevel.CHARACTERS;
-			#if (neko || mac || linux || hl)
+			#if (neko || mac || hl)
 			// other targets still uses dummy positions to make UTF8 work
 			// TODO: confirm
 			__hbBuffer.addUTF8(text, 0, -1);
