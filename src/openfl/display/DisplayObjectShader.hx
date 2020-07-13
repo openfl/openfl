4bd9b26d6cebe1085b9b7422e0a7c45841744d92
Vortelio <contact@vortelio.ru>
2020-07-13 15:27:16 +0500

    Update DisplayObjectShader.hx

--- src/openfl/display/DisplayObjectShader.hx
+++ src/openfl/display/DisplayObjectShader.hx
@@ -8,28 +8,33 @@ import openfl.utils.ByteArray;
 #end
 class DisplayObjectShader extends Shader
 {
-	@:glVertexHeader("attribute float openfl_Alpha;
+	@:glVertexHeader("
+		attribute float openfl_Alpha;
 		attribute vec4 openfl_ColorMultiplier;
 		attribute vec4 openfl_ColorOffset;
 		attribute vec4 openfl_Position;
 		attribute vec2 openfl_TextureCoord;
 
 		varying float openfl_Alphav;
+		varying vec4 openfl_ColorMultiplierv;
+		varying vec4 openfl_ColorOffsetv;
 		varying vec2 openfl_TextureCoordv;
 		varying vec4 colorM;
 
 		uniform mat4 openfl_Matrix;
 		uniform bool openfl_HasColorTransform;
-		uniform vec2 openfl_TextureSize;")
-	@:glVertexBody("openfl_Alphav = openfl_Alpha;
+		uniform vec2 openfl_TextureSize;
+		")
+	@:glVertexBody("
+		openfl_Alphav = openfl_Alpha;
 		openfl_TextureCoordv = openfl_TextureCoord;
 
 		colorM = vec4 (1.0, 1.0, 1.0, 1.0);
 
 		if (openfl_HasColorTransform) {
 
-			vec4 openfl_ColorMultiplierv = openfl_ColorMultiplier;
-			vec4 openfl_ColorOffsetv = openfl_ColorOffset / 255.0;
+			openfl_ColorMultiplierv = openfl_ColorMultiplier;
+			openfl_ColorOffsetv = openfl_ColorOffset / 255.0;
 
 			mat4 colorMultiplier = mat4 (0);
 			colorMultiplier[0][0] = openfl_ColorMultiplierv.x;
@@ -38,45 +43,43 @@ class DisplayObjectShader extends Shader
 			colorMultiplier[3][3] = 1.0; // openfl_ColorMultiplierv.w;
 
 			colorM = clamp (openfl_ColorOffsetv + (colorM * colorMultiplier), 0.0, 1.0);
-
 		}
 
 		colorM *= openfl_Alphav;
 
-		gl_Position = openfl_Matrix * openfl_Position;")
+		gl_Position = openfl_Matrix * openfl_Position;
+		")
 	@:glVertexSource("#pragma header
-
 		void main(void) {
-
 			#pragma body
-
 		}")
-	@:glFragmentHeader("
+	@:glFragmentHeader(
+		#if android
+		"precision mediump float;" +
+		#end
+		"varying float openfl_Alphav;
+		varying vec4 openfl_ColorMultiplierv;
+		varying vec4 openfl_ColorOffsetv;
 		varying vec2 openfl_TextureCoordv;
 		varying vec4 colorM;
 
+		uniform bool openfl_HasColorTransform;
 		uniform sampler2D openfl_Texture;
-		uniform vec2 openfl_TextureSize;")
+		uniform vec2 openfl_TextureSize;
+		")
 	@:glFragmentBody("
 		gl_FragColor = texture2D (openfl_Texture, openfl_TextureCoordv) * colorM;
 	")
 	#if emscripten
 	@:glFragmentSource("#pragma header
-
 		void main(void) {
-
 			#pragma body
-
 			gl_FragColor = gl_FragColor.bgra;
-
 		}")
 	#else
 	@:glFragmentSource("#pragma header
-
 		void main(void) {
-
 			#pragma body
-
 		}")
 	#end
 	public function new(code:ByteArray = null)
