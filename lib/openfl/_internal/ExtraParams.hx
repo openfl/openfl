74d167276172c9f960d85946bbf0cea875037fcd
Joshua Granick <jgranick@users.noreply.github.com>
2020-08-07 10:23:47 -0700

    Adding internal source paths to extra params macro (resolve #2390)

--- lib/openfl/_internal/ExtraParams.hx
+++ lib/openfl/_internal/ExtraParams.hx
@@ -29,6 +29,39 @@ class ExtraParams
 			}
 			#end
 
+			if (Context.defined("lime"))
+			{
+				var childPath = Context.resolvePath("openfl/external");
+
+				var parts = StringTools.replace(childPath, "\\", "/").split("/");
+
+				if (parts.length > 3)
+				{
+					parts.pop();
+					parts.pop();
+					parts.pop();
+
+					var openflPath = parts.join("/");
+
+					var paths = [
+						"/packages/application/src", "/packages/assets/src", "/packages/bitmapdata/src", "/packages/displayobject/src",
+						"/packages/errors/src", "/packages/eventdispatcher/src", "/packages/externalinterface/src", "/packages/filereference/src",
+						"/packages/filters/src", "/packages/geom/src", "/packages/graphics/src", "/packages/input/src", "/packages/loader/src",
+						"/packages/movieclip/src", "/packages/printing/src", "/packages/renderer-cairo/src", "/packages/renderer-canvas/src",
+						"/packages/renderer-core/src", "/packages/renderer-dom/src", "/packages/renderer-flash/src", "/packages/renderer-stage3D/src",
+						"/packages/security/src", "/packages/sensors/src", "/packages/shader/src", "/packages/sharedobject/src", "/packages/socket/src",
+						"/packages/sound/src", "/packages/stage/src", "/packages/stage3D/src", "/packages/system/src", "/packages/telemetry/src",
+						"/packages/textfield/src", "/packages/tilemap/src", "/packages/urlloader/src", "/packages/urlrequest/src", "/packages/urlstream/src",
+						"/packages/utils/src", "/packages/video/src"
+					];
+
+					for (path in paths)
+					{
+						Compiler.addClassPath(openflPath + path);
+					}
+				}
+			}
+
 			if (Context.defined("js") && !Context.defined("nodejs") && !Context.defined("lime"))
 			{
 				var childPath = Context.resolvePath("openfl/external");
