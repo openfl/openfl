openfl/openfl

commit 8cdce8877002ce85b0fc689a0bef96b588c62b20

Bad copy paste

packages/textfield/src/openfl/text/_internal/TextEngine.hx
@@ -1039,6 +1039,11 @@ class TextEngine
 			}
 			else
 			{
+				positions = getPositions(text, startIndex, endIndex);
+				widthValue = getPositionsWidth(positions);
+			}
+			else
+			{
 				var tempIndex = startIndex;
 				var tempRangeEnd = formatRange.end;
 				var countRanges = 0;

commit 436604a6410f488db262997a1aa6f716b9501dfe

FIX: duplicated by mistake code block at TextEngine.hx
Caused by: 8cdce8877002ce85b0fc689a0bef96b588c62b20

packages/textfield/src/openfl/text/_internal/TextEngine.hx
@@ -1039,11 +1039,6 @@ class TextEngine
 			}
 			else
 			{
-				positions = getPositions(text, startIndex, endIndex);
-				widthValue = getPositionsWidth(positions);
-			}
-			else
-			{
 				var tempIndex = startIndex;
 				var tempRangeEnd = formatRange.end;
 				var countRanges = 0;