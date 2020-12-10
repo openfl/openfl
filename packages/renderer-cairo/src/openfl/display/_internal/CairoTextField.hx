769cc2c5207b55e10a8f97042bc40a1a4a42282c
Chris Speciale <christopher.gaf@gmail.com>
2020-12-10 01:13:41 -0500

    Update CairoTextField.hx

--- packages/renderer-cairo/src/openfl/display/_internal/CairoTextField.hx
+++ packages/renderer-cairo/src/openfl/display/_internal/CairoTextField.hx
@@ -32,6 +32,8 @@ class CairoTextField
 	{
 		#if lime_cairo
 		var textEngine = textField.__textEngine;
+		//textVounds maximizes rendering efficiency by clipping the rectangle to a minimal size containing only the text. Measurements 
+		//will always be smaller than bounds.
 		var useTextBounds = !(textEngine.background || textEngine.border);
 		var bounds = useTextBounds ? textEngine.textBounds : textEngine.bounds;
 		var graphics = textField.__graphics;
@@ -67,7 +69,9 @@ class CairoTextField
 					case START:
 						//not supported?
 					case JUSTIFY:
-						//not supported in Textfield yet?
+						cursorOffsetX += textField.defaultTextFormat.leftMargin;
+						cursorOffsetX += textField.defaultTextFormat.indent;
+						cursorOffsetX += textField.defaultTextFormat.blockIndent;
 					case END:
 						//not supported in Textfield yet?
 				}
