6cb46129cf6f42ee43638cdbe4dc9d3284a4ff07
dimensionscape <christopher.gaf@gmail.com>
2021-01-20 13:57:42 -0500

    format code

--- src/openfl/net/DatagramSocket.hx
+++ src/openfl/net/DatagramSocket.hx
@@ -15,32 +15,33 @@ import sys.net.Address;
 import sys.net.Host;
 import sys.net.UdpSocket;
 #end
+
 /**
- * The DatagramSocket class enables code to send and receive Universal Datagram Protocol (UDP) packets.
- * 
-This feature is supported on all desktop operating systems, on iOS, and on Android. You can test for support at run time 
-using the DatagramSocket.isSupported property.
+	* The DatagramSocket class enables code to send and receive Universal Datagram Protocol (UDP) packets.
+	*
+	This feature is supported on all desktop operating systems, on iOS, and on Android. You can test for support at run time
+	using the DatagramSocket.isSupported property.
 
-Datagram packets are individually transmitted between the source and destination. Packets can arrive in a different order 
-than they were sent. Packets lost in transmission are not retransmitted, or even detected.
+	Datagram packets are individually transmitted between the source and destination. Packets can arrive in a different order
+	than they were sent. Packets lost in transmission are not retransmitted, or even detected.
 
-Data sent using a datagram socket is not automatically broken up into packets of transmittable size. If you send a UDP 
-packet that exceeds the maximum transmission unit (MTU) size, network discards the packet (without warning). The limiting 
-MTU is the smallest MTU of any interface, switch, or router in the transmission path.
+	Data sent using a datagram socket is not automatically broken up into packets of transmittable size. If you send a UDP
+	packet that exceeds the maximum transmission unit (MTU) size, network discards the packet (without warning). The limiting
+	MTU is the smallest MTU of any interface, switch, or router in the transmission path.
 
-The Socket class uses TCP which provides guaranteed packet delivery and automatically divides and reassembles large packets. 
-TCP also provides better network bandwidth management. These features mean that data sent using a TCP socket incurs higher 
-latency, but for most uses, the benefits of TCP far outweigh the costs. Most network communication should use the Socket class 
-rather than the DatagramSocket class.
+	The Socket class uses TCP which provides guaranteed packet delivery and automatically divides and reassembles large packets.
+	TCP also provides better network bandwidth management. These features mean that data sent using a TCP socket incurs higher
+	latency, but for most uses, the benefits of TCP far outweigh the costs. Most network communication should use the Socket class
+	rather than the DatagramSocket class.
 
-The DatagramSocket class is useful for working with applications where a small transmission latency is important and packet 
-loss is tolerable. For example, network operations in voice-over-IP (VoIP) applications and real-time, multiplayer games can 
-often benefit from UDP. The DatagramSocket class is also useful for some server-side applications. Since UDP is a stateless 
-protocol, a server can handle more requests from more clients than it can with TCP.
+	The DatagramSocket class is useful for working with applications where a small transmission latency is important and packet
+	loss is tolerable. For example, network operations in voice-over-IP (VoIP) applications and real-time, multiplayer games can
+	often benefit from UDP. The DatagramSocket class is also useful for some server-side applications. Since UDP is a stateless
+	protocol, a server can handle more requests from more clients than it can with TCP.
 
-The DatagramSocket class can only be used in targets that support UDP.
+	The DatagramSocket class can only be used in targets that support UDP.
 
-@event data Dispatched when this socket receives a packet of data.
+	@event data Dispatched when this socket receives a packet of data.
  */
 #if !openfl_debug
 @:fileXml('tags="haxe,release"')
@@ -52,41 +53,42 @@ class DatagramSocket extends EventDispatcher
 		Indicates whether or not DatagramSocket features are supported in the run-time environment.
 	**/
 	public static var isSupported(default, null):Bool = #if !js true #else false #end;
-#if !js	
+
+	#if !js
 	/**
 		Indicates whether this socket object is currently bound to a local address and port.
 	**/
 	public var bound(default, null):Bool;
-	
+
 	/**
 		Indicates whether this socket object is currently connected to a remote address and port.
 	**/
 	public var connected(get, null):Bool;
-	
+
 	/**
 		The IP address this socket is bound to on the local machine.
 	**/
 	public var localAddress(get, null):String;
-	
+
 	/**
 		The port this socket is bound to on the local machine.
 	**/
 	public var localPort(get, null):Int;
-	
+
 	/**
 		The IP address of the remote machine to which this socket is connected.
 	**/
 	public var remoteAddress(get, null):String;
-	
+
 	/**
 		The port on the remote machine to which this socket is connected.
 	**/
 	public var remotePort(get, null):Int;
-	
+
 	@:noCompletion private var __udpSocket:UdpSocket;
 	@:noCompletion private var __isReceiving:Bool;
 	@:noCompletion private var __iBytes:Bytes = Bytes.alloc(4096);
-	@:noCompletion private var __buffer:ByteArray;	
+	@:noCompletion private var __buffer:ByteArray;
 
 	/**
 		Creates a DatagramSocket object
@@ -102,31 +104,31 @@ class DatagramSocket extends EventDispatcher
 
 	/**
 		Binds this socket to the specified local address and port.
-		
+
 		The bind() method executes synchronously. The bind operation completes before the next line of code is executed.
-		
+
 		Once a DatagramSocket is bound, it should be closed with the close() method before disposal to avoid subsequent
 		receiving of messages.
-		
-		@param localPort The number of the port to bind to on the local computer. If localPort, is set to 0 (the default), 
-		the next available system port is bound. Permission to connect to a port number below 1024 is subject to the system 
-		security policy. On Mac and Linux systems, for example, the application must be running with root privileges to 
-		connect to ports below 1024. 
-		@param localAddress The IP address on the local machine to bind to. This address can be an IPv4 or IPv6 address. 
-		If localAddress is set to 0.0.0.0 (the default), the socket listens on all available IPv4 addresses. To listen on 
-		all available IPv6 addresses, you must specify "::" as the localAddress argument. To use an IPv6 address, the 
+
+		@param localPort The number of the port to bind to on the local computer. If localPort, is set to 0 (the default),
+		the next available system port is bound. Permission to connect to a port number below 1024 is subject to the system
+		security policy. On Mac and Linux systems, for example, the application must be running with root privileges to
+		connect to ports below 1024.
+		@param localAddress The IP address on the local machine to bind to. This address can be an IPv4 or IPv6 address.
+		If localAddress is set to 0.0.0.0 (the default), the socket listens on all available IPv4 addresses. To listen on
+		all available IPv6 addresses, you must specify "::" as the localAddress argument. To use an IPv6 address, the
 		computer and network must both be configured to support IPv6. Furthermore, a socket bound to an IPv4 address cannot
-		connect to a socket with an IPv6 address. Likewise, a socket bound to an IPv6 address cannot connect to a socket with 
-		an IPv4 address. The type of address must match. 
+		connect to a socket with an IPv6 address. Likewise, a socket bound to an IPv6 address cannot connect to a socket with
+		an IPv4 address. The type of address must match.
 		@throws IOError This error occurs if the socket cannot be bound, such as when:
 
 		1. localPort is already in use by another socket.
-		2. the user account under which the application is running doesn't have sufficient system privileges to bind to the 
+		2. the user account under which the application is running doesn't have sufficient system privileges to bind to the
 		specified port. (Privilege issues typically occur when localPort < 1024.)
 		3. This DatagramSocket object is already bound.
 		4. This DatagramSocket object has been closed.
-		@throws ArgumentError This error occurs when localAddress is not a syntactically well-formed IP address. 
-		@throws RangeError This error occurs when localPort is less than 0 or greater than 65535. 
+		@throws ArgumentError This error occurs when localAddress is not a syntactically well-formed IP address.
+		@throws RangeError This error occurs when localPort is less than 0 or greater than 65535.
 	**/
 	public function bind(localPort:Int = 0, localAddress:String = "0.0.0.0"):Void
 	{
@@ -141,12 +143,13 @@ class DatagramSocket extends EventDispatcher
 		}
 		catch (e:Dynamic)
 		{
-			switch(e){
+			switch (e)
+			{
 				case "Bind failed":
 					throw new IOError("Operation attempted on invalid socket.");
 				case "Unresolved host":
 					throw new ArgumentError("One of the parameters is invalid");
-			}				
+			}
 		}
 	}
 
@@ -160,11 +163,11 @@ class DatagramSocket extends EventDispatcher
 	{
 		try
 		{
-		__udpSocket.close();
+			__udpSocket.close();
 		}
 		catch (e:Dynamic)
 		{
-			//do nothing
+			// do nothing
 		}
 		__isReceiving = false;
 		bound = false;
@@ -188,18 +191,17 @@ class DatagramSocket extends EventDispatcher
 		@param remotePort The port number on the remote machine used to establish a connection.
 		@throws ArgumentError This error occurs when localAddress is not a syntactically valid address. Or when a default
 		route address ('0.0.0.0' or '::') is used.
-		@throws RangeError This error occurs when localPort is less than 1 or greater than 65535. 
-		@throws IOError This error occurs if the socket cannot be connected, such as when bind() has not been called before 
-		the call to connect() and default binding to the remote address family is not possible. 
+		@throws RangeError This error occurs when localPort is less than 1 or greater than 65535.
+		@throws IOError This error occurs if the socket cannot be connected, such as when bind() has not been called before
+		the call to connect() and default binding to the remote address family is not possible.
 	**/
-		
 	public function connect(remoteAddress:String, remotePort:Int):Void
 	{
 		if (localPort > 65535 || localPort < 0)
 		{
 			throw new RangeError("Invalid socket port number specified.");
 		}
-		
+
 		try
 		{
 			__udpSocket.connect(new Host(remoteAddress), remotePort);
@@ -207,12 +209,13 @@ class DatagramSocket extends EventDispatcher
 		}
 		catch (e:Dynamic)
 		{
-			switch(e){
+			switch (e)
+			{
 				case "Bind failed":
 					throw new IOError("Operation attempted on invalid socket.");
 				case "Unresolved host":
 					throw new ArgumentError("One of the parameters is invalid");
-			}	
+			}
 		}
 	}
 
@@ -246,38 +249,37 @@ class DatagramSocket extends EventDispatcher
 		@param port The port number on the remote machine. A value greater than 0 and less than 65536 is required if the
 		port has not already been specified using the connect() method.
 		@throws ArgumentError If the socket is not connected and address is not a well-formed IP address.
-		@throws RangeError This error occurs when port is less than 1 or greater than 65535. 
+		@throws RangeError This error occurs when port is less than 1 or greater than 65535.
 		@throws IOError This error occurs:
 
 		If bind() has not been called, and when default binding to the destination address family is not possible.
-		On some operating systems, an IOError is thrown if the connect() method is called when an ICMP "destination 
-		unreachable" message has already been received from the target host. (Thus, the error is thrown on the second 
-		failed attempt to send data, not the first.) Other operating systems, such as Windows, disregard these ICMP messages, 
+		On some operating systems, an IOError is thrown if the connect() method is called when an ICMP "destination
+		unreachable" message has already been received from the target host. (Thus, the error is thrown on the second
+		failed attempt to send data, not the first.) Other operating systems, such as Windows, disregard these ICMP messages,
 		so no error is thrown.
-		@throws RangeError If offset is greater than the length of the ByteArray specified in bytes or if the amount of data 
-		specified to be written by offset plus length exceeds the data available. 
-		@throws IllegalOperationError If the address or port parameters are specified when the socket has already been connected. 
+		@throws RangeError If offset is greater than the length of the ByteArray specified in bytes or if the amount of data
+		specified to be written by offset plus length exceeds the data available.
+		@throws IllegalOperationError If the address or port parameters are specified when the socket has already been connected.
 	**/
-		
 	public function send(bytes:ByteArray, offset:UInt = 0, length:UInt = 0, address:String = null, port:Int = 0):Void
 	{
 		if (localPort > 65535 || localPort < 0)
 		{
 			throw new RangeError("Invalid socket port number specified.");
 		}
-		
+
 		if (offset + length > bytes.length)
 		{
 			throw new RangeError("The supplied index is out of bounds.");
 		}
-		try{
+		try
+		{
 			if (length == 0)
 			{
 				length = bytes.length;
 			}
 			if (address == null)
 			{
-				
 				if (connected)
 				{
 					__udpSocket.output.writeBytes(cast bytes, offset, length);
@@ -305,54 +307,57 @@ class DatagramSocket extends EventDispatcher
 			throw new IOError("Operation attempted on invalid socket.");
 		}
 	}
-	
-	override public function addEventListener(type:String, listener:Dynamic->Void, useCapture:Bool = false, priority:Int = 0, useWeakReference:Bool = false):Void 
+
+	override public function addEventListener(type:String, listener:Dynamic->Void, useCapture:Bool = false, priority:Int = 0,
+			useWeakReference:Bool = false):Void
 	{
 		super.addEventListener(type, listener, useCapture, priority, useWeakReference);
-		if (type == DatagramSocketDataEvent.DATA){
+		if (type == DatagramSocketDataEvent.DATA)
+		{
 			Lib.current.addEventListener(Event.ENTER_FRAME, __onFrameUpdate);
 		}
 	}
-	
-	override public function removeEventListener(type:String, listener:Dynamic->Void, useCapture:Bool = false):Void 
+
+	override public function removeEventListener(type:String, listener:Dynamic->Void, useCapture:Bool = false):Void
 	{
 		super.removeEventListener(type, listener, useCapture);
-		if (type == DatagramSocketDataEvent.DATA){
+		if (type == DatagramSocketDataEvent.DATA)
+		{
 			Lib.current.removeEventListener(Event.ENTER_FRAME, __onFrameUpdate);
 		}
 	}
 
 	@:noCompletion private function __onFrameUpdate(e:Event):Void
-	{		
+	{
 		while (__isReceiving)
 		{
 			try
 			{
-				//Todo: Avoid new object creation
-				var address:Address = new Address();				
+				// Todo: Avoid new object creation
+				var address:Address = new Address();
 				var bytesReady = __udpSocket.readFrom(__iBytes, 0, __iBytes.length, address);
 				__buffer.writeBytes(__iBytes, 0, bytesReady);
 				__buffer.position = 0;
-				dispatchEvent(new DatagramSocketDataEvent(DatagramSocketDataEvent.DATA, false, false,
-				address.getHost().toString(), address.port, __udpSocket.host().host.toString(), __udpSocket.host().port,
-				__buffer));
+				dispatchEvent(new DatagramSocketDataEvent(DatagramSocketDataEvent.DATA, false, false, address.getHost().toString(), address.port,
+					__udpSocket.host().host.toString(), __udpSocket.host().port, __buffer));
 				__buffer.clear();
 			}
 			catch (e:Error)
 			{
-				break;					
+				break;
 			}
 			catch (e:Dynamic)
 			{
-				//do nothing
+				// do nothing
 			}
-		}			
+		}
 	}
-	
+
 	@:noCompletion private function get_connected():Bool
 	{
 		#if neko
-		try{
+		try
+		{
 			__udpSocket.peer();
 			return true;
 		}
@@ -400,5 +405,5 @@ class DatagramSocket extends EventDispatcher
 		}
 		return 0;
 	}
-#end
+	#end
 }
