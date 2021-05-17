c95589f8dd2e34242d41c00584e26006abb27087
Chris Speciale <christopher.gaf@gmail.com>
2021-05-17 04:38:11 -0400

    New FileListEvent.hx
    For the filesystem api

---
+++ src/openfl/events/FileListEvent.hx
@@ -0,0 +1,32 @@
+package openfl.events;
+#if !air
+import openfl.events.Event;
+import openfl.filesystem.File;
+
+/**
+ * ...
+ * @author Christopher Speciale
+ */
+class FileListEvent extends Event 
+{
+	public static inline var DIRECTORY_LISTING:String = "directoryListing";
+	public static inline var SELECT_MULTIPLE:String = "selectMultiple";
+	
+	public var files:Array<File>;
+	
+	public function new(type:String, files:Array<File>, bubbles:Bool=false, cancelable:Bool=false) 
+	{
+		super(type, bubbles, cancelable);
+		this.files = files;
+		
+	}
+	
+	override public function clone():Event 
+	{
+		return new FileListEvent(type, files, bubbles, cancelable);
+	}
+	
+}
+#else
+typedef FileListEvent = flash.events.FileListEvent
+#end
