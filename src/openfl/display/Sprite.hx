d5792681d1609e58ab2067696660f8c50f6a7664
Joshua Granick <jgranick@users.noreply.github.com>
2021-10-31 18:39:28 -0700

    Sprite can be used as a linkage base class in FLAs

--- src/openfl/display/Sprite.hx
+++ src/openfl/display/Sprite.hx
@@ -27,8 +27,11 @@ import openfl.ui.MouseCursor;
 @:access(openfl.display.Stage)
 @:access(openfl.geom.Matrix)
 @:access(openfl.geom.Point)
+@:autoBuild(openfl.utils._internal.AssetsMacro.initBinding())
 class Sprite extends DisplayObjectContainer
 {
+	@:noCompletion private static var __constructor:Sprite->Void;
+
 	/**
 		Specifies the button mode of this sprite. If `true`, this
 		sprite behaves as a button, which means that it triggers the display of
@@ -148,6 +151,27 @@ class Sprite extends DisplayObjectContainer
 		__drawableType = SPRITE;
 		__buttonMode = false;
 		useHandCursor = true;
+
+		if (__constructor != null)
+		{
+			var method = __constructor;
+			__constructor = null;
+
+			method(this);
+		}
+	}
+
+	/**
+		Creates a new Sprite based upon the first frame of a Timeline instance.
+
+		@param timeline A Timeline object
+		@return A new Sprite
+	**/
+	public static function fromTimeline(timeline:Timeline):Sprite
+	{
+		var sprite = new Sprite();
+		timeline.initializeSprite(sprite);
+		return sprite;
 	}
 
 	/**
