56b6c7e408bfb076a19ac9d3d4b3a576bb6222d9
Chris Speciale <christopher.gaf@gmail.com>
2021-11-18 09:02:19 -0500

    Reinstate intended Object changes with @:callable meta

--- src/openfl/utils/Object.hx
+++ src/openfl/utils/Object.hx
@@ -1,6 +1,10 @@
 package openfl.utils;
 
-@:forward() abstract Object(ObjectType) from ObjectType from Dynamic to Dynamic
+import haxe.Constraints.Function;
+import openfl.display.DisplayObjectContainer;
+
+@:transitive
+abstract Object(ObjectType) from ObjectType from Dynamic to Dynamic
 {
 	public inline function new()
 	{
@@ -56,30 +60,532 @@ package openfl.utils;
 	}
 
 	@:op(a.b)
-	private inline function __fieldRead(name:String):Dynamic
+	private inline function __fieldRead(name:String):ObjectValue
 	{
 		return __get(name);
 	}
 
 	#if haxe4
 	@:op(a.b)
-	private inline function __fieldWrite(name:String, value:Dynamic):Dynamic
+	private inline function __fieldWrite(name:String, value:ObjectValue):ObjectValue
 	{
 		return __set(name, value);
 	}
 	#end
 
 	@SuppressWarnings("checkstyle:FieldDocComment")
-	@:arrayAccess @:noCompletion @:dox(hide) public inline function __get(key:String):Dynamic
+	@:arrayAccess @:noCompletion @:dox(hide) public inline function __get(key:String):ObjectValue
 	{
-		return Reflect.getProperty(this, key);
+		var _this:ObjectValue = this;
+		return _this[key];
 	}
 
 	@SuppressWarnings("checkstyle:FieldDocComment")
-	@:arrayAccess @:noCompletion @:dox(hide) public inline function __set(key:String, value:Dynamic):Dynamic
+	@:arrayAccess @:noCompletion @:dox(hide) public inline function __set(key:String, value:ObjectValue):ObjectValue
+	{
+		var _this:ObjectValue = this;
+		return _this[key] = value;
+	}
+
+	@SuppressWarnings("checkstyle:FieldDocComment")
+	@:arrayAccess @:noCompletion @:dox(hide) public inline function __getArray(index:Int):ObjectValue
+	{
+		var arr = cast(this, Array<Dynamic>);
+		return arr[index];
+	}
+
+	@SuppressWarnings("checkstyle:FieldDocComment")
+	@:arrayAccess @:noCompletion @:dox(hide) public inline function __setArray(index:Int, value:ObjectValue):ObjectValue
+	{
+		var arr = cast(this, Array<Dynamic>);
+		return arr[index] = value;
+	}
+}
+
+@SuppressWarnings("checkstyle:FieldDocComment")
+@:transitive
+@:callable private abstract ObjectValue(Dynamic) from Dynamic to Dynamic
+{
+	@:to private function toFunction():Function
+	{
+		return this;
+	}
+
+	@:to private function toFloat():Float
+	{
+		if (#if (haxe_ver >= 4.2) Std.isOfType #else Std.is #end (this, Float))
+		{
+			return cast this;
+		}
+		else
+		{
+			return Math.NaN;
+		}
+	}
+
+	@:to private function toInt():Int
+	{
+		return cast this;
+	}
+
+	@:to private function toBool():Bool
+	{
+		if (#if (haxe_ver >= 4.2) Std.isOfType #else Std.is #end (this, Bool))
+		{
+			return cast this;
+		}
+		else if (#if (haxe_ver >= 4.2) Std.isOfType #else Std.is #end (this, Float))
+		{
+			return this != 0;
+		}
+		else
+		{
+			return this != null;
+		}
+	}
+
+	@:op(a.b)
+	@:arrayAccess
+	@:noCompletion @:dox(hide) public /*inline*/ function __get(key:String):ObjectValue
+	{
+		if (Reflect.hasField(this, key))
+		{
+			return Reflect.field(this, key);
+		}
+		else if (#if (haxe_ver >= 4.2) Std.isOfType #else Std.is #end (this, DisplayObjectContainer))
+		{
+			var container:DisplayObjectContainer = cast this;
+			var child = container.getChildByName(key);
+			if (child != null) return child;
+		}
+		return Reflect.getProperty(this, key);
+	}
+
+	@:op(a.b)
+	@:arrayAccess
+	@:noCompletion @:dox(hide) public inline function __set(key:String, value:ObjectValue):ObjectValue
 	{
 		Reflect.setProperty(this, key, value);
 		return value;
+	}
+
+	@:arrayAccess @:noCompletion @:dox(hide) public inline function __getArray(index:Int):ObjectValue
+	{
+		var arr = cast(this, Array<Dynamic>);
+		return arr[index];
+	}
+
+	@:op(-A) private inline function negate():ObjectValue
+	{
+		var float:Float = this;
+		return -float;
+	}
+
+	@:op(++A) private inline function preIncrement():ObjectValue
+	{
+		var float:Float = this;
+		return ++float;
+	}
+
+	@:op(A++) private inline function postIncrement():ObjectValue
+	{
+		var float:Float = this;
+		return float++;
+	}
+
+	@:op(--A) private inline function preDecrement():ObjectValue
+	{
+		var float:Float = this;
+		return --float;
+	}
+
+	@:op(A--) private inline function postDecrement():ObjectValue
+	{
+		var float:Float = this;
+		return float--;
+	}
+
+	@:op(A + B) private static inline function add(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA + floatB;
+	}
+
+	@:op(A + B) @:commutative private static inline function addInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var floatA:Float = a;
+		return floatA + b;
+	}
+
+	@:op(A + B) @:commutative private static function addFloat(a:ObjectValue, b:Float):Float
+	{
+		var floatA:Float = a;
+		return floatA + b;
+	}
+
+	@:op(A - B) private static inline function sub(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA - floatB;
+	}
+
+	@:op(A - B) private static inline function subInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var floatA:Float = a;
+		return floatA - b;
+	}
+
+	@:op(A - B) private static inline function intSub(a:Int, b:ObjectValue):ObjectValue
+	{
+		var floatB:Float = b;
+		return a - floatB;
+	}
+
+	@:op(A - B) private static function subFloat(a:ObjectValue, b:Float):Float
+	{
+		var floatA:Float = a;
+		return floatA - b;
+	}
+
+	@:op(A - B) private static function floatSub(a:Float, b:ObjectValue):Float
+	{
+		var floatB:Float = b;
+		return a - floatB;
+	}
+
+	@:op(A * B) private static function mul(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA * floatB;
+	}
+
+	@:op(A * B) @:commutative private static inline function mulInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var floatA:Float = a;
+		return floatA * b;
+	}
+
+	@:op(A * B) @:commutative private static function mulFloat(a:ObjectValue, b:Float):Float
+	{
+		var floatA:Float = a;
+		return floatA * b;
+	}
+
+	@:op(A / B) private static function div(a:ObjectValue, b:ObjectValue):Float
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA / floatB;
+	}
+
+	@:op(A / B) private static function divInt(a:ObjectValue, b:Int):Float
+	{
+		var floatA:Float = a;
+		return floatA / b;
+	}
+
+	@:op(A / B) private static function intDiv(a:Int, b:ObjectValue):Float
+	{
+		var floatB:Float = b;
+		return a / floatB;
+	}
+
+	@:op(A / B) private static function divFloat(a:ObjectValue, b:Float):Float
+	{
+		var floatA:Float = a;
+		return floatA / b;
+	}
+
+	@:op(A / B) private static function floatDiv(a:Float, b:ObjectValue):Float
+	{
+		var floatB:Float = b;
+		return a / floatB;
+	}
+
+	@:op(A % B) private static function mod(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA % floatB;
+	}
+
+	@:op(A % B) private static function modInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var floatA:Float = a;
+		return floatA % b;
+	}
+
+	@:op(A % B) private static function intMod(a:Int, b:ObjectValue):ObjectValue
+	{
+		var floatB:Float = b;
+		return a % floatB;
+	}
+
+	@:op(A % B) private static function modFloat(a:ObjectValue, b:Float):Float
+	{
+		var floatA:Float = a;
+		return floatA % b;
+	}
+
+	@:op(A % B) private static function floatMod(a:Float, b:ObjectValue):Float
+	{
+		var floatB:Float = b;
+		return a % floatB;
+	}
+
+	@:op(A == B) private static function eq(a:ObjectValue, b:ObjectValue):Bool
+	{
+		var dynamicA:Dynamic = a;
+		var dynamicB:Dynamic = b;
+		return dynamicA == dynamicB;
+	}
+
+	@:op(A == B) @:commutative private static function eqDynamic(a:ObjectValue, b:Dynamic):Bool
+	{
+		var dynamicA:Dynamic = a;
+		return dynamicA == b;
+	}
+
+	@:op(A != B) private static function neq(a:ObjectValue, b:ObjectValue):Bool
+	{
+		var dynamicA:Dynamic = a;
+		var dynamicB:Dynamic = b;
+		return dynamicA != dynamicB;
+	}
+
+	@:op(A != B) @:commutative private static function neqDynamic(a:ObjectValue, b:Dynamic):Bool
+	{
+		var dynamicA:Dynamic = a;
+		return dynamicA != b;
+	}
+
+	@:op(A < B) private static function lt(a:ObjectValue, b:ObjectValue):Bool
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA < floatB;
+	}
+
+	@:op(A < B) private static function ltInt(a:ObjectValue, b:Int):Bool
+	{
+		var floatA:Float = a;
+		return floatA < b;
+	}
+
+	@:op(A < B) private static function intLt(a:Int, b:ObjectValue):Bool
+	{
+		var floatB:Float = b;
+		return a < floatB;
+	}
+
+	@:op(A < B) private static function ltFloat(a:ObjectValue, b:Float):Bool
+	{
+		var floatA:Float = a;
+		return floatA < b;
+	}
+
+	@:op(A < B) private static function floatLt(a:Float, b:ObjectValue):Bool
+	{
+		var floatB:Float = b;
+		return a < floatB;
+	}
+
+	@:op(A <= B) private static function lte(a:ObjectValue, b:ObjectValue):Bool
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA <= floatB;
+	}
+
+	@:op(A <= B) private static function lteInt(a:ObjectValue, b:Int):Bool
+	{
+		var floatB:Float = b;
+		return a <= floatB;
+	}
+
+	@:op(A <= B) private static function intLte(a:Int, b:ObjectValue):Bool
+	{
+		var floatA:Float = a;
+		return floatA <= b;
+	}
+
+	@:op(A <= B) private static function lteFloat(a:ObjectValue, b:Float):Bool
+	{
+		var floatB:Float = b;
+		return a <= floatB;
+	}
+
+	@:op(A <= B) private static function floatLte(a:Float, b:ObjectValue):Bool
+	{
+		var floatA:Float = a;
+		return floatA <= b;
+	}
+
+	@:op(A > B) private static function gt(a:ObjectValue, b:ObjectValue):Bool
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA > floatB;
+	}
+
+	@:op(A > B) private static function gtInt(a:ObjectValue, b:Int):Bool
+	{
+		var floatB:Float = b;
+		return a > floatB;
+	}
+
+	@:op(A > B) private static function intGt(a:Int, b:ObjectValue):Bool
+	{
+		var floatB:Float = b;
+		return a > floatB;
+	}
+
+	@:op(A > B) private static function gtFloat(a:ObjectValue, b:Float):Bool
+	{
+		var floatB:Float = b;
+		return a > floatB;
+	}
+
+	@:op(A > B) private static function floatGt(a:Float, b:ObjectValue):Bool
+	{
+		var floatB:Float = b;
+		return a > floatB;
+	}
+
+	@:op(A >= B) private static function gte(a:ObjectValue, b:ObjectValue):Bool
+	{
+		var floatA:Float = a;
+		var floatB:Float = b;
+		return floatA >= floatB;
+	}
+
+	@:op(A >= B) private static function gteInt(a:ObjectValue, b:Int):Bool
+	{
+		var floatB:Float = b;
+		return a >= floatB;
+	}
+
+	@:op(A >= B) private static function intGte(a:Int, b:ObjectValue):Bool
+	{
+		var floatB:Float = b;
+		return a >= floatB;
+	}
+
+	@:op(A >= B) private static function gteFloat(a:ObjectValue, b:Float):Bool
+	{
+		var floatB:Float = b;
+		return a >= floatB;
+	}
+
+	@:op(A >= B) private static function floatGte(a:Float, b:ObjectValue):Bool
+	{
+		var floatB:Float = b;
+		return a >= floatB;
+	}
+
+	@:op(~A) private function complement():ObjectValue
+	{
+		var int:Int = this;
+		return ~int;
+	}
+
+	@:op(A & B) private static function and(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var intA:Int = a;
+		var intB:Int = b;
+		return intA & intB;
+	}
+
+	@:op(A & B) @:commutative private static function andInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var intA:Int = a;
+		return intA & b;
+	}
+
+	@:op(A | B) private static function or(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var intA:Int = a;
+		var intB:Int = b;
+		return intA | intB;
+	}
+
+	@:op(A | B) @:commutative private static function orInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var intA:Int = a;
+		return intA | b;
+	}
+
+	@:op(A ^ B) private static function xor(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var intA:Int = a;
+		var intB:Int = b;
+		return intA ^ intB;
+	}
+
+	@:op(A ^ B) @:commutative private static function xorInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var intA:Int = a;
+		return intA ^ b;
+	}
+
+	@:op(A >> B) private static function shr(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var intA:Int = a;
+		var intB:Int = b;
+		return intA >> intB;
+	}
+
+	@:op(A >> B) private static function shrInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var intA:Int = a;
+		return intA >> b;
+	}
+
+	@:op(A >> B) private static function intShr(a:Int, b:ObjectValue):ObjectValue
+	{
+		var intB:Int = b;
+		return a >> intB;
+	}
+
+	@:op(A >>> B) private static function ushr(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var intA:Int = a;
+		var intB:Int = b;
+		return intA >>> intB;
+	}
+
+	@:op(A >>> B) private static function ushrInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var intA:Int = a;
+		return intA >>> b;
+	}
+
+	@:op(A >>> B) private static function intUshr(a:Int, b:ObjectValue):ObjectValue
+	{
+		var intB:Int = b;
+		return a >>> intB;
+	}
+
+	@:op(A << B) private static function shl(a:ObjectValue, b:ObjectValue):ObjectValue
+	{
+		var intA:Int = a;
+		var intB:Int = b;
+		return intA << intB;
+	}
+
+	@:op(A << B) private static function shlInt(a:ObjectValue, b:Int):ObjectValue
+	{
+		var intA:Int = a;
+		return intA << b;
+	}
+
+	@:op(A << B) private static function intShl(a:Int, b:ObjectValue):ObjectValue
+	{
+		var intB:Int = b;
+		return a << intB;
 	}
 }
 
