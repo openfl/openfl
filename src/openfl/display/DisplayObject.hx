af7b29bb9585193c9f2d8bc7b4a64a2ca94cd1aa
Rainy <939029835@qq.com>
2022-05-06 23:03:49 +0800

    inline __updateFlag

--- src/openfl/display/DisplayObject.hx
+++ src/openfl/display/DisplayObject.hx
@@ -1728,7 +1728,7 @@ class DisplayObject extends EventDispatcher implements IBitmapDrawable #if (open
 	#if (queue_experimental_optimization && !dom)
 	@:noCompletion private var _flag:Bool = false;
 
-	@:noCompletion private function __updateFlag(add:Bool = true):Void
+	@:noCompletion inline private function __updateFlag(add:Bool = true):Void
 	{
 		if (add)
 		{
