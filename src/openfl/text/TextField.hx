520fb37d2a67b18b4750431f568ac06dae3e1ea3
Junsred <63877982+Junsred@users.noreply.github.com>
2022-05-26 01:00:10 +0300

    revert first commit

--- src/openfl/text/TextField.hx
+++ src/openfl/text/TextField.hx
@@ -2822,9 +2822,6 @@ class TextField extends InteractiveObject
 	{
 		__updateLayout();
 
-		if (value > __textEngine.maxScrollV) value = __textEngine.maxScrollV;
-		if (value < 1) value = 1;
-
 		if (value != __textEngine.scrollV || __textEngine.scrollV == 0)
 		{
 			__dirty = true;
