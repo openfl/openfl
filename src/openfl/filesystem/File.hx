efde05d52afc13e75cab81a493e024fd3860ac78
Chris Speciale <christopher.gaf@gmail.com>
2022-10-26 08:54:06 -0400

    File: Prevent multiple filter types until Lime Fix
    This ensures that only the first filter type can be used. The glue in Lime for our native file dialog lib only allows a single type to be used so we can implement multiple types as soon as the issue is corrected in Lime.

--- src/openfl/filesystem/File.hx
+++ src/openfl/filesystem/File.hx
@@ -1623,20 +1623,28 @@ class File extends FileReference
 
 	@:noCompletion private function __getFilterTypes(typeFilter:Array<FileFilter>):String
 	{
-		var filter:String = null;
-
+		var filterString:String = null;
+		var filters = [];
+		
 		if (typeFilter != null)
 		{
-			var filters = [];
+			
 
-			for (type in typeFilter)
+			for (filter in typeFilter)
 			{
-				filters.push(StringTools.replace(StringTools.replace(type.extension, "*.", ""), ";", ","));
+				var types:Array<String> = filter.extension.split(";");
+				
+				for (type in types){
+					filters.push(StringTools.replace(type, "*.", ""));
+				}
 			}
 
-			filter = filters.join(";");
+			filterString = filters.join(",");			
 		}
-		return filter;
+
+		//TODO: Multiple types are not yet supported in Lime so instead we return the first index of filters
+		//return filterString;
+		return filters[0];
 	}
 
 	@:noCompletion private static function __getTempPath(dir:Bool):String
