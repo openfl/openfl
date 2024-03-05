2ade77d18347609f3a828d785103bb7ff0033340
Joseph Cloutier <player3.14@gmail.com>
2024-03-04 22:10:32 -0500

    Work around text highlighting crash (#2602).
    The wrong characters are still highlighted, but it no longer crashes, which is more important for now.

--- src/openfl/display/_internal/CairoTextField.hx
+++ src/openfl/display/_internal/CairoTextField.hx
@@ -355,13 +355,6 @@ class CairoTextField
 								selectionEnd = group.endIndex;
 							}
 
-							// this isn't supposed to happen, but better to
-							// avoid a crash if there's a bug somewhere
-							if (glyphs.length < selectionEnd - selectionStart)
-							{
-								selectionEnd = selectionStart + glyphs.length;
-							}
-
 							var start, end;
 
 							start = textField.getCharBoundaries(selectionStart);
@@ -393,6 +386,11 @@ class CairoTextField
 
 								selectionStart -= group.startIndex;
 								selectionEnd -= group.startIndex;
+								if (selectionEnd > glyphs.length)
+								{
+									selectionEnd = glyphs.length;
+								}
+
 								for (i in selectionStart...selectionEnd)
 									selectedGylphs.push(glyphs[i]);
 								cairo.showGlyphs(selectedGylphs);
