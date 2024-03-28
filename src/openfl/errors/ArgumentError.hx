b124bd5b6949a7b6d20209c85b20e8b1e77e3489
Josh Tynjala <joshtynjala@bowlerhat.dev>
2024-03-28 12:37:51 -0700

    Some missing docs for the openfl.errors types

--- src/openfl/errors/ArgumentError.hx
+++ src/openfl/errors/ArgumentError.hx
@@ -1,12 +1,22 @@
 package openfl.errors;
 
 #if !flash
+/**
+	The ArgumentError class represents an error that occurs when the arguments
+	supplied in a function do not match the arguments defined for that function.
+	This error occurs, for example, when a function is called with the wrong
+	number of arguments, an argument of the incorrect type, or an invalid
+	argument.
+**/
 #if !openfl_debug
 @:fileXml('tags="haxe,release"')
 @:noDebug
 #end
 class ArgumentError extends Error
 {
+	/**
+		Creates an ArgumentError object.
+	**/
 	public function new(message:String = "")
 	{
 		super(message);
--- src/openfl/errors/TypeError.hx
+++ src/openfl/errors/TypeError.hx
@@ -1,6 +1,10 @@
 package openfl.errors;
 
 #if !flash
+/**
+	A TypeError exception is thrown when the actual type of an operand is
+	different from the expected type.
+**/
 #if !openfl_debug
 @:fileXml('tags="haxe,release"')
 @:noDebug
