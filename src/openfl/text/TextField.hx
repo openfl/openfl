fac0b815124be38a72f268389ebdf7db5b33bb0b
Josh Tynjala <joshtynjala@bowlerhat.dev>
2024-06-21 08:54:23 -0700

    TextField: call __stopTextInput() before stage reference is set to null to prevent memory leak
    Enures that window event listeners are removed, if the TextField had focus at the time

--- src/openfl/text/TextField.hx
+++ src/openfl/text/TextField.hx
@@ -1607,6 +1607,14 @@ class TextField extends InteractiveObject
 		__setRenderDirty();
 	}
 
+	@:noCompletion private override function __setStageReference(stage:Stage):Void
+	{
+		// call __stopTextInput() before this.stage is set to null to ensure
+		// that all window listeners are removed to avoid a memory leak
+		__stopTextInput();
+		super.__setStageReference(stage);
+	}
+
 	@:noCompletion private override function __allowMouseFocus():Bool
 	{
 		// mouse focus is still allowed when tabEnabled is false
