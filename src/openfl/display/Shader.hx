be7fe9a8547944dd41b83a350f000a871cfd48c9
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2025-03-17 09:46:31 +0200

    Update Shader.hx

--- src/openfl/display/Shader.hx
+++ src/openfl/display/Shader.hx
@@ -12,6 +12,7 @@ import openfl.utils._internal.Log;
 import openfl.display3D.Context3D;
 import openfl.display3D.Program3D;
 import openfl.utils.ByteArray;
+import openfl.Lib;
 
 /**
 	// TODO: Document GLSL Shaders
@@ -119,6 +120,8 @@ import openfl.utils.ByteArray;
 @:access(openfl.display3D.Program3D)
 @:access(openfl.display.ShaderInput)
 @:access(openfl.display.ShaderParameter)
+@:access(openfl.display.Stage)
+@:access(openfl.events.UncaughtErrorEvents)
 // #if (!display && !macro)
 #if !macro
 @:autoBuild(openfl.utils._internal.ShaderMacro.build())
@@ -511,7 +514,22 @@ class Shader
 
 				// TODO
 				// program.uploadSources (vertex, fragment);
-				program.__glProgram = __createGLProgram(vertex, fragment);
+
+				if (Lib.current.stage.__uncaughtErrorEvents.__enabled)
+				{
+					try
+					{
+						program.__glProgram = __createGLProgram(vertex, fragment);
+					}
+					catch (e:Dynamic)
+					{
+						Lib.current.stage.__handleError(e);
+	
+						program.__glProgram = null;
+					}
+				}
+				else
+					program.__glProgram = __createGLProgram(vertex, fragment);
 
 				__context.__programs.set(id, program);
 			}
